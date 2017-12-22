1e7c9ff422cb7c60f4558afc54fdb4dc080fff2e
Gary Foster <gary@radicalbear.com>
2017-12-22 06:51:52 -0700

    Refactor mailer

--- app/jobs/global_validity_job.rb
+++ app/jobs/global_validity_job.rb
@@ -8,7 +8,7 @@ class GlobalValidityJob < ApplicationJob
       RadMailer.global_validity(company, current_member, problems).deliver_later
     else
       message = "No invalid data found in company #{company.name} (#{company.id})"
-      RadbearMailer.simple_message(company, current_member, message, message).deliver_later
+      RadMailer.simple_message(company, current_member, message, message).deliver_later
     end
   end
 end
--- app/mailers/rad_mailer.rb
+++ app/mailers/rad_mailer.rb
@@ -1,9 +1,34 @@
 class RadMailer < ActionMailer::Base
+  include ActionView::Helpers::TextHelper
   helper RadCommon::SecuredLinkHelper
 
   layout 'radbear_mailer'
   before_action :set_defaults
   default from: Devise.mailer_sender
+
+  def simple_message(company, recipient, subject, message, options = {})
+    @company = company
+    @recipient = recipient
+    @message = simple_format(message)
+
+    if options[:email_action]
+      @email_action = options[:email_action]
+    end
+
+    if @recipient.respond_to?(:email)
+      to_address = "\"#{@recipient}\" <#{@recipient.email}>"
+    elsif @recipient.class == String
+      to_address = @recipient
+    else
+      raise "recipient of type #{@recipient.class} if not valid"
+    end
+
+    if options[:from]
+      mail(to: to_address, subject: subject, from: options[:from])
+    else
+      mail(to: to_address, subject: subject)
+    end
+  end
 
   def global_validity(company, recipient, problems)
     @company = company
