0ae79748999be0b653b96b2764c793cdf706757a
Gary Foster <gary@radicalbear.com>
2018-01-01 11:39:43 -0500

    Validate company email

--- app/models/concerns/rad_common_company.rb
+++ app/models/concerns/rad_common_company.rb
@@ -1,16 +1,20 @@
 module RadCommonCompany
   extend ActiveSupport::Concern
 
+  included do
+    validates :email, format: { with: Devise.email_regexp, message: 'has an invalid email format' }
+  end
+
   def send_system_message(from, message)
     self.members.active.each do |member|
-      RadbearMailer.simple_message(self, member, "Important Message From #{I18n::t(:app_name)}", message, from: from).deliver_later
+      RadbearMailer.simple_message(self, member, "Important Message From #{I18n.t(:app_name)}", message, from: from).deliver_later
     end
   end
 
   module ClassMethods
     def send_system_message_global(from, message)
       Member.active.by_id.each do |member|
-        RadbearMailer.simple_message(member.company, member, "Important Message From #{I18n::t(:app_name)}", message, from: from).deliver_later
+        RadbearMailer.simple_message(member.company, member, "Important Message From #{I18n.t(:app_name)}", message, from: from).deliver_later
       end
     end
   end
