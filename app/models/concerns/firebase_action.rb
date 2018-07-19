b5ad1d2d66d8bbf64aafe9c2c48567827da4c7b8
Gary Foster <gary@radicalbear.com>
2018-07-19 08:16:25 -0400

    Rad retry

--- app/models/concerns/firebase_action.rb
+++ app/models/concerns/firebase_action.rb
@@ -18,23 +18,20 @@ module FirebaseAction
                            button_text: 'View Logs',
                            button_url:  Rails.application.routes.url_helpers.firebase_logs_url }
 
-          RadbearMailer.simple_message(Company.main, admin, "User Error on #{I18n.t(:app_name)}", "An operation attempted by #{user_description} failed: #{error}.", email_action: email_action).deliver_later
+          RadbearMailer.simple_message(Company.main, admin,
+                                       "User Error on #{I18n.t(:app_name)}",
+                                       "An operation attempted by #{user_description} failed: #{error}.",
+                                       email_action: email_action).deliver_later
         end
       end
 
       data['timestamp'] = Time.zone.now.to_s
 
-      response = app.client.update("logs#{path}", data)
+      response = RadicalRetry.perform_request { app.client.update("logs#{path}", data) }
+      raise response.body unless response.success?
 
-      unless response.success?
-        raise response.body
-      end
-
-      response = app.client.delete("transactions#{path}")
-
-      unless response.success?
-        raise response.body
-      end
+      response = RadicalRetry.perform_request { app.client.delete("transactions#{path}") }
+      raise response.body unless response.success?
 
       user&.update_firebase_info(app)
     end
