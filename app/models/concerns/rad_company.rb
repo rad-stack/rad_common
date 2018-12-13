89a77cba3714cfd6d81370768153b47258ee8caa
garyfoster <gary@radicalbear.com>
2018-12-13 06:59:45 -0500

    Refactor common company code

--- app/models/concerns/rad_company.rb
+++ app/models/concerns/rad_company.rb
@@ -2,9 +2,36 @@ module RadCompany
   extend ActiveSupport::Concern
 
   included do
+    alias_attribute :to_s, :name
+
     scope :by_id, -> { order(:id) }
+
     validates :email, format: { with: Devise.email_regexp, message: 'has an invalid email format' }
+    validate :validate_only_one, on: :create
     validate :validate_domains
+    validates_with PhoneNumberValidator
+
+    audited
+  end
+
+  module ClassMethods
+    def main
+      Company.first
+    end
+  end
+
+  def full_address
+    "#{address_line_1}, #{address_line_2}"
+  end
+
+  def address_line_1
+    line = address_1
+    line = "#{line}, #{address_2}" if address_2.present?
+    line
+  end
+
+  def address_line_2
+    "#{city}, #{state} #{zipcode}"
   end
 
   def send_system_message(message)
@@ -14,22 +41,23 @@ module RadCompany
   end
 
   def global_validity_ran!
-    update_column(:validity_checked_at, Time.zone.now)
+    update! validity_checked_at: Time.zone.now
   end
 
   def check_global_validity
     error_messages = []
 
-    exclude_models = [ActiveRecord::SchemaMigration, ApplicationRecord, Audited::Audit, SecurityRolesUser] + Rails.application.config.global_validity_exclude
+    exclude_models = [ActiveRecord::SchemaMigration,
+                      ApplicationRecord,
+                      Audited::Audit,
+                      SecurityRolesUser] + Rails.application.config.global_validity_exclude
 
     Rails.application.eager_load!
     all_models = ActiveRecord::Base.descendants
     models = all_models - exclude_models
 
     models.each do |model|
-      unless exclude_models.include?(model.to_s)
-        error_messages = error_messages.concat(check_model(model))
-      end
+      error_messages = error_messages.concat(check_model(model)) unless exclude_models.include?(model.to_s)
     end
 
     specific_queries = Rails.application.config.global_validity_include
@@ -74,12 +102,14 @@ module RadCompany
       supressions = Rails.application.config.global_validity_supress || []
       supression = supressions.select { |item| item[:class] == record.class.to_s }.first
 
-      if supression
-        messages -= supression[:messages]
-      end
-
+      messages -= supression[:messages] if supression
       return unless messages.any?
+
       error_messages_array.push([record, messages.join(', ')])
+    end
+
+    def validate_only_one
+      errors.add(:base, 'Only one company record is allowed.') if Company.count.positive?
     end
 
     def validate_domains
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RadCommon::Engine.routes.draw do
-  get "global_search", to: "search#global_search"
-  get "global_search_result", to: "search#global_search_result"
-  post :email_error, to: "rad_common/sendgrid#email_error"
+  get 'global_search', to: 'search#global_search'
+  get 'global_search_result', to: 'search#global_search_result'
+  post :email_error, to: 'rad_common/sendgrid#email_error'
 
   resources :companies, only: [] do
     get :usage, on: :member
