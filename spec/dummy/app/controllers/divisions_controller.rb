f34741a80c0533495a80f2d4cd913057666e17d3
Eric Bundy <elbzero@gmail.com>
2019-03-20 18:49:30 -0400

    fix test

--- spec/dummy/app/controllers/divisions_controller.rb
+++ spec/dummy/app/controllers/divisions_controller.rb
@@ -58,6 +58,6 @@ class DivisionsController < ApplicationController
     end
 
     def permitted_params
-      params.require(:division).permit(:name, :code, :notify, :timezone)
+      params.require(:division).permit(:name, :code, :notify, :timezone, :owner_id)
     end
 end
