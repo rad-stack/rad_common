ebd1a8fc43bcd9d7d3185c5c5c4d960f365b1c4e
Eric Bundy <elbzero@gmail.com>
2019-04-02 16:13:39 -0400

    fix firebase tests

--- spec/requests/firebase_logs_spec.rb
+++ spec/requests/firebase_logs_spec.rb
@@ -7,14 +7,14 @@ describe 'Firebase Logs', type: :request do
     login_as(admin, scope: :user)
   end
 
-  xit 'retrieves all available firebase logs' do
+  it 'retrieves all available firebase logs' do
     get '/firebase_logs'
     expect(assigns(:firebase_logs).first.first).to eq('registrations')
   end
 
-  xit 'destroy events' do
+  it 'destroy events' do
     allow_any_instance_of(FirebaseLogDestroyJob).to receive(:perform).and_return(nil)
-    delete "firebase_logs/events", params: { type: 'all' }, headers: { HTTP_REFERER: firebase_logs_path}
+    delete '/firebase_logs/events', params: { type: 'all' }, headers: { HTTP_REFERER: firebase_logs_path}
     expect(response).to redirect_to(firebase_logs_path)
   end
 end
