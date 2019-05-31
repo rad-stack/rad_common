6e91c6a4b29c924a79e265ea38c1ce1265ae2647
garyfoster <gary@radicalbear.com>
2019-05-31 05:59:16 -0400

    Start on external roles

--- spec/dummy/app/models/security_role.rb
+++ spec/dummy/app/models/security_role.rb
@@ -1,19 +1,3 @@
 class SecurityRole < ApplicationRecord
   include RadSecurityRole
-
-  def self.seed_items
-    seed_admin
-    seed_user
-    seed_manager
-  end
-
-  class << self
-    private def seed_manager
-      group = get_group('Manager')
-      group.create_division = true
-      group.read_division = true
-      group.update_division = true
-      group.save!
-    end
-  end
 end
