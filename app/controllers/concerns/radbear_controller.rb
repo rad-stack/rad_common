ecbcc68a973c2876b64c5f99e9d547b3e1e3c634
Caleb Tocco <calebtocco@hotmail.com>
2019-07-03 16:52:09 -0400

    required validation

--- app/controllers/concerns/radbear_controller.rb
+++ app/controllers/concerns/radbear_controller.rb
@@ -6,9 +6,9 @@ module RadbearController
     before_action :set_raven_user_context
   end
 
-  def validate_active_storage_attachment(record, attribute, file, valid_types)
+  def validate_active_storage_attachment(record, attribute, file, valid_types, required)
     # TODO: Remove this method and all calls when active storage validations are added (expected in Rails 6)
-    return true if file.blank?
+    return !required if file.blank?
 
     if !file.content_type.in?(valid_types)
       flash[:error] = "File could not be saved. File type must be one of #{valid_types.join(', ')}."
