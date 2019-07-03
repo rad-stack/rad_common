fe08ad269ac9b15e99cb26ce9c55b41027e897c9
Eric Bundy <elbzero@gmail.com>
2019-07-03 12:58:02 -0400

    we are now generating request tests instead of controller tests so no need for this anymore

--- spec/dummy/lib/templates/rspec/scaffold/controller_spec.rb
+++
@@ -1,1 +0,0 @@
-# TODO remove this file. For now we have to generate this blank file until rspec allows us to skip generating controller tests in favor of request tests https://github.com/rspec/rspec-rails/issues/2056
