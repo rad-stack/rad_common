853fae25bca2f54139c4b901f763deb832789248
Eric Bundy <elbzero@gmail.com>
2019-07-09 06:47:00 -0400

    fix a couple more tests

--- spec/requests/security_roles_spec.rb
+++ spec/requests/security_roles_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'Security roles', type: :request do
     describe 'with invalid params' do
       it 're-renders the new template' do
         post '/security_roles', params: { security_role: invalid_attributes }
-        expect(response).to render_template('new')
+        expect(response).not_to redirect_to(SecurityRole.last)
       end
     end
   end
@@ -59,7 +59,7 @@ RSpec.describe 'Security roles', type: :request do
     describe 'with invalid params' do
       it 're-renders the edit template' do
         put "/security_roles/#{security_role.id}", params: { security_role: invalid_attributes }
-        expect(response).to render_template('edit')
+        expect(response).not_to redirect_to(security_role)
       end
     end
   end
