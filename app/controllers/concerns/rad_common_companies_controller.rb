f5f12f90a92e0feb128c7aaee8def74638367b2a
garyfoster <gary@radicalbear.com>
2019-08-10 05:51:10 -0600

    Allow interactive global validity to be disabled

--- app/controllers/concerns/rad_common_companies_controller.rb
+++ app/controllers/concerns/rad_common_companies_controller.rb
@@ -2,7 +2,12 @@ module RadCommonCompaniesController
   extend ActiveSupport::Concern
 
   def global_validity_check
+    unless Rails.application.config.global_validity_enable_interactive
+      raise 'interactive global validity check not enabled'
+    end
+
     authorize_action_for @company
+
     GlobalValidityJob.perform_later(current_user)
 
     flash[:success] = "We're checking the validity of all of your company's data. You will get an email with "\
