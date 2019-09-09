846d2bdbe015c68490bea609ae47c5660bf18cb5
Caleb Tocco <calebtocco@hotmail.com>
2019-09-09 12:26:05 -0400

    actually fi

--- app/services/attachment_count_checker.rb
+++ app/services/attachment_count_checker.rb
@@ -2,7 +2,8 @@ class AttachmentCountChecker
   def self.check_attachment(model_name, paperclip_attachment_name, active_storage_attachment_name)
     model = model_name.constantize
     paperclip_count = model.where("#{paperclip_attachment_name}_file_name is not null").count
-    active_storage_count = model.joins("#{active_storage_attachment_name}_attachment").count
+    attachment_joins_name = "#{active_storage_attachment_name}_attachment".to_sym
+    active_storage_count = model.joins(attachment_joins_name).count
     message = "#{model_name} \nPaperclip Count (#{paperclip_attachment_name}) - #{paperclip_count} \n"
     message += "Active Storage Count (#{active_storage_attachment_name}) - #{active_storage_count}"
 
