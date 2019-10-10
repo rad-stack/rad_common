cc0cb6b64dd71d1aef4518c061dce9530e716adc
Caleb Tocco <calebtocco@hotmail.com>
2019-10-10 15:45:41 -0400

    rename

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -1,7 +1,7 @@
 module Attachable
   include Hashable
 
-  def encoded_url(variant)
+  def permanent_attachment_url(variant)
     protocol = Rails.env.production? ? 'https' : 'http'
     host = Rails.env.production? ? ENV.fetch('HOST_NAME') : 'localhost:3000'
     "#{protocol}://#{host}/rad_common/attachments/#{encoded_id}/download?class=#{self.class}&variant=#{variant}"
