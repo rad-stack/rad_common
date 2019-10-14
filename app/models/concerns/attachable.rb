a8934e7a53bea3820ea5e3f3679cea74c9201ef2
Caleb Tocco <calebtocco@hotmail.com>
2019-10-14 07:53:29 -0400

    fix

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -2,7 +2,7 @@ module Attachable
   def permanent_attachment_url(attachment, variant)
     protocol = Rails.env.production? ? 'https' : 'http'
     host = Rails.env.production? ? ENV.fetch('HOST_NAME') : 'localhost:3000'
-    attachment_id = Hashable.hashids.encode(send(attachment))
+    attachment_id = Hashable.hashids.encode(send(attachment).id)
     "#{protocol}://#{host}/common/attachments/#{attachment_id}/#{variant}"
   end
 end
