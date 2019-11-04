a949367269c1706f087d47262f808bc27c446d7d
Caleb Tocco <calebtocco@hotmail.com>
2019-11-04 16:28:16 -0500

    attachment route specs

---
+++ spec/requests/attachments_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Attachments', type: :request do
+  let!(:division) { create :division }
+
+  before { division.logo.attach(io: Rack::Test::UploadedFile.new(Rails.root.join('app', 'assets', 'images', 'app_logo.png')), filename: 'logo.png') }
+
+  context 'permanent attachment url' do
+    it 'allows navigation' do
+      get AttachmentUrlGenerator.permanent_attachment_url(division.logo)
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'permanent attachment variant url' do
+    it 'allows navigation' do
+      get AttachmentUrlGenerator.permanent_attachment_variant_url(division, :logo_variant)
+      expect(response.status).to eq 200
+    end
+  end
+end
