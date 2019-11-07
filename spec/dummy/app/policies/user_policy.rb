32dc64ab4c857a2e738f5fc9162b66e3d88e0d23
garyfoster <gary@radicalbear.com>
2019-11-07 09:37:12 -0500

    Use alias

--- spec/dummy/app/policies/user_policy.rb
+++ spec/dummy/app/policies/user_policy.rb
@@ -1,8 +1,5 @@
 class UserPolicy < ApplicationPolicy
-  def audit_search?
-    audit?
-  end
-
+  alias audit_search? audit?
   alias audit_by? audit?
   alias resend_invitation? create?
   alias confirm? update?
