84d9a0a46d1f0d500becc2df236202ce764f5aa6
garyfoster <gary@radicalbear.com>
2019-11-08 11:26:54 -0500

    Fix sorting

--- spec/dummy/app/services/division_search.rb
+++ spec/dummy/app/services/division_search.rb
@@ -1,6 +1,12 @@
 class DivisionSearch
   def self.search(query, params, current_user)
     filters = [{ input_label: 'Owner', column: :owner_id, options: User.by_name }]
-    RadCommon::Search.new(query: query, filters: filters, current_user: current_user, params: params)
+    sorts = [{ column: 'name', label: 'Name', direction: 'desc' }]
+
+    RadCommon::Search.new(query: query,
+                          filters: filters,
+                          sort_columns: sorts,
+                          current_user: current_user,
+                          params: params)
   end
 end
