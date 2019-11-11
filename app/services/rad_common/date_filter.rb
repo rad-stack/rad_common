9feda9afbb9d40b46eb587d0858c31e90ffea31e
Caleb Tocco <calebtocco@hotmail.com>
2019-11-11 13:55:04 -0500

    facepalm

--- app/services/rad_common/date_filter.rb
+++ app/services/rad_common/date_filter.rb
@@ -13,8 +13,9 @@ module RadCommon
       start_at = start_at_value(params)
       end_at = end_at_value(params)
 
-      results.where("#{column} >= ?", start_at) if start_at.present?
-      results.where("#{column} <= ?", end_at) if end_at.present?
+      results = results.where("#{column} >= ?", start_at) if start_at.present?
+      results = results.where("#{column} <= ?", end_at) if end_at.present?
+      results
     end
 
     def searchable_name
