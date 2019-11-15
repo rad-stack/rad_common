8f62c50402100c1f9947cc026600d0d90ef7d2e2
Eric Bundy <elbzero@gmail.com>
2019-11-15 15:04:25 -0500

    add some search specs

--- spec/services/rad_common/search_spec.rb
+++ spec/services/rad_common/search_spec.rb
@@ -1,28 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe RadCommon::Search, type: :service do
+
+  let(:user) { create(:user) }
+
   describe 'results' do
     subject do
-      described_class.new(query: Division,
+      described_class.new(query: query,
                           filters: filters,
                           current_user: user,
-                          params: {}).results
+                          params: ActionController::Parameters.new(search: params)).results
     end
 
     let!(:division) { create :division }
 
     context 'when using a like filter' do
-      it 'filters results'
+      let!(:role_1) { create(:security_role, name: 'foo') }
+      let!(:role_2) { create(:security_role, name: 'bar') }
+      let(:query) { SecurityRole }
+      let(:filters) { [{ column: :name, type: RadCommon::LikeFilter }] }
+      let(:params) { { name_like: 'foo' } }
+
+      it 'filters results' do
+        expect(subject).to include role_1
+        expect(subject).to_not include role_2
+      end
     end
 
     context 'when using a date filter' do
-      it 'filters results'
+      let(:query) { User }
+      let(:user_1) { create :user, confirmed_at: 2.days.ago }
+      let(:user_2) { create :user, confirmed_at: 3.days.from_now }
+      let(:filters) { [{ column: :confirmed_at, type: RadCommon::DateFilter }] }
+      let(:params) {
+        { confirmed_at_start: (3.days.ago).strftime('%Y-%m-%d'),
+          confirmed_at_end: DateTime.current.strftime('%Y-%m-%d') } }
+
+      it 'filters results' do
+        expect(subject).to include user_1
+        expect(subject).not_to include user_2
+      end
     end
 
     context 'when using a select filter' do
-      let(:filters) { [{ input_label: 'Owner', column: :owner_id, options: User.by_name }] }
+      let(:query) { User }
+      let(:user_active) { create :user, user_status: UserStatus.default_active_status }
+      let(:user_pending) { create :user, user_status: UserStatus.default_pending_status }
+      let(:filters) { [{ column: :user_status_id, options: UserStatus.by_id }] }
+      let(:params) { { user_status_id: UserStatus.default_active_status.id } }
 
-      it 'filters results'
+      it 'filters results' do
+        expect(subject).to include user_active
+        expect(subject).not_to include user_pending
+      end
+    end
+
+    describe 'authorized' do
+      let(:query) { Division }
+      let(:filters) { [{ input_label: 'Owner', column: :owner_id, options: User.by_name }] }
+      let(:params) { {} }
 
       context 'when authorized' do
         let(:user) { create :admin }
