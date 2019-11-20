53075ea2322755db8635e2f925ad1f20a8a069fd
garyfoster <gary@radicalbear.com>
2019-11-20 07:48:14 -0500

    Enhance dummy data to test this feature

--- spec/dummy/app/controllers/divisions_controller.rb
+++ spec/dummy/app/controllers/divisions_controller.rb
@@ -4,7 +4,7 @@ class DivisionsController < ApplicationController
 
   def index
     authorize Division
-    @divisions = Division.all.page(params[:page])
+    @divisions = Division.sorted.page(params[:page])
   end
 
   def show; end
@@ -67,6 +67,8 @@ class DivisionsController < ApplicationController
     end
 
     def division_attachments_and_types
-      [{ attr: :logo, types: ['image/png'] }, { attr: :avatar, types: ['image/jpeg'] }]
+      [{ attr: :logo, types: ['image/png'] },
+       { attr: :avatar, types: ['image/jpeg'] },
+       { attr: :attachment, types: ['image/jpeg', 'text/plain'] }]
     end
 end
