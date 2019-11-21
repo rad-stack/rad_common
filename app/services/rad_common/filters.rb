9a6939670b9bdd9c02f30f140539aeb6b2f2539c
Eric Bundy <elbzero@gmail.com>
2019-11-21 09:07:49 -0500

    these need to be in rad_common module

--- app/services/rad_common/filters.rb
+++ app/services/rad_common/filters.rb
@@ -1,46 +1,48 @@
-class Filters
-  attr_reader :filters
+module RadCommon
+  class Filters
+    attr_reader :filters
 
-  def initialize(filters:, search: search)
-    @search = search
-    @filters = build_search_filters(filters)
-    @filter_hash = Hash[@filters.collect { |f| [f.searchable_name, f] }]
+    def initialize(filters:, search: search)
+      @search = search
+      @filters = build_search_filters(filters)
+      @filter_hash = Hash[@filters.collect { |f| [f.searchable_name, f] }]
 
-    setup_filter_defaults
-  end
+      setup_filter_defaults
+    end
 
-  def apply_filtering(results)
-    results = results.authorized(current_user)
-    results = apply_joins(results)
-    apply_filters(results)
-  end
+    def apply_filtering(results)
+      results = results.authorized(current_user)
+      results = apply_joins(results)
+      apply_filters(results)
+    end
 
-  private
+    private
 
-    def apply_filters(results)
-      @filters.each do |filter|
-        results = filter.apply_filter(@results, @search.search_params)
-        results ||= @results
+      def apply_filters(results)
+        @filters.each do |filter|
+          results = filter.apply_filter(@results, @search.search_params)
+          results ||= @results
+        end
+
+        results
       end
 
-      results
-    end
+      def apply_joins(results)
+        results.joins(joins)
+      end
 
-    def apply_joins(results)
-      results.joins(joins)
-    end
-
-    def build_search_filters(filters)
-      filters.map do |filter|
-        if filter.has_key? :type
-          filter[:type].send(:new, filter)
-        else
-          SearchFilter.new(filter)
+      def build_search_filters(filters)
+        filters.map do |filter|
+          if filter.has_key? :type
+            filter[:type].send(:new, filter)
+          else
+            SearchFilter.new(filter)
+          end
         end
       end
-    end
 
-    def joins
-      filters.select { |f| f.respond_to? :joins }.map(&:joins).compact
-    end
+      def joins
+        filters.select { |f| f.respond_to? :joins }.map(&:joins).compact
+      end
+  end
 end
