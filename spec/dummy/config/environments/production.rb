9da1021e9677816335a931da8335d8dec51e24e3
garyfoster <gary@radicalbear.com>
2019-12-13 05:45:25 -0500

    Reduce logging

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -46,7 +46,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
+
+  # reduce log verbosity (sql statements)
+  config.active_record.logger = nil
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
