eddb4df3edb7582da216f84614cac260c8ac6a0f
Caleb Tocco <calebtocco@hotmail.com>
2019-12-23 06:58:10 -0500

    remove from dummy

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -1,8 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   include SchemaValidations
 
-  class_attribute :schema_validations_loaded
-
-  before_validation :load_schema_validations
   self.abstract_class = true
 end
