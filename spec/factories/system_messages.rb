43aa6fd11c2531266957e8d50eff7cc71a08e8c2
Eric Bundy <elbzero@gmail.com>
2020-01-29 11:24:48 -0500

    add action_text to schema

--- spec/factories/system_messages.rb
+++ spec/factories/system_messages.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     send_to { :all_users }
     association :user
     message { Faker::Movies::StarWars.quote }
+    email_message_body { Faker::Movies::StarWars.quote }
     message_type { 'email' }
   end
 end
