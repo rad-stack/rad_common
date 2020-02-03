f30995899afd6a70e6387a1d9e30d196bbd8ee55
garyfoster <gary@radicalbear.com>
2020-02-03 11:24:04 -0500

    Comment for workaround

--- app/inputs/rich_text_area_input.rb
+++ app/inputs/rich_text_area_input.rb
@@ -1,4 +1,7 @@
 class RichTextAreaInput < SimpleForm::Inputs::Base
+  # TODO: using workaround in here: https://github.com/heartcombo/simple_form/issues/1638
+  # https://swell.radicalbear.com/tasks/31625
+
   def input(wrapper_options = nil)
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
