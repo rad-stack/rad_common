cf8c45a4b111d598358b2b75b6189fc327902295
Jose <jgerena1111@gmail.com>
2020-02-19 12:33:50 -0500

    added content_type_icon spec

---
+++ spec/models/content_type_icon_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe ContentTypeIcon, type: :model do
+  let(:content_type) { 'application/msword' }
+
+  context 'when content is a known type' do
+    it 'assigns an icon' do
+      
+    end
+  end
+end
