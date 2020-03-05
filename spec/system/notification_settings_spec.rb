1cf6352fb083b5810daabff9698be89341c627f8
Eric Bundy <elbzero@gmail.com>
2020-03-05 11:37:49 -0500

    add some specs for dynamic updating of settings

--- spec/system/notification_settings_spec.rb
+++ spec/system/notification_settings_spec.rb
@@ -18,6 +18,19 @@ RSpec.describe 'NotificationSettings', type: :system do
         visit '/rad_common/notification_settings'
         expect(page).to have_content(notification_type.description)
       end
+
+      it 'updates the settings successfully without save button', :js do
+        visit '/rad_common/notification_settings'
+        expect(NotificationSetting.count).to eq 0
+        page.check('notification_setting[feed]')
+        expect(NotificationSetting.count).to eq 1
+      end
+
+      it 'displays error message when updating without button', :js do
+        visit '/rad_common/notification_settings'
+        page.uncheck('notification_setting[email]')
+        expect(page.body).to include 'The setting could not be saved: Enabled requires one of email/sms/feed be turned on'
+      end
     end
 
     context 'user' do
