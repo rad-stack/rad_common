46bd51beb973bab954af6874ae0b23c7daef51ee
garyfoster <gary@radicalbear.com>
2020-03-20 06:25:31 -0400

    Tighten notification saving

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -7,15 +7,21 @@ class NotificationSettingsController < ApplicationController
   end
 
   def create
-    notification_type_id = permitted_params[:notification_type_id]
+    notification_type = NotificationType.find_by(id: permitted_params[:notification_type_id])
 
-    notification_setting = NotificationSetting.find_or_initialize_by(notification_type_id: notification_type_id,
+    if notification_type.blank?
+      skip_authorization
+      render json: { error: 'Invalid parameters' }, status: :unprocessable_entity
+      return
+    end
+
+    notification_setting = NotificationSetting.find_or_initialize_by(notification_type: notification_type,
                                                                      user_id: permitted_params[:user_id])
 
     notification_setting.enabled = permitted_params[:enabled]
-    notification_setting.email = permitted_params[:email]
-    notification_setting.feed = permitted_params[:feed]
-    notification_setting.sms = permitted_params[:sms] if RadicalTwilio.twilio_enabled?
+    notification_setting.email = permitted_params[:email] if notification_type.email_enabled?
+    notification_setting.feed = permitted_params[:feed] if notification_type.feed_enabled?
+    notification_setting.sms = permitted_params[:sms] if RadicalTwilio.twilio_enabled? && notification_type.sms_enabled?
 
     authorize notification_setting
 
