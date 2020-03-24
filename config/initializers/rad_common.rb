4380f2ab696325c1c954caad77093787b4e99721
garyfoster <gary@radicalbear.com>
2020-03-24 07:04:24 -0400

    Luxurious CRM inflection

--- config/initializers/rad_common.rb
+++ config/initializers/rad_common.rb
@@ -3,6 +3,7 @@ require "#{Gem::Specification.find_by_name('rad_common').gem_dir}/lib/core_exten
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'SMS'
   inflect.acronym 'PDF'
+  inflect.acronym 'CRM'
 end
 
 ActiveRecord::Base.prepend CoreExtensions::ActiveRecord::Base::SchemaValidations
