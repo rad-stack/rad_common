7e9cd797bf36771a668e5ff8db1dd770608ca32a
garyfoster <gary@radicalbear.com>
2020-04-27 12:15:46 -0400

    Consistent config

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  Rails.application.routes.default_url_options[:host] = RadCommon::AppInfo.new.host_name
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
