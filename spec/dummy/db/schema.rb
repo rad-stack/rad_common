2e488055aa8595a8457019c142a59ae1ccddbe44
Eric Bundy <elbzero@gmail.com>
2020-06-15 15:35:36 -0400

    add some more specs for datetime_column?

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_154123) do
+ActiveRecord::Schema.define(version: 2020_06_15_193248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_154123) do
     t.string "timezone"
     t.decimal "hourly_rate", precision: 8, scale: 2, default: "0.0", null: false
     t.string "additional_info"
+    t.date "date_established"
     t.index ["name"], name: "index_divisions_on_name", unique: true, where: "(division_status = 0)"
     t.index ["owner_id"], name: "index_divisions_on_owner_id"
   end
--- spec/services/rad_common/date_filter_spec.rb
+++ spec/services/rad_common/date_filter_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RadCommon::DateFilter, type: :service do
   describe 'datetime_column?' do
     subject(:date_filter) do
-      described_class.new(column: :created_at, type: RadCommon::DateFilter).send(:datetime_column?, results)
+      described_class.new(column: column, type: RadCommon::DateFilter).send(:datetime_column?, results)
     end
 
     let(:results) { Division.all }
@@ -12,6 +12,13 @@ RSpec.describe RadCommon::DateFilter, type: :service do
       let(:column) { :created_at }
       it 'detects datetime correctly' do
         expect(subject).to eq true
+      end
+    end
+
+    context 'date_established' do
+      let(:column) { :date_established }
+      it 'detects datetime correctly' do
+        expect(subject).to eq false
       end
     end
   end
