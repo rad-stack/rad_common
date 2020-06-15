4c1327fb912d85dd45da342595d435f5dd65e637
Eric Bundy <elbzero@gmail.com>
2020-06-15 15:36:04 -0400

    rubocop

--- spec/services/rad_common/date_filter_spec.rb
+++ spec/services/rad_common/date_filter_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe RadCommon::DateFilter, type: :service do
   describe 'datetime_column?' do
     subject(:date_filter) do
-      described_class.new(column: column, type: RadCommon::DateFilter).send(:datetime_column?, results)
+      described_class.new(column: column, type: described_class).send(:datetime_column?, results)
     end
 
     let(:results) { Division.all }
 
     context 'created_at' do
       let(:column) { :created_at }
+
       it 'detects datetime correctly' do
         expect(subject).to eq true
       end
@@ -17,6 +18,7 @@ RSpec.describe RadCommon::DateFilter, type: :service do
 
     context 'date_established' do
       let(:column) { :date_established }
+
       it 'detects datetime correctly' do
         expect(subject).to eq false
       end
