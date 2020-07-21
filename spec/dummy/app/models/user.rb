020c38c02e928ad93ce8698740c9f9be7ef2be2a
garyfoster <gary@radicalbear.com>
2020-07-21 12:06:09 -0400

    Format

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  devise :authy_authenticatable, :database_authenticatable, :registerable, :confirmable, :recoverable,
-         :trackable, :timeoutable, :lockable, :password_archivable, :password_expirable,
-         :secure_validatable, :expirable, :invitable
+  devise :authy_authenticatable, :database_authenticatable, :registerable, :confirmable, :recoverable, :trackable,
+         :timeoutable, :lockable, :password_archivable, :password_expirable, :secure_validatable, :expirable, :invitable
 
   include RadbearUser
   include RadAuthy
