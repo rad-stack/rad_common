bcc3410c2117256613d97aa406eaa70a34311fd9
garyfoster <gary@radicalbear.com>
2020-08-11 10:17:54 -0400

    UI for sign in activity

--- app/models/login_activity.rb
+++ app/models/login_activity.rb
@@ -1,3 +1,4 @@
 class LoginActivity < ApplicationRecord
   belongs_to :user, polymorphic: true, optional: true
+  scope :recent_first, -> { order(id: :desc) }
 end
---
+++ spec/rad_common/system/login_activities_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'LoginActivities', type: :system do
+  let(:user) { create :admin }
+
+  before { login_as user, scope: :user }
+
+  describe 'index' do
+    it 'displays the login activities' do
+      visit '/rad_common/login_activities'
+      expect(page).to have_content(user.email)
+    end
+  end
+end
