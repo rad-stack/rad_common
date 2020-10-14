f5fd5b3aa7c128ca29ee28c3e9238ad81d35e049
jgerena11 <jgerena1111@gmail.com>
2020-10-14 16:36:07 -0400

    added login activity search filters

---
+++ app/services/login_activity_search.rb
@@ -0,0 +1,44 @@
+class LoginActivitySearch < RadCommon::Search
+  def initialize(params, current_user)
+    @current_user = current_user
+
+    super(query: LoginActivity,
+          filters: filters_def,
+          sort_columns: sort_columns_def,
+          params: params,
+          current_user: current_user)
+  end
+
+  private
+
+  def filters_def
+    [{ start_input_label: 'Start Date',
+       end_input_label: 'End Date',
+       column: :created_at,
+       type: RadCommon::DateFilter},
+     { input_label: 'Email',
+       column: 'users.email',
+       options:
+          [['Users', user_emails],
+           ['Clients', client_emails],
+           ['Inactive', inactive_user_emails],
+       blank_value_label: 'All Emails',
+       grouped: true }]
+  end
+
+  def all_emails
+    Pundit.policy_scope!(current_user, User).active.select(:email).order(email: :asc)
+  end
+
+  def client_emails
+    Pundit.policy_scope!(current_user, User).active.external.select(:email).order(email: :asc)
+  end
+
+  def user_emails
+    Pundit.policy_scope!(current_user, User).active.internal.select(:email).order(email: :asc)
+  end
+
+  def inactive_user_emails
+    Pundit.policy_scope!(current_user, User).inactive.select(:email.order(email: :asc)
+  end
+end
