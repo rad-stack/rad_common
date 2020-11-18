2b07efa14f9f2d575a2c9ad8841014ad1ad7a196
garyfoster <gary@radicalbear.com>
2020-11-18 13:18:00 -0500

    Yearly

--- app/models/system_usage.rb
+++ app/models/system_usage.rb
@@ -7,7 +7,7 @@ class SystemUsage
       when 'yearly'
         { start: today.advance(years: -item).beginning_of_year.beginning_of_day,
           end: today.advance(years: -item).end_of_year.end_of_day,
-          label: today.advance(years: -item).beginning_of_year.strftime('%B, %Y') }
+          label: today.advance(years: -item).beginning_of_year.strftime('%Y') }
       when 'monthly'
         { start: today.advance(months: -item).beginning_of_month.beginning_of_day,
           end: today.advance(months: -item).end_of_month.end_of_day,
