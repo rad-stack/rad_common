8b5a71cb44b8498a35f82b0ea53ea9503cf0205b
garyfoster <gary@radicalbear.com>
2020-12-02 14:35:24 -0500

    Refactor

---
+++ app/services/sms_sender.rb
@@ -0,0 +1,75 @@
+class SMSSender
+  attr_accessor :message, :recipients, :current_user, :media_url, :error_lines, :from
+
+  def initialize(message, recipients, current_user, media_url)
+    self.message = message
+    self.recipients = recipients
+    self.current_user = current_user
+    self.media_url = media_url
+
+    self.error_lines = []
+    self.from = RadicalTwilio.next_phone_number
+  end
+
+  def send!
+    recipients.each do |user_id|
+      user = User.find(user_id)
+
+      if user.mobile_phone.blank?
+        error_lines.push "The message to #{user} failed: they do not have a mobile phone number."
+        next
+      end
+
+      send_message user
+    end
+
+    handle_problems
+  end
+
+  private
+
+    def send_message(user)
+      RadicalRetry.perform_request do
+        if media_url.present?
+          RadicalTwilio.send_mms from: from, to: "+1#{user.mobile_phone}", message: message, media_url: media_url
+        else
+          # TODO: why not +1 like above?
+          RadicalTwilio.send_sms from: from, to: user.mobile_phone, message: message
+        end
+      end
+    rescue Twilio::REST::RestError => e
+      # TODO: will this be captured even though that exception is handled by rad retry?
+      error_lines.push "The message to #{user} failed: #{e.description}."
+    end
+
+    def handle_problems
+      # TODO: what to do if no current user?
+      return if error_count.zero? || current_user.blank?
+
+      RadbearMailer.simple_message(current_user, error_subject, error_body).deliver_later
+    end
+
+    def recipient_count
+      recipients.count
+    end
+
+    def error_count
+      error_lines.count
+    end
+
+    def error_subject
+      if recipient_count == 1
+        "Error Sending #{message_type} Message"
+      else
+        "Error Sending #{message_type} Messages"
+      end
+    end
+
+    def message_type
+      media_url.present? ? 'MMS' : 'SMS'
+    end
+
+    def error_body
+      error_lines.join('/n')
+    end
+end
---
+++ spec/services/sms_sender_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe SMSSender, type: :service do
+  let(:user_1) { create :user }
+  let(:user_2) { create :user }
+  let(:user_3) { create :user }
+  let(:message) { 'test message' }
+  let(:recipients) { [user_1.id, user_2.id, user_3.id] }
+  let(:current_user) { create :user }
+  let(:media_url) { nil }
+  let(:sms_sender) { described_class.new(message, recipients, current_user, media_url) }
+
+  before do
+    allow(RadicalTwilio).to receive(:send_sms).and_return(nil)
+    allow(RadicalTwilio).to receive(:send_mms).and_return(nil)
+  end
+
+  context 'without errors' do
+    subject { sms_sender.send! }
+
+    it { is_expected.to be_nil }
+  end
+
+  context 'with errors' do
+    subject { mail&.body&.encoded }
+
+    let(:user_2) { create :user, mobile_phone: nil }
+    let(:mail) { ActionMailer::Base.deliveries.last }
+
+    before do
+      ActionMailer::Base.deliveries = []
+      sms_sender.send!
+    end
+
+    context 'with one error' do
+      it { is_expected.to include user_2.to_s }
+    end
+
+    context 'with two errors' do
+      let(:user_3) { create :user, mobile_phone: nil }
+
+      it { is_expected.to include user_2.to_s }
+      it { is_expected.to include user_3.to_s }
+    end
+
+    context 'with one recipient' do
+      let(:recipients) { [user_2.id] }
+
+      it { is_expected.to include user_2.to_s }
+    end
+  end
+end
