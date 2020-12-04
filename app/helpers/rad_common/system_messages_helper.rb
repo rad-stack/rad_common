461ad0ed3526c9d2a45ad65457f70035c7a08476
garyfoster <gary@radicalbear.com>
2020-12-04 11:35:18 -0500

    Prepare for SMS revamp

--- app/helpers/rad_common/system_messages_helper.rb
+++ app/helpers/rad_common/system_messages_helper.rb
@@ -2,7 +2,7 @@ module RadCommon
   module SystemMessagesHelper
     def system_message_title(system_message)
       "System Message Sent to #{enum_to_translated_option(SystemMessage, :send_to, system_message.send_to)} "\
-      "on #{format_datetime(@system_message.created_at)}"
+      "on #{format_datetime(system_message.created_at)}"
     end
 
     def message_type_options
@@ -15,6 +15,12 @@ module RadCommon
       options = options_for_enum(SystemMessage, :send_to)
       options.reject! { |option| option.include?('client_users') } unless RadCommon.external_users
       options
+    end
+
+    def system_message_show_data(system_message)
+      [{ label: 'Message', value: system_message.html_message },
+       { label: 'Message Type',
+         value: enum_to_translated_option(SystemMessage, :message_type, system_message.message_type) }]
     end
   end
 end
---
+++ spec/services/radical_twilio_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe RadicalTwilio, type: :service do
+  describe 'current_phone_number' do
+    context 'with 3 numbers' do
+      before { ENV['TWILIO_PHONE_NUMBERS'] = '9044444444,9044444445,9044444446' }
+
+      it 'returns the numbers' do
+        expect(described_class.next_phone_number).to eq '9044444444'
+        expect(described_class.next_phone_number).to eq '9044444445'
+        expect(described_class.next_phone_number).to eq '9044444446'
+        expect(described_class.next_phone_number).to eq '9044444444'
+        expect(described_class.next_phone_number).to eq '9044444445'
+      end
+    end
+
+    context 'with 1 number' do
+      before { ENV['TWILIO_PHONE_NUMBERS'] = '9044444444' }
+
+      it 'returns the number' do
+        expect(described_class.next_phone_number).to eq '9044444444'
+        expect(described_class.next_phone_number).to eq '9044444444'
+        expect(described_class.next_phone_number).to eq '9044444444'
+      end
+    end
+
+    context 'with no numbers' do
+      before { ENV['TWILIO_PHONE_NUMBERS'] = nil }
+
+      it 'returns nil' do
+        expect(described_class.next_phone_number).to eq nil
+      end
+    end
+  end
+end
