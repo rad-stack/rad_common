475046b7e4548caf130168f65a3dadaad476fc01
jgerena11 <jgerena1111@gmail.com>
2021-03-08 15:09:36 -0500

    tweaks

--- spec/rad_common/models/system_usage_spec.rb
+++ spec/rad_common/models/system_usage_spec.rb
@@ -6,14 +6,15 @@ describe SystemUsage, type: :model do
   let(:system_usage) { described_class.new(params, user) }
 
   describe 'date_column_ranges' do
-    subject(:date_ranges) { system_usage.date_column_ranges.pluck(:start) }
+    subject(:date_ranges) { system_usage.date_column_ranges.map { |range| [range[:start], range[:end]] } }
 
-    let(:day) { Time.current.beginning_of_week(:sunday).beginning_of_day }
-    let(:expected_dates) do
-      [day - 3.weeks,
-       day - 2.weeks,
-       day - 1.week,
-       day]
+    let(:first_day_of_week) { Time.current.beginning_of_week(:sunday).beginning_of_day }
+    let(:last_day_of_week) { Time.current.end_of_week(:sunday).end_of_day }
+    let(:expected_date_ranges) do
+      [[first_day_of_week - 3.weeks, last_day_of_week - 3.weeks],
+       [first_day_of_week - 2.weeks, last_day_of_week - 2.weeks],
+       [first_day_of_week - 1.week, last_day_of_week - 1.week],
+       [first_day_of_week, last_day_of_week]]
     end
 
     before do
@@ -21,7 +22,7 @@ describe SystemUsage, type: :model do
     end
 
     it 'returns list of weekly date ranges' do
-      expect(date_ranges).to eq(expected_dates)
+      expect(date_ranges).to eq(expected_date_ranges)
     end
   end
 end
