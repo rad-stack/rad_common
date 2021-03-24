bd9306c64c00d73e0fffd8aecaae637426f00273
garyfoster <gary@radicalbear.com>
2021-03-24 13:24:08 -0600

    Test quotes linting

--- app/models/twilio_log.rb
+++ app/models/twilio_log.rb
@@ -1,4 +1,4 @@
 class TwilioLog < ApplicationRecord
-  belongs_to :from_user, class_name: 'User'
+  belongs_to :from_user, class_name: "User"
   belongs_to :to_user, class_name: 'User', optional: true
 end
