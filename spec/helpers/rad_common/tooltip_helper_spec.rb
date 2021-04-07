8f25efefe7756d6a29d7dfb87de7e617df5a4722
garyfoster <gary@radicalbear.com>
2021-04-07 05:26:41 -0400

    Allow changing icon on tooltip

--- spec/helpers/rad_common/tooltip_helper_spec.rb
+++ spec/helpers/rad_common/tooltip_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe RadCommon::TooltipHelper do
   let(:html_tag) { 'span' }
   let(:title) { 'Test' }
-  let(:placement) { 'bottom' }
+  let(:icon) { 'fa-heartbeat' }
 
   describe '#icon_tooltip' do
     it 'correctly formats to an icon tooltip' do
@@ -27,6 +27,11 @@ describe RadCommon::TooltipHelper do
       tooltip = icon_tooltip(html_tag, title)
       expect(tooltip).to include('data-placement="top"')
     end
+
+    it 'changes the tooltip icon with 3rd argument' do
+      tooltip = icon_tooltip(html_tag, title, icon)
+      expect(tooltip).to include('fa-heartbeat')
+    end
   end
 
   describe '#tooltip' do
@@ -43,11 +48,6 @@ describe RadCommon::TooltipHelper do
     it 'displays nothing if title is empty string' do
       tooltip = tooltip(html_tag, '')
       expect(tooltip).to eq(nil)
-    end
-
-    it 'changes the tooltip placement with 3rd argument' do
-      tooltip = tooltip(html_tag, title, placement)
-      expect(tooltip).to include('data-placement="bottom"')
     end
   end
 end
