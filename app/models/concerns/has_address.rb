80657eff5a32b385da89eb2ae796a81b56005cac
garyfoster <gary@radicalbear.com>
2021-04-08 07:54:34 -0400

    Handle blanks

--- app/models/concerns/has_address.rb
+++ app/models/concerns/has_address.rb
@@ -2,15 +2,15 @@ module HasAddress
   extend ActiveSupport::Concern
 
   def full_address
-    [address_line_1, address_line_2].compact.join(', ')
+    [address_line_1, address_line_2].compact.join(', ').presence
   end
 
   def address_line_1
-    [address_1, address_2].compact.join(', ')
+    [address_1, address_2].compact.join(', ').presence
   end
 
   def address_line_2
-    city_state = [city, state].compact.join(', ')
-    [city_state, zipcode].compact.join(' ')
+    city_state = [city, state].compact.join(', ').presence
+    [city_state, zipcode].compact.join(' ').presence
   end
 end
