8e33f2ae7a061cdeabd9cf351f72ee51f0945bec
Eric Bundy <elbzero@gmail.com>
2021-04-21 15:56:14 -0400

    another hound fix

--- spec/services/phone_number_formatter_spec.rb
+++ spec/services/phone_number_formatter_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumberFormatter, type: :service do
-
   describe 'format' do
     it 'formats phone numbers correctly' do
       expect(described_class.format('(999)999-9999')).to eq '(999) 999-9999'
