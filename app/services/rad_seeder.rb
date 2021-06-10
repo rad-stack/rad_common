901dee2411a0681f07617c2528412523f9f97c6e
garyfoster <gary@radicalbear.com>
2021-06-10 14:46:03 -0400

    Refactor seeder

--- app/services/rad_seeder.rb
+++ app/services/rad_seeder.rb
@@ -4,12 +4,12 @@ class RadSeeder
   def seed!
     display_log 'seeding base tables'
 
-    FactoryBot.create(:company, email: seeded_user_config.first[:email]) if Company.count.zero?
     SecurityRole.seed_items if SecurityRole.count.zero?
     UserStatus.seed_items if UserStatus.count.zero?
 
     seed_notification_types
     seed_users
+    seed_company
 
     @users = User.all
   end
@@ -29,6 +29,9 @@ class RadSeeder
     def seed_users
       return unless (Rails.env.development? || Company.staging?) && User.count.zero?
 
+      # TODO: is this needed?
+      # User.skip_callback(:save, :after, :notify_user_approved, raise: false)
+
       display_log 'seeding users'
 
       seeded_user_config.each do |seeded_user|
@@ -36,7 +39,7 @@ class RadSeeder
                        user_status: user_status,
                        first_name: seeded_user[:first_name],
                        last_name: seeded_user[:last_name],
-                       security_roles: [role_by_name(seeded_user[:security_role])] }
+                       security_roles: user_security_roles(seeded_user) }
 
         if seeded_user[:trait].present?
           FactoryBot.create seeded_user[:factory], seeded_user[:trait], attributes
@@ -44,6 +47,12 @@ class RadSeeder
           FactoryBot.create seeded_user[:factory], attributes
         end
       end
+    end
+
+    def seed_company
+      return if Company.count.positive?
+
+      FactoryBot.create :company, email: seeded_user_config.first[:email]
     end
 
     def seeded_user_config
@@ -54,6 +63,12 @@ class RadSeeder
       return JSON.parse(ENV.fetch('SEEDED_USERS')) if Rails.env.production?
 
       JSON.parse(File.read(Rails.root.join('db/seeded_users.json')))
+    end
+
+    def user_security_roles(seeded_user)
+      return [] if seeded_user[:security_role].blank?
+
+      [role_by_name(seeded_user[:security_role])]
     end
 
     def user_role
@@ -78,5 +93,13 @@ class RadSeeder
 
     def user_status
       UserStatus.default_active_status
+    end
+
+    def month_begin
+      Date.current.beginning_of_month
+    end
+
+    def random_date(from_date, to_date)
+      Faker::Date.unique.between(from: from_date, to: to_date)
     end
 end
