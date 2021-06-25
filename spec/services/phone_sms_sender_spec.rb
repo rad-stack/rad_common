6427c370a88885d937f25347afa28e3f28907f94
garyfoster <gary@radicalbear.com>
2021-06-25 06:09:17 -0400

    Comment

--- spec/services/phone_sms_sender_spec.rb
+++ spec/services/phone_sms_sender_spec.rb
@@ -57,6 +57,7 @@ RSpec.describe PhoneSMSSender, type: :service do
           create :twilio_log, opt_out_message_sent: true, to_number: RadicalTwilio.human_to_twilio_format(mobile_phone)
         end
 
+        # TODO: this has been intermittently failing on codeship
         it { is_expected.to eq "I'm taking your surfboard" }
       end
     end
