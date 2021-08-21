85d358b83770334280c7ef3d408dea241b257dbb
garyfoster <gary@radicalbear.com>
2021-08-21 13:09:55 -0600

    Refactor

--- app/helpers/companies_helper.rb
+++
@@ -1,5 +0,0 @@
-module CompaniesHelper
-  def edit_company_title(company)
-    safe_join(['Editing Company: ', link_to(company.name.presence || 'Missing', '/rad_common/company')])
-  end
-end
--- app/helpers/rad_common/companies_helper.rb
+++ app/helpers/rad_common/companies_helper.rb
@@ -8,5 +8,9 @@ module RadCommon
        :timezone,
        :validity_checked_at]
     end
+
+    def edit_company_title(company)
+      safe_join(['Editing Company: ', link_to(company.name.presence || 'Missing', '/rad_common/company')])
+    end
   end
 end
