4791956b67e0e506af69ebed1def010ab0fd0b0f
garyfoster <gary@radicalbear.com>
2021-12-10 12:15:24 -0500

    More config stuff

--- app/services/rad_common_routes.rb
+++ app/services/rad_common_routes.rb
@@ -1,23 +1,31 @@
 module RadCommonRoutes
   def self.extended(router)
     router.instance_exec do
-      devise_for :users,
-                 path: 'auth',
-                 controllers: { confirmations: 'users/confirmations', invitations: 'users/invitations' }
+      devise_controllers = { confirmations: 'users/confirmations',
+                             devise_authy: 'users/devise_authy',
+                             invitations: 'users/invitations' }
 
-      resources :users do
-        member do
-          put :resend_invitation
-          put :confirm
-          put :reset_authy
+      devise_paths = { verify_authy: '/verify-token',
+                       enable_authy: '/enable-two-factor',
+                       verify_authy_installation: '/verify-installation' }
+
+      devise_for :users, path: 'auth', controllers: devise_controllers, path_names: devise_paths
+
+      authenticate :user, ->(u) { u.internal? } do
+        resources :users do
+          member do
+            put :resend_invitation
+            put :confirm
+            put :reset_authy
+          end
         end
-      end
 
-      resources :security_roles do
-        get :permission, on: :collection
-      end
+        resources :security_roles do
+          get :permission, on: :collection
+        end
 
-      resources :user_security_roles, only: :show
+        resources :user_security_roles, only: :show
+      end
 
       authenticate :user, ->(u) { u.admin? } do
         mount Sidekiq::Web => '/sidekiq'
