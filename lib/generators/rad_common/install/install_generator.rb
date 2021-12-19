94d599c65552edaaed78af6b95bb682409022c6a
garyfoster <gary@radicalbear.com>
2021-12-19 11:21:52 -0500

    Misc

--- lib/generators/rad_common/install/install_generator.rb
+++ lib/generators/rad_common/install/install_generator.rb
@@ -26,6 +26,7 @@ module RadCommon
         copy_file '../../../../../spec/dummy/babel.config.js', 'babel.config.js'
         copy_file '../gitignore.txt', '.gitignore'
         copy_file '../rails_helper.rb', 'spec/rails_helper.rb'
+        copy_file '../../../../../spec/dummy/public/403.html', 'public/403.html'
 
         # code style config
         copy_file '../../../../../.haml-lint.yml', '.haml-lint.yml'
