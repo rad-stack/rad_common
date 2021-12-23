ed6f15ecfda00ed9960520d9233394bbda44b1c2
garyfoster <gary@radicalbear.com>
2021-12-23 07:55:34 -0500

    roofing changes

--- app/controllers/rad_common/sendgrid_controller.rb
+++ app/controllers/rad_common/sendgrid_controller.rb
@@ -1,6 +1,6 @@
 module RadCommon
   class SendgridController < ApplicationController
-    skip_before_action :authenticate_user!
+    # TODO: skip_before_action :authenticate_user!
 
     def email_error
       skip_authorization
