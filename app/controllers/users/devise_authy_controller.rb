a8a5f1c5f79c722eda4d6a01fac1be831ef982eb
garyfoster <gary@radicalbear.com>
2022-02-02 13:15:15 -0500

    Allow authy to work without SMS (just use the authy app)

--- app/controllers/users/devise_authy_controller.rb
+++ app/controllers/users/devise_authy_controller.rb
@@ -1,15 +1,16 @@
 module Users
   class DeviseAuthyController < Devise::DeviseAuthyController
     def GET_verify_authy
-      return unless @resource.authy_sms?
+      if @resource.authy_sms?
+        response = Authy::API.request_sms(id: @resource.authy_id, force: true)
 
-      response = Authy::API.request_sms(id: @resource.authy_id, force: true)
-
-      if response.ok?
-        flash[:info] = 'A verification token has been texted to you.'
-      else
-        flash[:alert] = 'The verification code failed to send. Please click "Resend Text".'
+        if response.ok?
+          flash[:info] = 'A verification token has been texted to you.'
+        else
+          flash[:alert] = 'The verification code failed to send. Please click "Resend Text".'
+        end
       end
+
       super
     end
   end
