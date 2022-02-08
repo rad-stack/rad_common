be8afa6bbae7d0488dbb346a58bca4f6d18baab7
garyfoster <gary@radicalbear.com>
2022-02-08 06:31:51 -0500

    Fix spec

--- spec/models/concerns/duplicate_fixable_spec.rb
+++ spec/models/concerns/duplicate_fixable_spec.rb
@@ -3,10 +3,26 @@ require 'rails_helper'
 describe DuplicateFixable, type: :model do
   let(:phone_number) { create :phone_number }
   let(:email) { Faker::Internet.email }
-  let(:first_name) { Faker::Name.first_name }
-  let(:last_name) { Faker::Name.last_name }
-  let!(:attorney_1) { create :attorney, attorney_attributes.merge(company_name: 'ABC') }
-  let!(:attorney_2) { create :attorney, attorney_attributes.merge(company_name: 'XYZ') }
+  let(:first_name) { 'John' }
+  let(:last_name) { 'Smith' }
+
+  let!(:attorney_1) do
+    create :attorney, attorney_1_attributes.merge(company_name: 'ABC',
+                                                  address_1: 'Xxxx',
+                                                  address_2: nil,
+                                                  city: 'Xxxx',
+                                                  state: 'XX',
+                                                  zipcode: '11111')
+  end
+
+  let!(:attorney_2) do
+    create :attorney, attorney_2_attributes.merge(company_name: 'XYZ',
+                                                  address_1: 'Yyyy',
+                                                  address_2: nil,
+                                                  city: 'Yyyy',
+                                                  state: 'YY',
+                                                  zipcode: '22222')
+  end
 
   describe 'process_duplicates' do
     subject { attorney_1.duplicate.score }
@@ -17,17 +33,26 @@ describe DuplicateFixable, type: :model do
     end
 
     context 'when matching only on additional items' do
-      let(:attorney_attributes) { { phone_number: phone_number, email: email } }
+      let(:attorney_1_attributes) do
+        { phone_number: phone_number, email: email, first_name: 'Xxxx', last_name: 'Tttt' }
+      end
+
+      let(:attorney_2_attributes) do
+        { phone_number: phone_number, email: email, first_name: 'Yyyy', last_name: 'Ssss' }
+      end
 
       it { is_expected.to eq 32 }
     end
 
     context 'when matching on standard plus additional items' do
-      let(:attorney_attributes) do
+      let(:attorney_1_attributes) do
         { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
       end
 
-      # TODO: this fails intermittently
+      let(:attorney_2_attributes) do
+        { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
+      end
+
       it { is_expected.to eq 46 }
     end
   end
@@ -36,13 +61,23 @@ describe DuplicateFixable, type: :model do
     subject { attorney_1.send(:all_matches) }
 
     context 'when matching only on additional items' do
-      let(:attorney_attributes) { { phone_number: phone_number, email: email } }
+      let(:attorney_1_attributes) do
+        { phone_number: phone_number, email: email, first_name: 'Xxxx', last_name: 'Tttt' }
+      end
+
+      let(:attorney_2_attributes) do
+        { phone_number: phone_number, email: email, first_name: 'Yyyy', last_name: 'Ssss' }
+      end
 
       it { is_expected.to include attorney_2.id }
     end
 
     context 'when matching on standard plus additional items' do
-      let(:attorney_attributes) do
+      let(:attorney_1_attributes) do
+        { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
+      end
+
+      let(:attorney_2_attributes) do
         { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
       end
 
