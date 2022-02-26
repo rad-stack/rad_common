39dd366bbc7bb06a50002c0221f823b200f13218
garyfoster <gary@radicalbear.com>
2022-02-26 14:41:10 -0500

    Don't change permission labels

--- spec/helpers/rad_common/security_role_helper_spec.rb
+++ spec/helpers/rad_common/security_role_helper_spec.rb
@@ -32,11 +32,11 @@ describe RadCommon::SecurityRoleHelper do
   describe '#humanized_permission_fields' do
     it 'returns human-readable permission fields' do
       expect(helper.humanized_permission_fields).to eq(
-        [{ label: 'Administration', permission: 'admin' },
-         { label: 'Create Divisions', permission: 'create_division' },
-         { label: 'Delete Divisions', permission: 'delete_division' },
-         { label: 'View Divisions', permission: 'read_division' },
-         { label: 'Update Divisions', permission: 'update_division' }]
+        [{ label: 'Admin', permission: 'admin' },
+         { label: 'Create Division', permission: 'create_division' },
+         { label: 'Delete Division', permission: 'delete_division' },
+         { label: 'Read Division', permission: 'read_division' },
+         { label: 'Update Division', permission: 'update_division' }]
       )
     end
   end
