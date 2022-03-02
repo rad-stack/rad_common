49bfc446b18df95c0604e31a11cbf79e38d5ded3
garyfoster <gary@radicalbear.com>
2022-03-02 10:05:25 -0500

    Force opt out message on outgoing SMS

--- app/jobs/phone_sms_sender_job.rb
+++ app/jobs/phone_sms_sender_job.rb
@@ -1,7 +1,7 @@
 class PhoneSMSSenderJob < ApplicationJob
   queue_as :default
 
-  def perform(message, from_user_id, mobile_phone, media_url)
-    PhoneSMSSender.new(message, from_user_id, mobile_phone, media_url).send!
+  def perform(message, from_user_id, mobile_phone, media_url, force_opt_out)
+    PhoneSMSSender.new(message, from_user_id, mobile_phone, media_url, force_opt_out).send!
   end
 end
