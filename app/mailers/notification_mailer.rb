8f6ad0a121a118f3b861bde6f0a440851a5585c0
garyfoster <gary@radicalbear.com>
2022-03-10 07:30:14 -0500

    Show opt out notification settings link on notification sourced emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,13 +11,10 @@ class NotificationMailer < RadbearMailer
                       button_text: auto_approve ? 'Review' : 'Review & Approve',
                       button_url: edit_user_url(user) }
 
-    @recipient = User.where(id: recipients)
-    to_address = @recipient.map(&:formatted_email)
-
     @message = "#{user} has signed up on #{app_name(user)}"
     @message += auto_approve ? '.' : ' and is awaiting approval.'
 
-    mail(to: to_address, subject: "New User on #{app_name(user)}")
+    send_notification_mail recipients, "New User on #{app_name(user)}"
   end
 
   def user_was_approved(recipients, user_and_approver)
@@ -30,31 +27,29 @@ class NotificationMailer < RadbearMailer
 
     approved_by_name = (approver ? approver.to_s : 'an admin')
 
-    @recipient = User.where(id: recipients)
-    to_address = @recipient.map(&:formatted_email)
-
     @message = "#{user} was approved by #{approved_by_name} on #{RadicalConfig.app_name!}."
-    mail(to: to_address, subject: "User Was Approved on #{RadicalConfig.app_name!}")
+    send_notification_mail recipients, "User Was Approved on #{RadicalConfig.app_name!}"
   end
 
   def global_validity(recipients, problems)
-    @recipient = User.where(id: recipients)
-    to_address = @recipient.map(&:formatted_email)
-
     @problems = problems
     @message = "There #{@problems.count == 1 ? 'is' : 'are'} #{pluralize(@problems.count, 'invalid record')}."
 
-    mail(to: to_address, subject: "Invalid data in #{RadicalConfig.app_name!}")
+    send_notification_mail recipients, "Invalid data in #{RadicalConfig.app_name!}"
   end
 
   def global_validity_ran_long(recipients, run_stats)
-    @recipient = User.where(id: recipients)
-    to_address = @recipient.map(&:formatted_email)
-
     @run_stats = run_stats
     total_time = Time.at((@run_stats.sum { |item| item[:run_seconds] })).utc.strftime('%H:%M:%S')
     @message = "The Global Validity task took #{total_time} to complete, which is beyond the configured timeout."
 
-    mail(to: to_address, subject: "Global Validity in #{RadicalConfig.app_name!} Ran Long")
+    send_notification_mail recipients, "Global Validity in #{RadicalConfig.app_name!} Ran Long"
   end
+
+  private
+
+    def send_notification_mail(recipients, subject)
+      @recipient = User.where(id: recipients)
+      mail to: @recipient.map(&:formatted_email), subject: subject
+    end
 end
--- spec/dummy/app/mailers/division_mailer.rb
+++ spec/dummy/app/mailers/division_mailer.rb
@@ -1,10 +1,6 @@
 class DivisionMailer < NotificationMailer
   def division_updated(recipients, division)
-    @recipient = User.where(id: recipients)
-    to_address = @recipient.map(&:formatted_email)
-
     @division = division
-
-    mail(to: to_address, subject: 'Division Updated')
+    send_notification_mail recipients, 'Division Updated'
   end
 end
