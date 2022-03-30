5b1b9004b09c06fadf631aa480f410b5747c2556
Eric Bundy <elbzero@gmail.com>
2022-03-30 16:19:51 -0400

    lint fix

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -1,10 +1,10 @@
 const { environment } = require('@rails/webpacker');
 
 environment.config.merge({
-    externals: {
-        jquery: 'jQuery',
-        moment: 'moment'
-    }
+  externals: {
+    jquery: 'jQuery',
+    moment: 'moment'
+  }
 });
 
 module.exports = environment;
