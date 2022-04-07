1731fb149c15c4c9c1770646b2ec593b4da5274c
garyfoster <gary@radicalbear.com>
2022-04-07 06:58:31 -0400

    Dry out the export button pattern

--- app/helpers/rad_common/users_helper.rb
+++ app/helpers/rad_common/users_helper.rb
@@ -136,14 +136,6 @@ module RadCommon
               data: { confirm: 'Are you sure?' }
     end
 
-    def export_users_button
-      return unless policy(User.new).export?
-
-      link_to(icon(:file, 'Export to File'),
-              export_users_path(params.permit!.to_h.merge(format: :csv)),
-              class: 'btn btn-secondary btn-sm')
-    end
-
     def user_client_actions(user)
       [add_user_client_action(user, false)]
     end
