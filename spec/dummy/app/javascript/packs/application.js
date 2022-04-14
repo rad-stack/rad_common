bf66d89ae67f8fea7050e991c0f67e7c363346cc
garyfoster <gary@radicalbear.com>
2022-04-14 07:42:12 -0400

    Misc

--- spec/dummy/app/javascript/packs/application.js
+++ spec/dummy/app/javascript/packs/application.js
@@ -1,30 +1,15 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+require('trix');
+require('@rails/actiontext');
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import('bootstrap-select/dist/css/bootstrap-select.min.css');
-import('jquery-ui/themes/base/all.css');
-import('tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css');
+import 'bootstrap-select/dist/css/bootstrap-select.min.css';
+import 'jquery-ui/themes/base/all.css';
+import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css';
 import 'rad_common_js/src/css/rad_common/jquery-ui-overrides.scss';
 import 'rad_common_js/src/css/bootstrap_and_overrides.scss';
 import 'rad_common_js/src/css/main.css.scss';
 
-require("trix");
-require("@rails/actiontext");
-import {RadCommon} from "rad_common_js/src/radCommon";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import {RadCommon} from 'rad_common_js/src/radCommon';
 RadCommon.setup();
