1378dcd5c36c201cad0d095d4933febd47fb87e9
garyfoster <gary@radicalbear.com>
2022-06-14 11:57:11 -0600

    Updates

--- spec/factories/rad_common/clients.rb
+++ spec/factories/rad_common/clients.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :client, class: RadCommon::AppInfo.new.client_model_class_name do
     name { Faker::Company.name }
     valid_user_domains { ['abc.com'] }
+    email { 'joe@abc.com' }
   end
 end
