01777457ea2f893af3f58a7831152b14d8ae02df
garyfoster <gary@radicalbear.com>
2022-07-20 12:23:44 -0400

    Address validation with Lob

--- spec/dummy/db/migrate/20220719162246_address_validation.rb
+++ spec/dummy/db/migrate/20220719162246_address_validation.rb
@@ -3,5 +3,9 @@ class AddressValidation < ActiveRecord::Migration[6.1]
     add_column :companies, :bypass_address_validation, :boolean, null: false, default: false
     add_column :companies, :address_problems, :text
     add_column :companies, :lob_requests_made, :integer, null: false, default: 0
+
+    # add these 2 fields to any additional tables that require address validations
+    # add_column :foobars, :bypass_address_validation, :boolean, null: false, default: false
+    # add_column :foobars, :address_problems, :text
   end
 end
