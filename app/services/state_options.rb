3a4c2c88508199f0dda464044bc9d3a56e522916
garyfoster <gary@radicalbear.com>
2022-08-02 07:42:32 -0400

    Updates

--- app/services/state_options.rb
+++ app/services/state_options.rb
@@ -1,4 +1,58 @@
 class StateOptions
+  STATE_DATA = {
+    'Alabama' => 'AL',
+    'Alaska' => 'AK',
+    'Arizona' => 'AZ',
+    'Arkansas' => 'AR',
+    'California' => 'CA',
+    'Colorado' => 'CO',
+    'Connecticut' => 'CT',
+    'Delaware' => 'DE',
+    'Florida' => 'FL',
+    'Georgia' => 'GA',
+    'Hawaii' => 'HI',
+    'Idaho' => 'ID',
+    'Illinois' => 'IL',
+    'Indiana' => 'IN',
+    'Iowa' => 'IA',
+    'Kansas' => 'KS',
+    'Kentucky' => 'KY',
+    'Louisiana' => 'LA',
+    'Maine' => 'ME',
+    'Maryland' => 'MD',
+    'Massachusetts' => 'MA',
+    'Michigan' => 'MI',
+    'Minnesota' => 'MN',
+    'Mississippi' => 'MS',
+    'Missouri' => 'MO',
+    'Montana' => 'MT',
+    'Nebraska' => 'NE',
+    'Nevada' => 'NV',
+    'New Hampshire' => 'NH',
+    'New Jersey' => 'NJ',
+    'New Mexico' => 'NM',
+    'New York' => 'NY',
+    'North Carolina' => 'NC',
+    'North Dakota' => 'ND',
+    'Ohio' => 'OH',
+    'Oklahoma' => 'OK',
+    'Oregon' => 'OR',
+    'Pennsylvania' => 'PA',
+    'Puerto Rico' => 'PR',
+    'Rhode Island' => 'RI',
+    'South Carolina' => 'SC',
+    'South Dakota' => 'SD',
+    'Tennessee' => 'TN',
+    'Texas' => 'TX',
+    'Utah' => 'UT',
+    'Vermont' => 'VT',
+    'Virginia' => 'VA',
+    'Washington' => 'WA',
+    'West Virginia' => 'WV',
+    'Wisconsin' => 'WI',
+    'Wyoming' => 'WY'
+  }.freeze
+
   class << self
     def options
       active_states.map { |state, abbreviation| [state, abbreviation] }
@@ -23,67 +77,11 @@ class StateOptions
     private
 
       def active_states
-        data.select { |_state, abbreviation| active.include?(abbreviation) }
+        STATE_DATA.select { |_state, abbreviation| active.include?(abbreviation) }
       end
 
       def active
-        data.values
-      end
-
-      def data
-        {
-          'Alabama' => 'AL',
-          'Alaska' => 'AK',
-          'Arizona' => 'AZ',
-          'Arkansas' => 'AR',
-          'California' => 'CA',
-          'Colorado' => 'CO',
-          'Connecticut' => 'CT',
-          'Delaware' => 'DE',
-          'Florida' => 'FL',
-          'Georgia' => 'GA',
-          'Hawaii' => 'HI',
-          'Idaho' => 'ID',
-          'Illinois' => 'IL',
-          'Indiana' => 'IN',
-          'Iowa' => 'IA',
-          'Kansas' => 'KS',
-          'Kentucky' => 'KY',
-          'Louisiana' => 'LA',
-          'Maine' => 'ME',
-          'Maryland' => 'MD',
-          'Massachusetts' => 'MA',
-          'Michigan' => 'MI',
-          'Minnesota' => 'MN',
-          'Mississippi' => 'MS',
-          'Missouri' => 'MO',
-          'Montana' => 'MT',
-          'Nebraska' => 'NE',
-          'Nevada' => 'NV',
-          'New Hampshire' => 'NH',
-          'New Jersey' => 'NJ',
-          'New Mexico' => 'NM',
-          'New York' => 'NY',
-          'North Carolina' => 'NC',
-          'North Dakota' => 'ND',
-          'Ohio' => 'OH',
-          'Oklahoma' => 'OK',
-          'Oregon' => 'OR',
-          'Pennsylvania' => 'PA',
-          'Puerto Rico' => 'PR',
-          'Rhode Island' => 'RI',
-          'South Carolina' => 'SC',
-          'South Dakota' => 'SD',
-          'Tennessee' => 'TN',
-          'Texas' => 'TX',
-          'Utah' => 'UT',
-          'Vermont' => 'VT',
-          'Virginia' => 'VA',
-          'Washington' => 'WA',
-          'West Virginia' => 'WV',
-          'Wisconsin' => 'WI',
-          'Wyoming' => 'WY'
-        }
+        STATE_DATA.values
       end
   end
 end
