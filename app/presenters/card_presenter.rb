3464f110882ccc1da69631e883c330496390f242
garyfoster <gary@radicalbear.com>
2022-08-02 10:29:12 -0400

    Misc UX enhancements

--- app/presenters/card_presenter.rb
+++ app/presenters/card_presenter.rb
@@ -180,9 +180,7 @@ class CardPresenter
                                          id: "new_#{downcased_object_class}_link"))
     end
 
-    if show_index_button? && %w[show edit update new create].include?(action_name) && current_user &&
-       Pundit.policy!(current_user, check_policy_klass).index?
-
+    if show_index_button?
       actions.push(@view_context.link_to(@view_context.icon(:list, "View #{object_label_plural}"),
                                          index_path,
                                          class: 'btn btn-secondary btn-sm'))
@@ -288,12 +286,14 @@ class CardPresenter
 
     def show_index_button?
       return false if no_index_button
+      return false unless %w[show edit update new create].include?(action_name)
+      return false unless current_user && Pundit.policy!(current_user, check_policy_klass).index?
       return false if no_records?
 
       true
     end
 
     def no_records?
-      current_user && Pundit.policy_scope!(current_user, klass).count.zero?
+      Pundit.policy_scope!(current_user, klass).count.zero?
     end
 end
