f774f025bda8da3dfdbb9df80b346e27d4e27b68
garyfoster <gary@radicalbear.com>
2022-08-09 08:14:44 -0400

    Standardize address validation

--- app/services/state_options.rb
+++ app/services/state_options.rb
@@ -51,7 +51,20 @@ class StateOptions
     'Washington' => 'WA',
     'West Virginia' => 'WV',
     'Wisconsin' => 'WI',
-    'Wyoming' => 'WY'
+    'Wyoming' => 'WY',
+    'Alberta' => 'AB',
+    'British Columbia' => 'BC',
+    'Manitoba' => 'MB',
+    'New Brunswick' => 'NB',
+    'Newfoundland and Labrador' => 'NL',
+    'Northwest Territories' => 'NT',
+    'Nova Scotia' => 'NS',
+    'Nunavut' => 'NU',
+    'Ontario' => 'ON',
+    'Prince Edward Island' => 'PE',
+    'Quebec' => 'QC',
+    'Saskatchewan' => 'SK',
+    'Yukon' => 'YT'
   }.freeze
 
   class << self
