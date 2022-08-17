abd9c0458b49811b744d230a7617eb0020ebb22f
Eric Bundy <elbzero@gmail.com>
2022-08-17 14:42:14 -0400

    add rad chosen

--- spec/dummy/app/javascript/packs/application.js
+++ spec/dummy/app/javascript/packs/application.js
@@ -1,6 +1,5 @@
 require('trix');
 require('@rails/actiontext');
-require('chosen-js/chosen.jquery.min');
 
 import 'trix/dist/trix.css';
 import 'rad_common_js/src/css/actiontext.scss';
@@ -11,17 +10,8 @@ import 'rad_common_js/src/css/rad_common/jquery-ui-overrides.scss';
 import 'rad_common_js/src/css/bootstrap_and_overrides.scss';
 import 'rad_common_js/src/css/main.css.scss';
 import '@fortawesome/fontawesome-free/css/all';
-import 'chosen-js/chosen.min.css';
 
 import { RadCommon } from 'rad_common_js/src/radCommon';
 RadCommon.setup();
-
-$(document).ready(function() {
-    $('.rad-chosen').chosen({
-        allow_single_deselect: true,
-        no_results_text: 'No matching records found',
-        width: '100%'
-    });
-})
 
 import './app_specific.js'
