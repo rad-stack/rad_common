1eaa293044401c15c5fb60f76aa6b35f8334ea38
Eric Bundy <elbzero@gmail.com>
2022-09-12 09:34:36 -0400

    tweak this to use label for now
    Might need to discuss more later

--- app/services/onboarding_next_step.rb
+++ app/services/onboarding_next_step.rb
@@ -2,7 +2,7 @@ class OnboardingNextStep
   attr_reader :label, :path
 
   def initialize(label:, path:)
-    @label = "Setup Incomplete, Next Step #{label}"
+    @label = label
     @path = path
   end
 end
