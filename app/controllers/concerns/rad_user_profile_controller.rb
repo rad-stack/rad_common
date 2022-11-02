d7eab44032efe1d1b6b89e7f70716c4cfdf60ad2
Eric Bundy <elbzero@gmail.com>
2022-11-02 09:18:55 -0400

    rad user profile controller concern

---
+++ app/controllers/concerns/rad_user_profile_controller.rb
@@ -0,0 +1,33 @@
+module RadUserProfileController
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_user, only: %i[show edit update]
+    before_action :set_onboarded, only: :update
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @user.profile_entered = true
+
+    if @user.update(permitted_params)
+      update_redirect
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+    def set_user
+      @user = User.find(params[:id])
+      authorize @user, policy_class: UserProfilePolicy
+    end
+
+    def update_redirect
+      redirect_to user_profile_path(@user), notice: 'Your profile was successfully updated.'
+    end
+end
