90bc622a968c6d9ca607fb0c8fbcd2abd60f80bd
Caleb Tocco <calebtocco@hotmail.com>
2022-11-04 07:14:50 -0400

    Leverage Global Autocomplete

--- app/inputs/searchable_association_input.rb
+++ app/inputs/searchable_association_input.rb
@@ -1,22 +1,27 @@
 class SearchableAssociationInput < SimpleForm::Inputs::CollectionSelectInput
   MAX_DROPDOWN_SIZE = 300
 
+  delegate :current_user, to: :template
+
   def input(wrapper_options = nil)
-    search_field = options[:search_only] || options[:collection].size > MAX_DROPDOWN_SIZE
-    options[:collection] = search_field ? [object.public_send(reflection.name)].compact : options[:collection]
+    options[:collection] = search_only? ? [object.public_send(reflection.name)].compact : searchable_collection
     label_method, value_method = detect_collection_methods
-    input_html_options[:class].push(:selectpicker)
-    opts = input_html_options.merge('data-live-search' => true)
-    if search_field
-      opts.merge!(search_options)
-      options[:include_blank] = false
-    end
-    merged_input_options = merge_wrapper_options(opts, wrapper_options)
+    add_default_options
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     @builder.collection_select(attribute_name, collection, value_method, label_method,
                                input_options, merged_input_options)
   end
 
   private
+
+    def add_default_options
+      input_html_options[:class].push(:selectpicker)
+      input_html_options.merge!('data-live-search' => true)
+      return unless search_only?
+
+      input_html_options.merge!(search_options)
+      options[:include_blank] = false
+    end
 
     def search_options
       {
@@ -28,5 +33,28 @@ class SearchableAssociationInput < SimpleForm::Inputs::CollectionSelectInput
           'term' => '{{{q}}}'
         }.to_json
       }
+    end
+
+    def records
+      @records ||= global_autocomplete.base_autocomplete_collection(global_autocomplete.current_scope)
+    end
+
+    def searchable_collection
+      lambda {
+        global_autocomplete.global_autocomplete_result.map do |r|
+          [r[:label], r[:id], { 'data-subtext' => options[:show_subtext] ? r[:columns] : nil }]
+        end
+      }
+    end
+
+    def search_only?
+      options[:search_only] || records.size > MAX_DROPDOWN_SIZE
+    end
+
+    def global_autocomplete
+      @global_autocomplete ||=
+        GlobalAutocomplete.new({ limit: MAX_DROPDOWN_SIZE, global_search_scope: options[:search_scope] },
+                               GlobalSearch.new(current_user).scopes,
+                               current_user)
     end
 end
