2404b2eecc507a6b1c4dd9d90a04c6a37af6d894
Eric Bundy <elbzero@gmail.com>
2022-11-28 08:53:14 -0500

    try

--- lib/generators/rad_common/install/merge_package_json.rb
+++ lib/generators/rad_common/install/merge_package_json.rb
@@ -3,17 +3,15 @@ class MergePackageJson
     binding.pry
     if File.exists? 'custom-dependencies.json'
       custom_dependencies = JSON.parse(File.read('custom-dependencies.json'))
-      package = JSON.parse(File.read('../../../../../spec/dummy/package.json'))
+      package_file = File.expand_path(find_in_source_paths('../../../../../spec/dummy/package.json'))
+      package = JSON.parse(File.read(package_file))
 
       dependencies = package['dependencies']
       dependencies = dependencies.merge(custom_dependencies)
       package['dependencies'] = dependencies
-      binding.pry
       File.write('package.json', JSON.pretty_generate(package))
     else
-      binding.pry
       copy_file '../../../../../spec/dummy/package.json', 'package.json'
-      binding.pry
     end
   end
 end
