9b5b8b1f80a6f399a3ca8c519ef254e915ae9ca7
Eric Bundy <elbzero@gmail.com>
2022-11-28 09:00:34 -0500

    cleanup

--- lib/generators/rad_common/install/install_generator.rb
+++ lib/generators/rad_common/install/install_generator.rb
@@ -214,20 +214,15 @@ end
       protected
 
         def merge_package_json
-          binding.pry
           if File.exists? 'custom-dependencies.json'
             custom_dependencies = JSON.parse(File.read('custom-dependencies.json'))
             package = JSON.parse(File.read('../../../../../spec/dummy/package.json'))
-
             dependencies = package['dependencies']
             dependencies = dependencies.merge(custom_dependencies)
             package['dependencies'] = dependencies
-            binding.pry
             File.write('package.json', JSON.pretty_generate(package))
           else
-            binding.pry
             copy_file '../../../../../spec/dummy/package.json', 'package.json'
-            binding.pry
           end
         end
 
