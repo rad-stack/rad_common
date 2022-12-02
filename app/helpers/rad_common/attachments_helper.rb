fcd7c99c7618b9440c4472cbc4834ecc140bcf25
Eric Bundy <elbzero@gmail.com>
2022-12-02 14:14:53 -0500

    more attachment helper refactoring

--- app/helpers/rad_common/attachments_helper.rb
+++ app/helpers/rad_common/attachments_helper.rb
@@ -1,32 +1,41 @@
 module RadCommon
   module AttachmentsHelper
     def render_attachment_layout(attachment_name:, record:,
-                                 override_label: nil, show_filename: nil, no_delete_button: nil)
+                                 override_label: nil, show_filename: nil, no_delete_button: nil,
+                                 override_path: nil, new_tab: nil)
       return unless record.persisted?
 
       attachment = record.send(attachment_name)
       return unless !attachment.respond_to?(:attached?) || attachment.attached?
 
+      render_attachment_object(attachment: attachment, record: record, override_label: override_label,
+                               show_filename: show_filename, no_delete_button: no_delete_button,
+                               override_path: override_path, new_tab: new_tab)
+    end
+
+    def render_attachment_object(attachment:, record:,
+                                 override_label:, show_filename:, no_delete_button:, override_path:, new_tab:)
       label_override = override_label.presence
       filename_label = show_filename ? " #{attachment.filename}" : nil
       no_delete = no_delete_button || !policy(record).update?
-
-      render 'layouts/attachment_object', label_override: label_override,
-                                          filename_label: filename_label, no_delete: no_delete
+      render 'layouts/attachment_object', attachment: attachment, label_override: label_override,
+                                          filename_label: filename_label, no_delete: no_delete,
+                                          override_path: override_path, new_tab: new_tab
     end
 
-    def render_attachments_layouts(attachment_name:, record:,
-                                   override_label: nil, show_filename: nil, no_delete_button: nil)
+    def render_attachments_layout(attachment_name:, record:,
+                                  override_label: nil, show_filename: nil, no_delete_button: nil,
+                                  override_path: nil, new_tab: nil)
       return unless record.persisted?
 
       attachments = record.send(attachment_name)
       return if attachments.blank?
 
-      record.send(attachment_name).each do |attachment|
-        render 'layouts/attachment_object', attachment: attachment, record: record,
-                                            override_label: override_label, show_filename: show_filename,
-                                            no_delete_button: no_delete_button
-      end
+      record.send(attachment_name).map { |attachment|
+        render_attachment_object(attachment: attachment, record: record, override_label: override_label,
+                                 show_filename: show_filename, no_delete_button: no_delete_button,
+                                 override_path: override_path, new_tab: new_tab)
+      }.join(' ').html_safe
     end
   end
 end
