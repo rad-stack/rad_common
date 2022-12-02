01d9461a4f1de545496669878982d0637df99f85
Eric Bundy <elbzero@gmail.com>
2022-12-02 14:28:18 -0500

    more attachment refactoring

--- app/helpers/rad_common/attachments_helper.rb
+++ app/helpers/rad_common/attachments_helper.rb
@@ -23,6 +23,38 @@ module RadCommon
                                           override_path: override_path, new_tab: new_tab
     end
 
+    def render_attachment_image(attachment:, override_path:, new_tab:, label_override:, no_delete:)
+      link = (override_path.presence || url_for(attachment))
+      target = new_tab ? '_blank' : nil
+
+      attachment_label =
+        label_override.presence || image_tag(attachment, class: 'img-fluid rounded', id: 'attachment_layout')
+
+      [link_to(attachment_label, link, target: target, class: 'text-wrap'),
+       render_delete_attachment_link(attachment: attachment, no_delete: no_delete)].join(' ').html_safe
+    end
+
+    def render_delete_attachment_link(attachment:, no_delete:)
+      return if no_delete
+
+      link_to tag.i('', class: 'fa fa-times'),
+              RadCommon::Engine.routes.url_helpers.attachment_path(attachment.id),
+              method: :delete,
+              data: { confirm: 'Are you sure? Attachment cannot be recovered.' },
+              class: 'btn btn-danger ml-5'
+    end
+
+    def render_attachment_link(attachment:, no_delete:, label_override:, filename_label:)
+      default_label = tag.i('', class: "fa #{RadCommon::ContentTypeIcon.new(attachment.content_type)}") + filename_label
+      attachment_label = label_override.presence || default_label
+      [link_to(attachment_label,
+               url_for(attachment),
+               target: '_blank',
+               rel: :noopener,
+               class: 'btn btn-secondary attachment-link text-wrap'),
+       render_delete_attachment_link(attachment: attachment, no_delete: no_delete)].join(' ').html_safe
+    end
+
     def render_attachments_layout(attachment_name:, record:,
                                   override_label: nil, show_filename: nil, no_delete_button: nil,
                                   override_path: nil, new_tab: nil)
