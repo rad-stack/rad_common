1b6f631b94ec7a8bec16b369553d922bc7c1df29
garyfoster <gary@radicalbear.com>
2022-12-07 08:23:42 -0500

    Testing

--- app/models/notifications/twilio_error_threshold_passed_notification.rb
+++ app/models/notifications/twilio_error_threshold_passed_notification.rb
@@ -1,12 +1,18 @@
 module Notifications
   class TwilioErrorThresholdPassedNotification < ::NotificationType
     def mailer_message
-      "Twilio Error Threshold has been passed. #{payload * 100}% of messages have failed to deliver. " \
+      "Twilio Error Threshold has been passed. #{formatted_percent}% of messages have failed to deliver. " \
         'Check twilio logs for more details'
     end
 
     def subject_record
       nil
     end
+
+    private
+
+      def formatted_percent
+        ActiveSupport::NumberHelper.number_to_percentage(payload * 100)
+      end
   end
 end
