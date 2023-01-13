f523bea0029e4c2041a2b9e5079ccf2dc2d36445
garyfoster <gary@radicalbear.com>
2023-01-13 09:33:16 -0500

    Receive sendgrid statuses for suppressions and faild emails

--- app/services/rad_common_routes.rb
+++ app/services/rad_common_routes.rb
@@ -47,6 +47,7 @@ module RadCommonRoutes
 
       resources :user_profiles, only: %i[show edit update] if RadicalConfig.user_profiles?
       resources :twilio_statuses, only: :create
+      resources :sendgrid_statuses, only: :create
 
       get 'contact_us', to: 'pages#contact_us'
       get 'terms', to: 'pages#terms'
