dd9d437cfd54e1ae6979954c3c433785333c72eb
garyfoster <gary@radicalbear.com>
2023-01-13 09:42:32 -0500

    Receive sendgrid statuses for suppressions and faild emails

---
+++ app/controllers/sendgrid_statuses_controller.rb
@@ -0,0 +1,10 @@
+class SendgridStatusesController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
+  def create
+    skip_authorization
+
+    RadbearMailer.simple_message(User.active.admin.first, 'Sendgrid Status', params.to_s).deliver_later
+  end
+end
