c3ee77708f88580e4a953fe3f8ea373cb97ce131
garyfoster <gary@radicalbear.com>
2023-01-20 16:01:37 -0500

    Updates

--- app/controllers/sendgrid_statuses_controller.rb
+++ app/controllers/sendgrid_statuses_controller.rb
@@ -2,13 +2,22 @@ class SendgridStatusesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
+  before_action :check_multiple_emails
+
   def create
     skip_authorization
-
     Notifications::SendgridEmailStatusNotification.main.notify! payload
+    head :ok
   end
 
   private
+
+    def check_multiple_emails
+      binding.pry
+      return if params['_json'].pluck(:email).uniq.size == 1
+
+      raise "multiple emails detected: #{payload}"
+    end
 
     def payload
       params['_json'].map do |item|
