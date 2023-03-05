dd0897be8f586f8ddc37ccd6c20ef1637c17b01c
garyfoster <gary@radicalbear.com>
2023-03-05 06:30:29 -0500

    Replace deprecated Authy with Twilio Verify

---
+++ lib/devise-authy/models/authy_authenticatable.rb
@@ -0,0 +1,25 @@
+require 'devise-authy/hooks/authy_authenticatable'
+module Devise
+  module Models
+    module AuthyAuthenticatable
+      extend ActiveSupport::Concern
+
+      def with_authy_authentication?(request)
+        if self.authy_id.present? && self.authy_enabled
+          return true
+        end
+
+        return false
+      end
+
+      module ClassMethods
+        def find_by_authy_id(authy_id)
+          where(authy_id: authy_id).first
+        end
+
+        Devise::Models.config(self, :authy_remember_device, :authy_enable_onetouch, :authy_enable_qr_code)
+      end
+    end
+  end
+end
+
