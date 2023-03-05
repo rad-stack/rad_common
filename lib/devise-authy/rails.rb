a296a1ed0f70ef85e7c57ab557da57fd5d337c45
garyfoster <gary@radicalbear.com>
2023-03-05 06:28:57 -0500

    Replace deprecated Authy with Twilio Verify

---
+++ lib/devise-authy/rails.rb
@@ -0,0 +1,16 @@
+module DeviseAuthy
+  class Engine < ::Rails::Engine
+    ActiveSupport.on_load(:action_controller) do
+      include DeviseAuthy::Controllers::Helpers
+    end
+    ActiveSupport.on_load(:action_view) do
+      include DeviseAuthy::Views::Helpers
+    end
+
+    # extend mapping with after_initialize because it's not reloaded
+    config.after_initialize do
+      Devise::Mapping.send :prepend, DeviseAuthy::Mapping
+    end
+  end
+end
+
