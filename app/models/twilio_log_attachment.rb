36fa35f1e4382231efd2c18c2e080e88175512b0
Caleb Tocco <calebtocco@hotmail.com>
2023-04-27 07:59:04 -0400

    Attachment validation

--- app/models/twilio_log_attachment.rb
+++ app/models/twilio_log_attachment.rb
@@ -3,5 +3,7 @@ class TwilioLogAttachment < ApplicationRecord
 
   has_one_attached :attachment
 
-  validates :attachment, presence: true
+  validates :attachment,
+            presence: true,
+            content_type: { in: RadCommon::VALID_IMAGE_TYPES, message: RadCommon::VALID_CONTENT_TYPE_MESSAGE }
 end
