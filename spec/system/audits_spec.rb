e7691717cd67f3085bc245f4090002a8df947941
garyfoster <gary@radicalbear.com>
2023-05-05 06:28:13 -0400

    Fix audited deprecation

--- spec/system/audits_spec.rb
+++ spec/system/audits_spec.rb
@@ -9,7 +9,7 @@ describe 'Audits' do
     it 'shows audits for objects without show pages' do
       open_status = create :status, name: 'Open'
 
-      Audited.audit_model.as_user(admin) do
+      Audited.audit_class.as_user(admin) do
         open_status.update!(name: 'Foo')
       end
 
