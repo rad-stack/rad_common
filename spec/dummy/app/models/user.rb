b9e65643974135387e0045c4586d24e97e89ddca
garyfoster <gary@radicalbear.com>
2023-06-15 13:37:30 -0400

    Devise module consistency

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :trackable, :lockable, :invitable,
-         :timeoutable, :password_archivable, :password_expirable, :secure_validatable, :expirable,
-         :twilio_verify_authenticatable
-
+  include RadDeviseHigh
   include RadUser
 
   has_many :divisions, foreign_key: 'owner_id', dependent: :restrict_with_error
