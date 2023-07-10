463d9598ea39eed40bef1d94ef2bbfd503303a06
Caleb Tocco <calebtocco@hotmail.com>
2023-07-10 06:58:20 -0400

    Install Generator and File Rename

--- spec/dummy/app/javascript/packs/rad_mailer.js
+++ spec/dummy/app/javascript/packs/rad_mailer.js
@@ -1,2 +1,2 @@
-import 'rad_common_js/src/css/rad_common/radbear_mailer_reset.css';
-import 'rad_common_js/src/css/rad_common/radbear_mailer.css';
+import 'rad_common_js/src/css/rad_common/rad_mailer_reset.css';
+import 'rad_common_js/src/css/rad_common/rad_mailer.css';
