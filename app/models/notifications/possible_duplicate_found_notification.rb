bd85a2e4e39b171e4413bb1033adcffbe3047416
Eric Bundy <elbzero@gmail.com>
2023-07-18 15:12:29 -0400

    adjust notification messaging

--- app/models/notifications/possible_duplicate_found_notification.rb
+++ app/models/notifications/possible_duplicate_found_notification.rb
@@ -1,14 +1,21 @@
 module Notifications
   class PossibleDuplicateFoundNotification < ::NotificationType
     def absolute_user_ids
-      created_by_user = subject_record.audits.order(created_at: :desc).first&.user
       return [created_by_user.id] if created_by_user.present?
 
       []
     end
 
-    def subject_record
-      nil
+    def created_by_user
+      subject_record.audits.order(created_at: :desc).first&.user
+    end
+
+    def mailer_message
+      "Possible duplicate (#{subject_record}) entered by #{created_by_user}"
+    end
+
+    def description
+      'Possible duplicate found'
     end
   end
 end
