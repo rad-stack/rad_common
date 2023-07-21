2ef8cfb451083829d2dc27b4542ae4438a86fd6f
Eric Bundy <elbzero@gmail.com>
2023-07-21 15:36:18 -0400

    fix spec

--- spec/rad_common/services/seeder_spec.rb
+++ spec/rad_common/services/seeder_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Seeder, type: :service do
+  before do
+    allow_any_instance_of(Notifications::PossibleDuplicateFoundNotification).to receive(:created_by_user)
+                                                                                  .and_return(create_by_user)
+  end
+
   it 'runs' do
     described_class.new.seed!
   end
