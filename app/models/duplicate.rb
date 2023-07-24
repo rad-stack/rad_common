a0a2ef244dc268f8ed405f853eb4eedaa460f923
Eric Bundy <elbzero@gmail.com>
2023-07-24 12:45:19 -0400

    fix specs

--- app/models/duplicate.rb
+++ app/models/duplicate.rb
@@ -4,7 +4,7 @@ class Duplicate < ApplicationRecord
   after_commit :maybe_notify_duplicates, on: :create
 
   def maybe_notify_duplicates
-    return unless score >= duplicatable.class.score_upper_threshold
+    return unless score && score >= duplicatable.class.score_upper_threshold
 
     Notifications::PossibleDuplicateFoundNotification.main.notify!(duplicatable)
     Notifications::PossibleDuplicateAdminNotification.main.notify!(duplicatable)
