2c8f5056fcfbe6ca8dede08853b46ef57021d0a1
Eric Bundy <elbzero@gmail.com>
2023-07-24 10:41:07 -0400

    fix specs

--- spec/rad_common/services/seeder_spec.rb
+++ spec/rad_common/services/seeder_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Seeder, type: :service do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
   before do
     allow_any_instance_of(Notifications::PossibleDuplicateFoundNotification).to receive(:created_by_user)
-      .and_return(create(:user))
+      .and_return(user)
+    allow_any_instance_of(Notifications::PossibleDuplicateAdminNotification).to receive(:created_by_user)
+      .and_return(admin)
   end
 
   it 'runs' do
