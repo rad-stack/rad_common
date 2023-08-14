b4a51f9e8d75b762b0ef97ecca0236b6e8641323
garyfoster <gary@radicalbear.com>
2023-08-14 07:39:11 -0400

    Make spec more resilient

--- spec/rad_common/services/seeder_spec.rb
+++ spec/rad_common/services/seeder_spec.rb
@@ -1,15 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Seeder, type: :service do
-  let(:user) { create :user }
-  let(:admin) { create :admin }
-
-  before do
-    allow_any_instance_of(Notifications::PossibleDuplicateFoundNotification).to receive(:created_by_user)
-      .and_return(user)
-    allow_any_instance_of(Notifications::PossibleDuplicateAdminNotification).to receive(:created_by_user)
-      .and_return(admin)
-  end
+  before { allow_any_instance_of(Duplicate).to receive(:maybe_notify_duplicates) }
 
   it 'runs' do
     described_class.new.seed!
