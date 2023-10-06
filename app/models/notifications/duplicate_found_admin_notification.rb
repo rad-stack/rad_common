4efedec20b2f84c99d39709ca8ebba87006c0f49
garyfoster <gary@radicalbear.com>
2023-10-06 07:57:44 -0400

    Change link on duplicates admin email to the duplicates console

--- app/models/notifications/duplicate_found_admin_notification.rb
+++ app/models/notifications/duplicate_found_admin_notification.rb
@@ -8,5 +8,9 @@ module Notifications
       "The system detected that #{created_by_user} entered a possible duplicate (#{subject_record}). " \
         'Please review the record and ensure that it is indeed a new record.'
     end
+
+    def subject_url
+      Rails.application.routes.url_helpers.duplicates_url model: subject_record.class, id: subject_record.id
+    end
   end
 end
