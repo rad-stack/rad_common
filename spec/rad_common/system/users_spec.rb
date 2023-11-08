986b59b147672e689a90b37c11bc4d6254484c23
garyfoster <gary@radicalbear.com>
2023-11-08 06:09:21 -0500

    Allow configuring devise timeout hours per project or user

--- spec/rad_common/system/users_spec.rb
+++ spec/rad_common/system/users_spec.rb
@@ -399,14 +399,14 @@ RSpec.describe 'Users', type: :system do
     before { allow_any_instance_of(User).to receive(:twilio_verify_enabled?).and_return(false) }
 
     context 'with internal user' do
-      it 'sign in times out after 6 hours' do
+      it 'sign in times out after the configured hours' do
         visit new_user_session_path
         fill_in 'user_email', with: user.email
         fill_in 'user_password', with: password
         click_button 'Sign In'
         expect(page).to have_content('Signed in successfully')
 
-        Timecop.travel(365.minutes.from_now) do
+        Timecop.travel((RadConfig.timeout_hours!.hours + 5.minutes).from_now) do
           visit users_path
           expect(page).to have_content('Your session expired. Please sign in again to continue.')
         end
