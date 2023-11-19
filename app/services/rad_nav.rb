528798a419f2149814670060d126b1afe4a24c54
garyfoster <gary@radicalbear.com>
2023-11-19 05:51:39 -0500

    Navigation framework

--- app/services/rad_nav.rb
+++ app/services/rad_nav.rb
@@ -17,6 +17,52 @@ class RadNav
 
   private
 
+    def user_nav
+      top_nav_index_item('User')
+    end
+
+    def admin_menu
+      return unless current_user.admin?
+
+      dropdown_menu 'Admin', admin_menu_items
+    end
+
+    def admin_menu_items
+      safe_join(additional_admin_items + [render('layouts/navigation_admin', no_divider: no_admin_divider?)])
+    end
+
+    def additional_admin_items
+      []
+    end
+
+    def no_admin_divider?
+      additional_admin_items.blank?
+    end
+
+    def dropdown_menu(label, items)
+      tag.li(class: 'nav-item dropdown px-3') do # TODO: do we like the px-3?
+        safe_join [dropdown_menu_header(label), dropdown_menu_content(items)]
+      end
+    end
+
+    def dropdown_menu_header(label)
+      tag.a(class: 'nav-link dropdown-toggle', 'data-toggle': 'dropdown', href: '#') do
+        label
+      end
+    end
+
+    def dropdown_menu_content(items)
+      tag.ul(class: 'dropdown-menu') do
+        items
+      end
+    end
+
+    def dropdown_menu_item(label, path)
+      tag.li do
+        link_to label, path, class: 'dropdown-item'
+      end
+    end
+
     def top_nav_index_item(model_name, path: nil, badge: nil, label: nil)
       return unless policy(model_name.constantize).index?
 
@@ -31,43 +77,5 @@ class RadNav
           badge.present? ? safe_join([label, ' ', badge].compact) : label
         end
       end
-    end
-
-    def admin_menu
-      return unless current_user.admin?
-
-      tag.li(class: 'nav-item dropdown px-3') do # TODO: do we like the px-3?
-        safe_join [dropdown_menu('Admin'), admin_nav]
-      end
-    end
-
-    def user_nav
-      top_nav_index_item('User')
-    end
-
-    def dropdown_menu(label)
-      tag.a(class: 'nav-link dropdown-toggle', 'data-toggle': 'dropdown', href: '#') do
-        label
-      end
-    end
-
-    def dropdown_menu_item(label, path)
-      tag.li do
-        link_to label, path, class: 'dropdown-item'
-      end
-    end
-
-    def additional_admin_items
-      []
-    end
-
-    def admin_nav
-      tag.ul(class: 'dropdown-menu') do
-        safe_join(additional_admin_items + [render('layouts/navigation_admin', no_divider: no_admin_divider?)])
-      end
-    end
-
-    def no_admin_divider?
-      additional_admin_items.blank?
     end
 end
