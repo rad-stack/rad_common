7a34843333e3ec0c7efaeed3cca1fd805471195b
garyfoster <gary@radicalbear.com>
2023-11-25 13:56:17 -0500

    Admin menu

--- app/services/rad_nav/admin_menu.rb
+++ app/services/rad_nav/admin_menu.rb
@@ -2,7 +2,7 @@ module RadNav
   class AdminMenu
     attr_accessor :view_context, :additional_items
 
-    delegate :current_user, :render, to: :view_context
+    delegate :current_user, :render, :tag, to: :view_context
 
     def initialize(view_context, additional_items: [])
       @view_context = view_context
@@ -18,11 +18,60 @@ module RadNav
     private
 
       def admin_menu_items
-        additional_items + [render('layouts/navigation_admin', no_divider: no_admin_divider?)]
+        additional_items + standard_items
       end
 
-      def no_admin_divider?
-        additional_items.blank?
+      def standard_items
+        [divider,
+         RadNav::DropdownMenuItem.new(view_context, 'Audit Search', '/rad_common/audits').content,
+         RadNav::DropdownMenuItem.new(view_context, 'Background Jobs', '/sidekiq', separate_tab: true).content,
+         RadNav::DropdownMenuItem.new(view_context, 'Company Info', '/rad_common/company/edit').content,
+         generate_jwt,
+         RadNav::DropdownMenuItem.new(view_context, 'Notification Types', '/rad_common/notification_types').content,
+         RadNav::DropdownMenuIndexItem.new(view_context, 'SecurityRole').content,
+         sentry_test,
+         RadNav::DropdownMenuItem.new(view_context, 'Sign In Activity', '/rad_common/login_activities').content,
+         system_messages,
+         RadNav::DropdownMenuItem.new(view_context, 'System Usage', '/rad_common/system_usages').content,
+         twilio_logs,
+         RadNav::DropdownMenuIndexItem.new(view_context, 'User').content,
+         validate_database]
+      end
+
+      def divider
+        return if additional_items.blank?
+
+        tag.li(class: 'dropdown-divider')
+      end
+
+      def generate_jwt
+        return unless RadConfig.jwt_enabled?
+
+        RadNav::DropdownMenuItem.new(view_context, 'Generate JWT Token', view_context.new_json_web_token_path).content
+      end
+
+      def sentry_test
+        RadNav::DropdownMenuItem.new(view_context,
+                                     'Sentry Test',
+                                     RadCommon::Engine.routes.url_helpers.edit_sentry_test_path(current_user)).content
+      end
+
+      def system_messages
+        RadNav::DropdownMenuItem.new(view_context,
+                                     'System Message',
+                                     RadCommon::Engine.routes.url_helpers.new_system_message_path).content
+      end
+
+      def twilio_logs
+        return unless RadTwilio.new.twilio_enabled? || TwilioLog.exists?
+
+        RadNav::DropdownMenuItem.new(view_context, 'Twilio Logs', '/rad_common/twilio_logs').content
+      end
+
+      def validate_database
+        RadNav::DropdownMenuItem.new(view_context,
+                                     'Validate Database',
+                                     '/rad_common/global_validations/new').content
       end
   end
 end
--- app/services/rad_nav/dropdown_menu_item.rb
+++ app/services/rad_nav/dropdown_menu_item.rb
@@ -1,25 +1,38 @@
 module RadNav
   class DropdownMenuItem
-    attr_accessor :view_context, :label, :path, :permission, :badge
+    attr_accessor :view_context, :label, :path, :permission, :badge, :separate_tab
 
     delegate :tag, :link_to, :safe_join, to: :view_context
 
-    def initialize(view_context, label, path, badge: nil, permission: true)
+    def initialize(view_context, label, path, badge: nil, separate_tab: false, permission: true)
       @view_context = view_context
       @label = label
       @path = path
       @permission = permission
       @badge = badge
+      @separate_tab = separate_tab
     end
 
     def content
       return unless permission
 
       tag.li do
-        link_to path, class: 'dropdown-item' do
+        link_to path, link_options do
           badge.present? ? safe_join([label, ' ', badge].compact) : label
         end
       end
     end
+
+    private
+
+      def link_options
+        items = { class: 'dropdown-item' }
+        items = items.merge(target: '_blank', rel: :noopener) if separate_tab?
+        items
+      end
+
+      def separate_tab?
+        separate_tab
+      end
   end
 end
