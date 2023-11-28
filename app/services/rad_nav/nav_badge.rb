8307fc3357b9c88569a0dbf73859ee89c6a3707c
garyfoster <gary@radicalbear.com>
2023-11-28 06:32:16 -0500

    Check alert style

--- app/services/rad_nav/nav_badge.rb
+++ app/services/rad_nav/nav_badge.rb
@@ -8,6 +8,8 @@ module RadNav
       @view_context = view_context
       @alert_style = alert_style
       @count = count
+
+      check_alert_style
     end
 
     def content
@@ -17,5 +19,16 @@ module RadNav
         count.to_s
       end
     end
+
+    private
+
+      def check_alert_style
+        raise 'alert_style must be a symbol' unless alert_style.is_a?(Symbol)
+        raise 'alert style is invalid' unless alert_styles.include?(alert_style)
+      end
+
+      def alert_styles
+        %i[primary secondary success danger warning info light dark]
+      end
   end
 end
