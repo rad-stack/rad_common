08319e6de3c42495f6af2cdbbfcb6e1f9819b1e4
garyfoster <gary@radicalbear.com>
2023-11-29 05:43:02 -0500

    Nav content refactor

--- spec/system/nav_spec.rb
+++ spec/system/nav_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 describe 'Nav' do
-  let(:user) { create :admin }
   let(:attorney) { create :attorney }
 
-  before do
-    allow_any_instance_of(Duplicate).to receive :maybe_notify_duplicates
-
+  let(:duplicate_attorney) do
     create :attorney,
            first_name: attorney.first_name,
            last_name: attorney.last_name,
@@ -16,19 +13,40 @@ describe 'Nav' do
            zipcode: attorney.zipcode,
            phone_number: attorney.phone_number,
            email: attorney.email
+  end
+
+  before do
+    allow_any_instance_of(Duplicate).to receive :maybe_notify_duplicates
+
+    attorney
+    duplicate_attorney
 
     attorney.process_duplicates
+    duplicate_attorney.process_duplicates
 
     login_as user, scope: :user
   end
 
-  it 'shows the menu' do
-    visit root_path
-    expect(page).to have_content 'Clients'
+  context 'with admin' do
+    let(:user) { create :admin }
+
+    it 'shows the menu' do
+      visit root_path
+      expect(page).to have_content 'Clients'
+    end
+
+    context 'with users on admin menu' do
+      before { allow_any_instance_of(RadNav::AdminMenu).to receive(:include_users).and_return(true) }
+
+      it 'shows the menu' do
+        visit root_path
+        expect(page).to have_content 'Clients'
+      end
+    end
   end
 
-  context 'with users on admin menu' do
-    before { allow_any_instance_of(RadNav::AdminMenu).to receive(:include_users).and_return(true) }
+  context 'with user' do
+    let(:user) { create :user }
 
     it 'shows the menu' do
       visit root_path
