553ff19af733fbf3f54642c04800ba2127c75a89
garyfoster <gary@radicalbear.com>
2023-12-05 13:13:46 -0500

    Minor cleanup

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -1,4 +1,8 @@
 class UserStatus < ApplicationRecord
+  PENDING_STATUS_NAME = 'Pending'.freeze
+  ACTIVE_STATUS_NAME = 'Active'.freeze
+  INACTIVE_STATUS_NAME = 'Inactive'.freeze
+
   scope :not_pending, -> { where.not(id: default_pending_status.id) }
   scope :by_id, -> { order(:id) }
 
@@ -7,25 +11,14 @@ class UserStatus < ApplicationRecord
   strip_attributes
 
   def self.default_pending_status
-    UserStatus.find_by(name: 'Pending')
+    UserStatus.find_by(name: PENDING_STATUS_NAME)
   end
 
   def self.default_active_status
-    UserStatus.find_by(name: 'Active')
+    UserStatus.find_by(name: ACTIVE_STATUS_NAME)
   end
 
   def self.default_inactive_status
-    UserStatus.find_by(name: 'Inactive')
-  end
-
-  def button_style
-    case name
-    when 'Active'
-      'btn-success'
-    when 'Pending'
-      'btn-warning'
-    else
-      'btn-danger'
-    end
+    UserStatus.find_by(name: INACTIVE_STATUS_NAME)
   end
 end
