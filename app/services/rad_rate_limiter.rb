f767a058284ae54075d31c3695895921199accdd
Eric Bundy <elbzero@gmail.com>
2024-01-01 09:00:04 -0500

    switch to rails cache and add spec for rate limiter

--- app/services/rad_rate_limiter.rb
+++ app/services/rad_rate_limiter.rb
@@ -1,20 +1,14 @@
 class RadRateLimiter
   class RateLimitExceededError < StandardError; end
 
-  attr_reader :limit, :period, :key
-
   def initialize(limit:, period:, key:)
     @limit = limit
     @period = period
     @key = key
   end
 
-  def self.redis_client
-    @redis_client ||= RedisClient.new
-  end
-
   def run
-    check_rate_limit! unless Rails.env.test?
+    check_rate_limit!
 
     yield
   end
@@ -23,16 +17,15 @@ class RadRateLimiter
 
     def check_rate_limit!
       current_count = increment_key
-      set_expiration if current_count == 1
 
-      raise RateLimitExceededError, "Rate limit exceeded for key: #{key}" if current_count > limit
+      raise RateLimitExceededError, "Rate limit exceeded for key: #{@key}" if current_count > @limit
     end
 
     def increment_key
-      self.class.redis_client.call('INCR', "rate_limit:#{key}")
-    end
-
-    def set_expiration
-      self.class.redis_client.call('EXPIRE', "rate_limit:#{key}", period)
+      cache_key = "rate_limit:#{@key}"
+      current_value = Rails.cache.fetch(cache_key, expires_in: @period.minutes) { 0 }
+      incremented_value = current_value + 1
+      Rails.cache.write(cache_key, incremented_value)
+      incremented_value
     end
 end
