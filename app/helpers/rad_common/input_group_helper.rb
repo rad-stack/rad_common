9ba1f22db3c1d206517c0d58894a3049bbe473da
calebtocco <calebtocco4@gmail.com>
2024-02-05 08:37:09 -0500

    Fix Module

--- app/helpers/rad_common/input_group_helper.rb
+++ app/helpers/rad_common/input_group_helper.rb
@@ -1,9 +1,11 @@
-module InputGroupHelper
-  def addon(title, html_tag = 'span')
-    return if title.blank?
+module RadCommon
+  module InputGroupHelper
+    def addon(title, html_tag = 'span')
+      return if title.blank?
 
-    content_tag(html_tag.to_s, class: 'input-group-text') do
-      title
+      content_tag(html_tag.to_s, class: 'input-group-text') do
+        title
+      end
     end
   end
 end
