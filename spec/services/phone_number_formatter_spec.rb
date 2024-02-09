185dff098ca6e97ed30cb54467200b99991dfa25
Eric Bundy <elbzero@gmail.com>
2024-02-09 13:55:44 -0500

    update phone formatting requirements
    We need to invalidate when people put things in like ext 1234

--- spec/services/phone_number_formatter_spec.rb
+++ spec/services/phone_number_formatter_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumberFormatter, type: :service do
+  let(:valid_phone_formats) do
+    ['1233211234', '123-321-1234', '123 321 1234', ' 123 321 1234 ', '+11233211234',
+     '(123) 321-1234', '123-321-1234', '1-123-321-1234',
+     '21233211234', '+1 (123) 321-1234', '(123)321-1234']
+  end
+
   describe 'format' do
     it 'formats phone numbers correctly' do
-      expect(described_class.format('(123)321-1234')).to eq '(123) 321-1234'
-      expect(described_class.format('123321-1234')).to eq '(123) 321-1234'
-      expect(described_class.format('1233211234')).to eq '(123) 321-1234'
-      expect(described_class.format(1_233_211_234)).to eq '(123) 321-1234'
-      expect(described_class.format('123-321-1234')).to eq '(123) 321-1234'
+      valid_phone_formats.each do |phone_number|
+        expect(described_class.format(phone_number)).to eq '(123) 321-1234'
+      end
     end
   end
 end
