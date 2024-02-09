88ae8d534a3dd84c8e6d781d09c385d7cae98927
garyfoster <gary@radicalbear.com>
2024-02-09 14:18:23 -0500

    Add more specs

--- spec/services/phone_number_formatter_spec.rb
+++ spec/services/phone_number_formatter_spec.rb
@@ -7,10 +7,20 @@ RSpec.describe PhoneNumberFormatter, type: :service do
      '21233211234', '+1 (123) 321-1234', '(123)321-1234']
   end
 
+  let(:invalid_phone_formats) do
+    ['foo bar', 'foo bar 1', 'foo bar 1234567890', '123-321-1234 ext 5', '123-321-1234 foo bar']
+  end
+
   describe 'format' do
     it 'formats phone numbers correctly' do
       valid_phone_formats.each do |phone_number|
         expect(described_class.format(phone_number)).to eq '(123) 321-1234'
+      end
+    end
+
+    it 'ignores non-phone numbers' do
+      invalid_phone_formats.each do |phone_number|
+        expect(described_class.format(phone_number)).to be_nil
       end
     end
   end
