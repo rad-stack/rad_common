63cc0eae62d0a6d798d4b1cddf90eb3987cc9d5d
garyfoster <gary@radicalbear.com>
2024-02-14 13:47:30 -0500

    refactor grouped users

--- spec/services/user_grouper_spec.rb
+++ spec/services/user_grouper_spec.rb
@@ -32,68 +32,66 @@ describe UserGrouper do
     another_user
   end
 
-  describe 'grouped_list (formerly list)' do
-    context 'with standard case' do
+  context 'with standard case' do
+    let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
+
+    it { is_expected.to eq result }
+  end
+
+  context 'with scope' do
+    let(:scopes) { [:with_mobile_phone] }
+    let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
+
+    it { is_expected.to eq result }
+  end
+
+  context 'with scope without another user' do
+    let(:scopes) { [:with_mobile_phone] }
+    let(:another_user) { create :user, mobile_phone: nil }
+    let(:result) { [['Me', [current_user]], ['Users', [user]], ['Inactive', [inactive_user]]] }
+
+    it { is_expected.to eq result }
+  end
+
+  context 'with scope without me' do
+    let(:scopes) { [:with_mobile_phone] }
+    let(:current_user) { create :user, mobile_phone: nil }
+    let(:result) { [['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
+
+    it { is_expected.to eq result }
+  end
+
+  context 'with always include' do
+    context 'with active user' do
+      let(:always_include) { user }
       let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
 
       it { is_expected.to eq result }
     end
 
-    context 'with scope' do
-      let(:scopes) { [:with_mobile_phone] }
+    context 'with inactive user' do
+      let(:always_include) { inactive_user }
       let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
 
       it { is_expected.to eq result }
     end
 
-    context 'with scope without another user' do
+    context 'with active user not in scope' do
       let(:scopes) { [:with_mobile_phone] }
-      let(:another_user) { create :user, mobile_phone: nil }
-      let(:result) { [['Me', [current_user]], ['Users', [user]], ['Inactive', [inactive_user]]] }
+      let(:always_include) { user }
+      let(:user) { create :user, first_name: 'Allan', mobile_phone: nil }
+      let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
 
       it { is_expected.to eq result }
     end
 
-    context 'with scope without me' do
+    context 'with inactive user not in scope' do
       let(:scopes) { [:with_mobile_phone] }
-      let(:current_user) { create :user, mobile_phone: nil }
-      let(:result) { [['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
+      let(:always_include) { inactive_user }
+      let(:inactive_user) { create :user, :inactive, mobile_phone: nil }
+      let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
 
       it { is_expected.to eq result }
-    end
-
-    context 'with always include' do
-      context 'with active user' do
-        let(:always_include) { user }
-        let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
-
-        it { is_expected.to eq result }
-      end
-
-      context 'with inactive user' do
-        let(:always_include) { inactive_user }
-        let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
-
-        it { is_expected.to eq result }
-      end
-
-      context 'with active user not in scope' do
-        let(:scopes) { [:with_mobile_phone] }
-        let(:always_include) { user }
-        let(:user) { create :user, first_name: 'Allan', mobile_phone: nil }
-        let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
-
-        it { is_expected.to eq result }
-      end
-
-      context 'with inactive user not in scope' do
-        let(:scopes) { [:with_mobile_phone] }
-        let(:always_include) { inactive_user }
-        let(:inactive_user) { create :user, :inactive, mobile_phone: nil }
-        let(:result) { [['Me', [current_user]], ['Users', [user, another_user]], ['Inactive', [inactive_user]]] }
-
-        it { is_expected.to eq result }
-      end
     end
   end
 
