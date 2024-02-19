553c3dce142dbf4e9b31b5eaebe167dd53de518f
garyfoster <gary@radicalbear.com>
2024-02-19 06:53:04 -0500

    sidekiq.yml install should be controlled by procfile config

--- lib/generators/rad_common/install/install_generator.rb
+++ lib/generators/rad_common/install/install_generator.rb
@@ -52,7 +52,6 @@ module RadCommon
 
         copy_file '../../../../../spec/dummy/config/webpacker.yml', 'config/webpacker.yml'
         copy_file '../../../../../spec/dummy/config/puma.rb', 'config/puma.rb'
-        copy_file '../../../../../spec/dummy/config/sidekiq.yml', 'config/sidekiq.yml'
         directory '../../../../../spec/dummy/config/environments/', 'config/environments/'
         directory '../../../../../spec/dummy/config/webpack/', 'config/webpack/'
         template '../../../../../spec/dummy/config/initializers/devise.rb', 'config/initializers/devise.rb'
@@ -312,6 +311,7 @@ Seeder.new.seed!
           return if RadConfig.procfile_override?
 
           copy_file '../../../../../spec/dummy/Procfile', 'Procfile'
+          copy_file '../../../../../spec/dummy/config/sidekiq.yml', 'config/sidekiq.yml'
         end
 
         def update_seeder_method
