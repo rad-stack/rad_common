bd601ffb01c38c6ebc2f60150cc97814ae5d4c56
calebtocco <calebtocco4@gmail.com>
2024-02-19 11:00:09 -0500

    Prawn Font with Better UTF-8 Support

--- app/services/exporter.rb
+++ app/services/exporter.rb
@@ -38,6 +38,7 @@ class Exporter
 
     def generate_pdf
       pdf = Prawn::Document.new(page_layout: :landscape, page_size: 'A3', margin: [10, 10, 20, 20])
+      pdf.setup_font!
 
       pdf.image Rails.root.join('app', 'javascript', 'images', RadConfig.app_logo_filename!),
                 position: :left,
@@ -50,10 +51,7 @@ class Exporter
       data = records.map do |record|
         @current_record = record
         reset_attributes
-        # TODO: Consider adding global font that supports more UTF-8 characters
-        write_attributes.map do |cell|
-          cell.to_s.encode('Windows-1252', invalid: :replace, undef: :replace, replace: '')
-        end
+        write_attributes
       end
 
       pdf.table([headers] + data, header: true, width: pdf.bounds.width)
--- config/initializers/prawn.rb
+++ config/initializers/prawn.rb
@@ -1,1 +1,17 @@
-Prawn::Fonts::AFM.hide_m17n_warning = true
+module PrawnSetupFont
+  def setup_font!
+    font_path = "#{Gem::Specification.find_by_name('rad_common').gem_dir}/app/assets/fonts/"
+    font_families.update(
+      'DejaVuSans' => {
+        normal: "#{font_path}DejaVuSans.ttf",
+        italic: "#{font_path}DejaVuSansCondensed-Oblique.ttf",
+        bold: "#{font_path}DejaVuSans-Bold.ttf",
+        bold_italic: "#{font_path}DejaVuSansCondensed-BoldOblique.ttf"
+      }
+    )
+
+    font 'DejaVuSans'
+  end
+end
+
+Prawn::Document.include PrawnSetupFont
