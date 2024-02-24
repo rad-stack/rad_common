8b2ef3fd13e8babf1da473cb7fc72bcc2cf5c3d0
garyfoster <gary@radicalbear.com>
2024-02-24 08:40:27 -0500

    Updates

--- spec/rad_common/services/seeder_spec.rb
+++ spec/rad_common/services/seeder_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Seeder, type: :service do
-  before { allow_any_instance_of(Duplicate).to receive(:maybe_notify!) }
-
   it 'runs' do
     described_class.new.seed!
   end
