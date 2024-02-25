04813f50822ff1c9eb006df490b60a6bd35754eb
garyfoster <gary@radicalbear.com>
2024-02-25 06:17:27 -0500

    Updates

--- spec/services/duplicates_processor_2_spec.rb
+++ spec/services/duplicates_processor_2_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe DuplicatesProcessor do
-  let(:phone_number_1) { '(111) 111-1111' }
-  let(:email_1) { '111@abc.com' }
+  let(:first_name) { 'John' }
+  let(:last_name) { 'Smith' }
+  let(:company_name) { 'ABC' }
+  let(:address_1) { '100 8th St' }
+  let(:city) { 'Honolulu' }
+  let(:phone_number) { '(111) 111-1111' }
+  let(:email) { '111@abc.com' }
   let(:created_by) { create :user }
 
   before { attorney_2 }
@@ -13,65 +18,62 @@ RSpec.describe DuplicatesProcessor do
     context 'when matching only on additional items' do
       let(:attorney_1) do
         create :attorney,
-               company_name: 'ABC',
-               address_1: 'Xxxx',
+               first_name: first_name,
+               last_name: last_name,
+               company_name: company_name,
+               address_1: address_1,
                address_2: nil,
-               city: 'Xxxx',
+               city: city,
                state: 'FL',
                zipcode: '11111',
-               phone_number: phone_number_1,
-               email: email_1,
-               first_name: 'Xxxx',
-               last_name: 'Tttt'
+               phone_number: phone_number,
+               email: email
       end
 
       let(:attorney_2) do
         create :attorney,
+               first_name: 'Yyyy',
+               last_name: 'Ssss',
                company_name: 'XYZ',
                address_1: 'Yyyy',
                address_2: nil,
                city: 'Yyyy',
                state: 'CA',
                zipcode: '22222',
-               phone_number: phone_number_1,
-               email: email_1,
-               first_name: 'Yyyy',
-               last_name: 'Ssss'
+               phone_number: phone_number,
+               email: email
       end
 
       it { is_expected.to eq [{ id: attorney_2.id, score: 32 }] }
     end
 
     context 'when matching on standard plus additional items' do
-      let(:first_name) { 'John' }
-      let(:last_name) { 'Smith' }
-
       let(:attorney_1) do
         create :attorney,
-               company_name: 'ABC',
-               address_1: 'Xxxx',
+               first_name: first_name,
+               last_name: last_name,
+               company_name: company_name,
+               address_1: address_1,
                address_2: nil,
-               city: 'Xxxx',
+               city: city,
                state: 'FL',
                zipcode: '11111',
-               phone_number: phone_number_1,
-               email: email_1,
-               first_name: first_name,
-               last_name: last_name
+               phone_number: phone_number,
+               email: email
       end
 
       let(:attorney_2) do
         create :attorney,
+               first_name: first_name,
+               last_name: last_name,
                company_name: 'XYZ',
                address_1: 'Yyyy',
                address_2: nil,
                city: 'Yyyy',
                state: 'CA',
                zipcode: '22222',
-               phone_number: phone_number_1,
-               email: email_1,
-               first_name: first_name,
-               last_name: last_name
+               phone_number: phone_number,
+               email: email
       end
 
       it { is_expected.to eq [{ id: attorney_2.id, score: 46 }] }
@@ -79,13 +81,22 @@ RSpec.describe DuplicatesProcessor do
 
     context 'when matching only on additional items 2' do
       let(:attorney_1) do
-        create :attorney, address_2: 'Suite 111', zipcode: '11111', phone_number: phone_number_1, email: email_1
+        create :attorney,
+               first_name: first_name,
+               last_name: last_name,
+               company_name: company_name,
+               address_1: address_1,
+               address_2: 'Suite 111',
+               city: city,
+               zipcode: '11111',
+               phone_number: phone_number,
+               email: email
       end
 
       let(:attorney_2) do
         create :attorney,
-               first_name: attorney_1.first_name,
-               last_name: attorney_1.last_name,
+               first_name: first_name,
+               last_name: last_name,
                company_name: attorney_1.company_name,
                address_1: attorney_1.address_1,
                address_2: 'Room 222',
