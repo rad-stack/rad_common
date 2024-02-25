f363adede07b64c1ee95414c778f3257933b259c
garyfoster <gary@radicalbear.com>
2024-02-25 06:24:09 -0500

    Updates

--- spec/services/duplicates_processor_2_spec.rb
+++ spec/services/duplicates_processor_2_spec.rb
@@ -5,38 +5,39 @@ RSpec.describe DuplicatesProcessor do
   let(:last_name) { 'Smith' }
   let(:company_name) { 'ABC' }
   let(:address_1) { '100 8th St' }
+  let(:address_2) { nil }
   let(:city) { 'Honolulu' }
+  let(:state) { 'HI' }
+  let(:zipcode) { '11111' }
   let(:phone_number) { '(111) 111-1111' }
   let(:email) { '111@abc.com' }
   let(:created_by) { create :user }
 
-  before { attorney_2 }
+  let(:attorney_1) do
+    create :attorney,
+           first_name: first_name,
+           last_name: last_name,
+           company_name: company_name,
+           address_1: address_1,
+           address_2: address_2,
+           city: city,
+           state: state,
+           zipcode: zipcode,
+           phone_number: phone_number,
+           email: email
+  end
 
   describe 'matches' do
     subject { described_class.new(attorney_1).matches }
 
     context 'when matching only on additional items' do
-      let(:attorney_1) do
-        create :attorney,
-               first_name: first_name,
-               last_name: last_name,
-               company_name: company_name,
-               address_1: address_1,
-               address_2: nil,
-               city: city,
-               state: 'FL',
-               zipcode: '11111',
-               phone_number: phone_number,
-               email: email
-      end
-
-      let(:attorney_2) do
+      let!(:attorney_2) do
         create :attorney,
                first_name: 'Yyyy',
                last_name: 'Ssss',
                company_name: 'XYZ',
                address_1: 'Yyyy',
-               address_2: nil,
+               address_2: address_2,
                city: 'Yyyy',
                state: 'CA',
                zipcode: '22222',
@@ -48,27 +49,13 @@ RSpec.describe DuplicatesProcessor do
     end
 
     context 'when matching on standard plus additional items' do
-      let(:attorney_1) do
-        create :attorney,
-               first_name: first_name,
-               last_name: last_name,
-               company_name: company_name,
-               address_1: address_1,
-               address_2: nil,
-               city: city,
-               state: 'FL',
-               zipcode: '11111',
-               phone_number: phone_number,
-               email: email
-      end
-
-      let(:attorney_2) do
+      let!(:attorney_2) do
         create :attorney,
                first_name: first_name,
                last_name: last_name,
                company_name: 'XYZ',
                address_1: 'Yyyy',
-               address_2: nil,
+               address_2: address_2,
                city: 'Yyyy',
                state: 'CA',
                zipcode: '22222',
@@ -80,26 +67,13 @@ RSpec.describe DuplicatesProcessor do
     end
 
     context 'when matching only on additional items 2' do
-      let(:attorney_1) do
-        create :attorney,
-               first_name: first_name,
-               last_name: last_name,
-               company_name: company_name,
-               address_1: address_1,
-               address_2: 'Suite 111',
-               city: city,
-               zipcode: '11111',
-               phone_number: phone_number,
-               email: email
-      end
-
-      let(:attorney_2) do
+      let!(:attorney_2) do
         create :attorney,
                first_name: first_name,
                last_name: last_name,
                company_name: attorney_1.company_name,
                address_1: attorney_1.address_1,
-               address_2: 'Room 222',
+               address_2: address_2,
                city: attorney_1.city,
                state: attorney_1.state,
                zipcode: '22222',
