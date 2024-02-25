c7a1a9e1c5998f695081101d1b14d639e6b1f926
garyfoster <gary@radicalbear.com>
2024-02-25 06:07:06 -0500

    Updates

--- spec/services/duplicates_processor_2_spec.rb
+++ spec/services/duplicates_processor_2_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DuplicatesProcessor do
-  let(:phone_number) { create :phone_number }
-  let(:email) { Faker::Internet.email }
+  let(:phone_number_1) { '(111) 111-1111' }
+  let(:email_1) { '111@abc.com' }
   let(:created_by) { create :user }
 
   before { attorney_2 }
@@ -12,29 +12,31 @@ RSpec.describe DuplicatesProcessor do
 
     context 'when matching only on additional items' do
       let(:attorney_1) do
-        create :attorney, attorney_1_attributes.merge(company_name: 'ABC',
-                                                      address_1: 'Xxxx',
-                                                      address_2: nil,
-                                                      city: 'Xxxx',
-                                                      state: 'FL',
-                                                      zipcode: '11111')
+        create :attorney,
+               company_name: 'ABC',
+               address_1: 'Xxxx',
+               address_2: nil,
+               city: 'Xxxx',
+               state: 'FL',
+               zipcode: '11111',
+               phone_number: phone_number_1,
+               email: email_1,
+               first_name: 'Xxxx',
+               last_name: 'Tttt'
       end
 
       let(:attorney_2) do
-        create :attorney, attorney_2_attributes.merge(company_name: 'XYZ',
-                                                      address_1: 'Yyyy',
-                                                      address_2: nil,
-                                                      city: 'Yyyy',
-                                                      state: 'CA',
-                                                      zipcode: '22222')
-      end
-
-      let(:attorney_1_attributes) do
-        { phone_number: phone_number, email: email, first_name: 'Xxxx', last_name: 'Tttt' }
-      end
-
-      let(:attorney_2_attributes) do
-        { phone_number: phone_number, email: email, first_name: 'Yyyy', last_name: 'Ssss' }
+        create :attorney,
+               company_name: 'XYZ',
+               address_1: 'Yyyy',
+               address_2: nil,
+               city: 'Yyyy',
+               state: 'CA',
+               zipcode: '22222',
+               phone_number: phone_number_1,
+               email: email_1,
+               first_name: 'Yyyy',
+               last_name: 'Ssss'
       end
 
       it { is_expected.to eq [{ id: attorney_2.id, score: 32 }] }
@@ -45,29 +47,31 @@ RSpec.describe DuplicatesProcessor do
       let(:last_name) { 'Smith' }
 
       let(:attorney_1) do
-        create :attorney, attorney_1_attributes.merge(company_name: 'ABC',
-                                                      address_1: 'Xxxx',
-                                                      address_2: nil,
-                                                      city: 'Xxxx',
-                                                      state: 'FL',
-                                                      zipcode: '11111')
+        create :attorney,
+               company_name: 'ABC',
+               address_1: 'Xxxx',
+               address_2: nil,
+               city: 'Xxxx',
+               state: 'FL',
+               zipcode: '11111',
+               phone_number: phone_number_1,
+               email: email_1,
+               first_name: first_name,
+               last_name: last_name
       end
 
       let(:attorney_2) do
-        create :attorney, attorney_2_attributes.merge(company_name: 'XYZ',
-                                                      address_1: 'Yyyy',
-                                                      address_2: nil,
-                                                      city: 'Yyyy',
-                                                      state: 'CA',
-                                                      zipcode: '22222')
-      end
-
-      let(:attorney_1_attributes) do
-        { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
-      end
-
-      let(:attorney_2_attributes) do
-        { phone_number: phone_number, email: email, first_name: first_name, last_name: last_name }
+        create :attorney,
+               company_name: 'XYZ',
+               address_1: 'Yyyy',
+               address_2: nil,
+               city: 'Yyyy',
+               state: 'CA',
+               zipcode: '22222',
+               phone_number: phone_number_1,
+               email: email_1,
+               first_name: first_name,
+               last_name: last_name
       end
 
       it { is_expected.to eq [{ id: attorney_2.id, score: 46 }] }
@@ -75,7 +79,7 @@ RSpec.describe DuplicatesProcessor do
 
     context 'when matching only on additional items 2' do
       let(:attorney_1) do
-        create :attorney, address_2: 'Suite 111', zipcode: '11111', phone_number: '(111) 111-1111', email: '111@abc.com'
+        create :attorney, address_2: 'Suite 111', zipcode: '11111', phone_number: phone_number_1, email: email_1
       end
 
       let(:attorney_2) do
