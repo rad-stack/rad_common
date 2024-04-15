192fd207ff301e573cacb00facff7c5e0e27862e
garyfoster <gary@radicalbear.com>
2024-04-15 05:14:39 -0400

    Add sorted scope

--- app/models/contact_log.rb
+++ app/models/contact_log.rb
@@ -9,6 +9,8 @@ class ContactLog < ApplicationRecord
 
   alias_attribute :active?, :success?
 
+  scope :sorted, -> { order(created_at: :desc, id: :desc) }
+
   validates :from_user_id, presence: true, if: :outgoing?
   validates :message_sid, presence: true, if: :incoming?
   validates :media_url, absence: true, if: :incoming?
