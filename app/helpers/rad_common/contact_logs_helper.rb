f074bcd2830a716867424ef335f8226fc545b6c6
garyfoster <gary@radicalbear.com>
2024-08-12 13:35:29 -0400

    refactor to share code

--- app/helpers/rad_common/contact_logs_helper.rb
+++ app/helpers/rad_common/contact_logs_helper.rb
@@ -29,17 +29,8 @@ module RadCommon
     end
 
     def email_alert_input_label(record, attribute)
-      string_label = translated_attribute_label(record, attribute)
-
-      items = ContactLogRecipient.where(email: record.send(attribute)).sorted.limit(1)
-      return string_label if items.none?
-
-      contact_log_recipient = items.first
-      return string_label if contact_log_recipient.success?
-
-      contact_log = contact_log_recipient.contact_log
-      alert_tag = tag.span(class: 'badge badge-danger') { 'Outgoing Email Failed' }
-      content = policy(contact_log).show? ? link_to(alert_tag, contact_log) : alert_tag
+      string_label, content = email_alert_content(record, attribute)
+      return string_label unless content
 
       tag.label do
         safe_join(["#{string_label} ", content])
@@ -47,19 +38,25 @@ module RadCommon
     end
 
     def email_show_item(record, attribute)
-      string_label = translated_attribute_label(record, attribute)
+      string_label, content = email_alert_content(record, attribute)
+      value = record.send(attribute)
 
+      { label: string_label, value: content ? safe_join([value, ' ', content]) : value }
+    end
+
+    def email_alert_content(record, attribute)
+      string_label = translated_attribute_label(record, attribute)
       items = ContactLogRecipient.where(email: record.send(attribute)).sorted.limit(1)
-      return { label: string_label, value: record.send(attribute) } if items.none?
+      return [string_label, nil] if items.none?
 
       contact_log_recipient = items.first
-      return { label: string_label, value: record.send(attribute) } if contact_log_recipient.success?
+      return [string_label, nil] if contact_log_recipient.success?
 
       contact_log = contact_log_recipient.contact_log
       alert_tag = tag.span(class: 'badge badge-danger') { 'Outgoing Email Failed' }
       content = policy(contact_log).show? ? link_to(alert_tag, contact_log) : alert_tag
 
-      { label: string_label, value: safe_join([record.send(attribute), ' ', content]) }
+      [string_label, content]
     end
   end
 end
