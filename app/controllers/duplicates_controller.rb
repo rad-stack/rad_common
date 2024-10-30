5b7dd564d1b212c583f9655835f466e9156ff6e8
garyfoster <gary.foster@radstack.com>
2024-10-30 06:52:17 -0400

    configure notification types to send contact log metadata

--- app/controllers/duplicates_controller.rb
+++ app/controllers/duplicates_controller.rb
@@ -163,10 +163,8 @@ class DuplicatesController < ApplicationController
     end
 
     def email_options
-      { email_action: { message: 'Click here to view the details.',
-                        button_text: 'View',
-                        button_url: url_for(@record) },
+      { email_action: { message: 'Click here to view the details.', button_text: 'View', button_url: url_for(@record) },
         from_user: current_user,
-        record: @record}
+        record: @record }
     end
 end
