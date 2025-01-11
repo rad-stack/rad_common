9f6e15470452515c50de941171b874409b14f7c3
garyfoster <gary.foster@radstack.com>
2025-01-11 08:00:27 -0500

    Enhancements to permanent attachment URLs

--- app/services/rad_common_routes.rb
+++ app/services/rad_common_routes.rb
@@ -88,7 +88,9 @@ module RadCommonRoutes
       get 'attachments/:class_name/:id(.:format)/:variant(.:format)', to: 'rad_common/attachments#download_variant'
       get 'attachments/:id(.:format)', to: 'rad_common/attachments#download'
 
-      get 'attachments/:class_name/:id(.:format)/:variant(.:format)/:filename(.:format)', to: 'rad_common/attachments#download_variant'
+      get 'attachments/:class_name/:id(.:format)/:variant(.:format)/:filename(.:format)',
+          to: 'rad_common/attachments#download_variant'
+
       get 'attachments/:id(.:format)/:filename(.:format)', to: 'rad_common/attachments#download'
 
       get 'global_search', to: 'search#global_search'
