be17a8ae296f0896ed102c3d7785556f834bf2f1
garyfoster <gary.foster@radstack.com>
2025-01-11 06:06:53 -0500

    Enhancements to permanent attachment URLs

--- app/services/attachment_url_generator.rb
+++ app/services/attachment_url_generator.rb
@@ -1,16 +1,22 @@
 class AttachmentUrlGenerator
-  def self.permanent_attachment_variant_url(record, variant)
+  def self.permanent_attachment_variant_url(record, variant, include_filename: false)
     protocol = Rails.env.production? || Rails.env.staging? ? 'https' : 'http'
     host = RadConfig.host_name!
     record_id = Hashable.hashids.encode(record.id)
     class_name = record.class.model_name.route_key
-    "#{protocol}://#{host}/rad_common/attachments/#{class_name}/#{record_id}/#{variant}"
+    item = "#{protocol}://#{host}/attachments/#{class_name}/#{record_id}/#{variant}"
+    return item unless include_filename
+
+    "#{item}/#{record.logo_variant.filename}"
   end
 
-  def self.permanent_attachment_url(attachment)
+  def self.permanent_attachment_url(attachment, include_filename: false)
     protocol = Rails.env.production? || Rails.env.staging? ? 'https' : 'http'
     host = RadConfig.host_name!
     record_id = Hashable.hashids.encode(attachment.id)
-    "#{protocol}://#{host}/rad_common/attachments/#{record_id}"
+    item = "#{protocol}://#{host}/attachments/#{record_id}"
+    return item unless include_filename
+
+    "#{item}/#{attachment.filename}"
   end
 end
