e5703617ac43043a7de3ec217738b27591b0cd7d
garyfoster <gary.foster@radstack.com>
2025-02-20 12:20:40 -0800

    fix sniffs

--- app/services/s3_cors_settings_updater.rb
+++ app/services/s3_cors_settings_updater.rb
@@ -14,28 +14,28 @@ class S3CorsSettingsUpdater
       }
     )
 
-    puts "CORS settings updated for #{bucket} : #{check_cors}"
+    Rails.logger.info "CORS settings updated for #{bucket} : #{check_cors}"
   end
 
   private
 
-  def s3_client
-    @s3_client ||= Aws::S3::Client.new(
-      region: RadConfig.secret_config_item!(:s3_region),
-      access_key_id: RadConfig.secret_config_item!(:s3_access_key_id),
-      secret_access_key: RadConfig.secret_config_item!(:s3_secret_access_key)
-    )
-  end
+    def s3_client
+      @s3_client ||= Aws::S3::Client.new(
+        region: RadConfig.secret_config_item!(:s3_region),
+        access_key_id: RadConfig.secret_config_item!(:s3_access_key_id),
+        secret_access_key: RadConfig.secret_config_item!(:s3_secret_access_key)
+      )
+    end
 
-  def check_cors
-    s3_client.get_bucket_cors(bucket: bucket)
-  end
+    def check_cors
+      s3_client.get_bucket_cors(bucket: bucket)
+    end
 
-  def allowed_origin
-    @allowed_origin ||= "https://#{RadConfig.host_name!}"
-  end
+    def allowed_origin
+      @allowed_origin ||= "https://#{RadConfig.host_name!}"
+    end
 
-  def bucket
-    @bucket ||= RadConfig.secret_config_item!(:s3_bucket)
-  end
+    def bucket
+      @bucket ||= RadConfig.secret_config_item!(:s3_bucket)
+    end
 end
