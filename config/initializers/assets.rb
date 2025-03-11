9ca410dce0c1dda6f8b4247e8bc6bf7e1c5a2b99
Eric Bundy <elbzero@gmail.com>
2025-03-11 13:54:18 -0400

    skip whole file if react app

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,7 @@
+return if RadConfig.react_app?
+
 Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
-# Rails.application.config.assets.excluded_paths << Rails.root.join('app/assets/scss')
+Rails.application.config.assets.excluded_paths << Rails.root.join('app/assets/scss')
 Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts')
 
 if Rake::Task.task_defined?('assets:precompile')
