b542efb31ffeec86cf9c63f5b96e6b53a1f160e0
garyfoster <gary.foster@radstack.com>
2025-03-28 06:03:12 -0400

    updates after merge

--- app/services/rad_audit_search.rb
+++ app/services/rad_audit_search.rb
@@ -1,6 +1,7 @@
 class RadAuditSearch < RadCommon::Search
   def initialize(params, current_user)
     @current_user = current_user
+    @params = params
 
     super(query: RadAudit,
           filters: filters_def,
@@ -9,25 +10,52 @@ class RadAuditSearch < RadCommon::Search
           current_user: current_user)
   end
 
+  def single_record?
+    params.dig(:search, :single_record).present?
+  end
+
+  def single_record
+    return unless single_record?
+
+    params[:search][:single_record].split(':').first.constantize.find(params[:search][:single_record].split(':').last)
+  end
+
   private
 
+    def query_def
+      if single_record?
+        RadAudit.where(auditable: single_record).or(RadAudit.where(associated: single_record))
+      else
+        RadAudit
+      end
+    end
+
+    def default_date
+      single_record? ? nil : Date.current
+    end
+
     def filters_def
-      [{ start_input_label: 'Start Date',
-         end_input_label: 'End Date',
-         column: :created_at,
-         type: RadCommon::DateFilter },
-       { input_label: 'Record Type',
-         column: :auditable_type,
-         options: RadCommon::AppInfo.new.audited_models },
-       { input_label: 'User',
-         column: :user_id,
-         options: user_array },
-       { input_label: 'Action',
-         column: :action,
-         options: %w[create update destroy] },
-       { column: :remote_address, type: RadCommon::LikeFilter },
-       { column: :auditable_id, type: RadCommon::EqualsFilter, data_type: :integer },
-       { column: :audited_changes, type: RadCommon::LikeFilter }]
+      items = [{ start_input_label: 'Start Date',
+                 end_input_label: 'End Date',
+                 column: :created_at,
+                 type: RadCommon::DateFilter }]
+
+      unless single_record?
+        items += [{ input_label: 'Record Type',
+                    column: :auditable_type,
+                    options: RadCommon::AppInfo.new.audited_models },
+                  { column: :auditable_id,
+                    type: RadCommon::EqualsFilter,
+                    data_type: :integer,
+                    input_label: 'Record ID' }]
+      end
+
+      items + [{ input_label: 'User', column: :user_id, options: user_array },
+               { input_label: 'Action',
+                 column: :action,
+                 options: %w[create update destroy] },
+               { column: :remote_address, type: RadCommon::LikeFilter },
+               { column: :audited_changes, type: RadCommon::LikeFilter }]
     end
 
     def sort_columns_def
@@ -42,6 +70,6 @@ class RadAuditSearch < RadCommon::Search
     end
 
     def user_array
-      Pundit.policy_scope!(current_user, User).by_name.pluck(Arel.sql("first_name || ' ' || last_name"), :id)
+      Pundit.policy_scope!(current_user, User).sorted.pluck(Arel.sql("first_name || ' ' || last_name"), :id)
     end
 end
