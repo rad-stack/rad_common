aa542b2bcff4d9681f24d77c5a7a948bc34dd20c
garyfoster <gary.foster@radstack.com>
2025-03-29 06:34:14 -0400

    updates

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -1,24 +1,17 @@
 class NotificationSettingsController < ApplicationController
+  before_action :set_notification_type, only: :create
+
   def index
     authorize NotificationSetting
     skip_policy_scope
   end
 
   def create
-    notification_type = NotificationType.find_by(id: permitted_params[:notification_type_id])
-
-    if notification_type.blank?
-      skip_authorization
-      # TODO: convert json to turbo
-      render json: { error: 'Invalid parameters' }, status: :unprocessable_entity
-      return
-    end
-
-    notification_setting = NotificationSetting.init_for_user(notification_type, User.find(permitted_params[:user_id]))
+    notification_setting = NotificationSetting.init_for_user(@notification_type, User.find(permitted_params[:user_id]))
     notification_setting.enabled = permitted_params[:enabled]
-    notification_setting.email = permitted_params[:email] if notification_type.email_enabled?
-    notification_setting.feed = permitted_params[:feed] if notification_type.feed_enabled?
-    notification_setting.sms = permitted_params[:sms] if RadConfig.twilio_enabled? && notification_type.sms_enabled?
+    notification_setting.email = permitted_params[:email] if @notification_type.email_enabled?
+    notification_setting.feed = permitted_params[:feed] if @notification_type.feed_enabled?
+    notification_setting.sms = permitted_params[:sms] if RadConfig.twilio_enabled? && @notification_type.sms_enabled?
 
     authorize notification_setting
 
@@ -28,14 +21,19 @@ class NotificationSettingsController < ApplicationController
                        can_update: true,
                        toast_success: 'The setting was successfully saved.' }
     else
-      # TODO: convert json to turbo
-      render json: { error:
-                         "The setting could not be saved: #{notification_setting.errors.full_messages.join(', ')}" },
-             status: :unprocessable_entity
+      message = "The setting could not be saved: #{notification_setting.errors.full_messages.join(', ')}"
+
+      render partial: 'notification_settings/toggle_enabled',
+             locals: { notification_setting: notification_setting, can_update: true, toast_error: message }
     end
   end
 
   private
+
+    def set_notification_type
+      @notification_type = NotificationType.find_by(id: permitted_params[:notification_type_id])
+      raise 'Invalid parameters' if @notification_type.blank?
+    end
 
     def permitted_params
       params.require(:notification_setting).permit(:notification_type_id, :enabled, :user_id, :email, :feed, :sms)
