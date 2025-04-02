d14278d26175491a4273692886ee19192f01b7bd
garyfoster <gary.foster@radstack.com>
2025-04-02 04:48:58 -0400

    updates

--- spec/dummy/db/migrate/20250402083306_add_sms_message_id_index.rb
+++ spec/dummy/db/migrate/20250402083306_add_sms_message_id_index.rb
@@ -1,4 +1,4 @@
-class AddSMSMessageIdIndex < ActiveRecord::Migration[7.2]
+class AddSMSMessageIdIndex < ActiveRecord::Migration[7.0]
   def change
     add_index :contact_logs, :sms_message_id
   end
