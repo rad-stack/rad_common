5035d9a893e1182a6b9bdaaa5b8368ce8edcb977
garyfoster <gary.foster@radstack.com>
2025-04-26 14:09:56 -0700

    updates

--- spec/shared/system/invitations_spec.rb
+++ spec/shared/system/invitations_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Invitations', type: :system, invite_specs: true do
+RSpec.describe 'Invitations', :invite_specs, type: :system do
   let(:company) { Company.main }
   let(:admin) { create :admin }
   let(:user) { create :user }
@@ -8,26 +8,20 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
   let(:external_domain) { 'abc.com' }
   let(:first_name) { Faker::Name.first_name }
   let(:last_name) { Faker::Name.last_name }
+  let(:name_display) { RadConfig.last_first_user? ? "#{last_name}, #{first_name}" : "#{first_name} #{last_name}" }
   let(:valid_email) { "#{Faker::Internet.user_name}@#{email_domain}" }
   let(:external_email) { "#{Faker::Internet.user_name}@#{external_domain}" }
   let!(:internal_role) { create :security_role, allow_invite: true }
-  let!(:external_role) { create :security_role, :external, allow_invite: true }
+  let(:external_role) { create :security_role, :external, allow_invite: true }
+  let(:open_invite_error) { "There is an open invitation for this user: #{invitee.email}" }
 
-  let(:invite_message) do
-    if Devise.paranoid
-      'If your email address exists in our database, you will receive a password'
-    else
-      'not found'
-    end
-  end
-
-  before { allow_any_instance_of(User).to receive(:twilio_verify_enabled?).and_return false }
+  before { external_role if RadConfig.external_users? }
 
   describe 'user' do
     before { login_as user, scope: :user }
 
     describe 'new' do
-      xit 'does not allow' do
+      it 'does not allow' do
         visit new_user_invitation_path
         expect(page).to have_content 'Access Denied'
       end
@@ -39,55 +33,83 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
 
     describe 'new' do
       context 'when valid' do
-        xit 'invites a user' do
+        let(:another_role) { create :security_role, :external, allow_invite: true }
+        let(:multiple_roles) { false }
+        let(:all_users) { true }
+
+        before do
+          allow(RadConfig).to receive(:twilio_verify_all_users?).and_return(all_users)
+
+          if multiple_roles
+            another_role
+            create :security_role, :external, allow_invite: true
+          end
+
           visit new_user_invitation_path
 
-          select internal_role.name, from: 'Initial security role'
-          fill_in 'Email', with: valid_email
-          fill_in 'First name', with: first_name
-          fill_in 'Last name', with: last_name
+          select invite_role.name, from: 'Initial Security Role' if RadConfig.external_users?
+          fill_in 'Email', with: invite_email
+          fill_in 'First Name', with: first_name
+          fill_in 'Last Name', with: last_name
           fill_in 'Mobile Phone', with: '(999) 231-1111'
           click_button 'Send'
 
-          expect(page).to have_content "We invited '#{first_name} #{last_name}'"
-          expect(User.last.security_roles.first).to eq internal_role
-          expect(User.last.internal?).to be true
+          expect(page).to have_content "We invited '#{name_display}'"
+          expect(User.last.security_roles.first).to eq invite_role
+          expect(User.last.active?).to be true
         end
 
-        xit 'invites an external user', external_user_specs: true do
-          visit new_user_invitation_path
+        context 'with internal user' do
+          let(:invite_role) { internal_role }
+          let(:invite_email) { valid_email }
 
-          select external_role.name, from: 'Initial security role'
-          fill_in 'Email', with: external_email
-          fill_in 'First name', with: first_name
-          fill_in 'Last name', with: last_name
-          fill_in 'Mobile Phone', with: '(999) 231-1111'
-          click_button 'Send'
-
-          expect(page).to have_content "We invited '#{first_name} #{last_name}'"
-          expect(User.last.security_roles.first).to eq external_role
-          expect(User.last.external?).to be true
+          xit 'invites' do
+            expect(User.last.internal?).to be true
+            expect(User.last.twilio_verify_enabled?).to be true
+          end
         end
 
-        it 'invites an external user and sets initial role', external_user_specs: true do
-          create :security_role, :external, allow_invite: true
-          initial_role = create :security_role, :external, allow_invite: true
+        context 'with twilio verify off' do
+          let(:invite_role) { internal_role }
+          let(:invite_email) { valid_email }
+          let(:all_users) { false }
 
-          visit new_user_invitation_path
+          xit 'invites' do
+            expect(User.last.twilio_verify_enabled?).to be false
+          end
+        end
 
-          select initial_role.name, from: 'Initial security role'
-          fill_in 'Email', with: external_email
-          fill_in 'First name', with: first_name
-          fill_in 'Last name', with: last_name
-          fill_in 'Mobile Phone', with: '(999) 231-1111'
-          click_button 'Send'
+        context 'with external user' do
+          let(:invite_role) { external_role }
+          let(:invite_email) { external_email }
 
-          expect(page).to have_content "We invited '#{first_name} #{last_name}'"
-          expect(User.last.security_roles.first).to eq initial_role
+          xit 'invites', :external_user_specs do
+            expect(User.last.external?).to be true
+          end
+        end
+
+        context 'with multiple external roles' do
+          let(:multiple_roles) { true }
+          let(:invite_role) { another_role }
+          let(:invite_email) { external_email }
+
+          xit 'invites an external user and sets initial role', :external_user_specs do
+            expect(SecurityRole.external.size).to eq 3
+          end
         end
       end
 
       context 'when invalid' do
+        xit 'because of blank email' do
+          visit new_user_invitation_path
+
+          fill_in 'First Name', with: first_name
+          fill_in 'Last Name', with: last_name
+          click_button 'Send an invitation'
+
+          expect(page).to have_content "Email can't be blank"
+        end
+
         it 'because of blank first and last name' do
           visit new_user_invitation_path
 
@@ -103,8 +125,8 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
 
           bad_email = 'j@g.com'
           fill_in 'Email', with: bad_email
-          fill_in 'First name', with: first_name
-          fill_in 'Last name', with: last_name
+          fill_in 'First Name', with: first_name
+          fill_in 'Last Name', with: last_name
           fill_in 'Mobile Phone', with: '(999) 231-1111'
           click_button 'Send an invitation'
 
@@ -115,12 +137,13 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
           visit new_user_invitation_path
 
           fill_in 'Email', with: valid_email
-          fill_in 'First name', with: 'f'
-          fill_in 'Last name', with: 'b'
+          fill_in 'First Name', with: 'f'
+          fill_in 'Last Name', with: 'b'
           fill_in 'Mobile Phone', with: '(999) 231-1111'
           click_button 'Send an invitation'
 
-          expect(page).to have_content "We invited 'f b'"
+          name_display = RadConfig.last_first_user? ? 'b, f' : 'f b'
+          expect(page).to have_content "We invited '#{name_display}'"
         end
       end
     end
@@ -130,12 +153,12 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
         visit new_user_invitation_path
 
         fill_in 'Email', with: valid_email
-        fill_in 'First name', with: first_name
-        fill_in 'Last name', with: last_name
+        fill_in 'First Name', with: first_name
+        fill_in 'Last Name', with: last_name
         fill_in 'Mobile Phone', with: '(999) 231-1111'
         click_button 'Send'
 
-        expect(page).to have_content "We invited '#{first_name} #{last_name}'"
+        expect(page).to have_content "We invited '#{name_display}'"
 
         visit user_path(User.last)
         click_link 'Resend Invitation'
@@ -153,38 +176,38 @@ RSpec.describe 'Invitations', type: :system, invite_specs: true do
                    initial_security_role_id: internal_role.id)
     end
 
-    it 'does not allow invitee to reset password after invite expires' do
-      expect(invitee.errors.count).to eq(0)
-      invitee.invitation_created_at = 3.weeks.ago
-      invitee.invitation_sent_at = 3.weeks.ago
-      invitee.save!
+    let(:mail) { ActionMailer::Base.deliveries.last }
 
-      visit new_user_password_path
-      fill_in 'Email', with: invitee.email
-      click_button 'Send Me Reset Password Instructions'
-      expect(page).to have_content invite_message
+    before do
+      admin
+      ActionMailer::Base.deliveries.clear
     end
 
     it 'notifies admin when invitee accepts' do
-      ActionMailer::Base.deliveries = []
-      create :user_accepts_invitation_notification, security_roles: [admin.security_roles.first]
-
       invitee.accept_invitation!
 
-      mail = ActionMailer::Base.deliveries.last
       expect(mail.subject).to include 'Accepted'
       expect(mail.to).to include admin.email
     end
 
-    it "doesn't let unaccepted invitee reset password" do
-      ActionMailer::Base.deliveries = []
+    context 'when user is struggling' do
+      xit "doesn't let unaccepted invitee reset password" do
+        visit new_user_password_path
+        fill_in 'Email', with: invitee.email
+        click_on 'Send Me Reset Password Instructions'
 
-      visit new_user_password_path
-      fill_in 'Email', with: invitee.email
-      click_button 'Send Me Reset Password Instructions'
-      expect(page).to have_content invite_message
+        expect(mail.subject).to include 'User Has Open Invitation'
+        expect(mail.to).to include admin.email
+      end
 
-      expect(ActionMailer::Base.deliveries.count).to eq 0
+      xit "doesn't let unaccepted invitee request confirmation instructions" do
+        visit new_user_confirmation_path
+        fill_in 'Email', with: invitee.email
+        click_on 'Resend Confirmation Instructions'
+
+        expect(mail.subject).to include 'User Has Open Invitation'
+        expect(mail.to).to include admin.email
+      end
     end
   end
 end
