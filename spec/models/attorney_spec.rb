0b39e5f7f9963eef200f2d1f3161a3083fe93224
Brock Denson <brockd9401@gmail.com>
2025-04-29 18:15:43 -0400

    Whitespace

--- spec/models/attorney_spec.rb
+++ spec/models/attorney_spec.rb
@@ -6,10 +6,7 @@ RSpec.describe Attorney do
     let(:rich_text_audit) { Audited::Audit.where(auditable_type: 'ActionText::RichText').last }
 
     it 'creates audits for the attorney and the rich text' do
-      expect {
-        attorney.update!(notes: 'Rich text content')
-      }.to change(Audited::Audit, :count).by(2)
-
+      expect { attorney.update!(notes: 'Rich text content') }.to change(Audited::Audit, :count).by(2)
       expect(rich_text_audit.auditable.record).to eq(attorney)
       expect(rich_text_audit.audited_changes['body']).to include('Rich text content')
       expect(rich_text_audit.audited_changes['body']).to include('<div class="trix-content">')
