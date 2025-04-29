dee2931f04f77d4dcaa5440c026f1debc61e5f6c
Brock Denson <brockd9401@gmail.com>
2025-04-29 18:13:12 -0400

    Formatting fixes

--- spec/models/attorney_spec.rb
+++ spec/models/attorney_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Attorney do
   describe 'auditing rich text fields' do
-    subject(:update_notes) { attorney.update!(notes: 'Rich text content') }
-
     let(:attorney) { create :attorney }
+    let(:rich_text_audit) { Audited::Audit.where(auditable_type: 'ActionText::RichText').last }
 
     it 'creates audits for the attorney and the rich text' do
-      expect { update_notes }.to change(Audited::Audit, :count).by(2)
-
-      rich_text_audit = Audited::Audit.where(auditable_type: 'ActionText::RichText').last
+      expect {
+        attorney.update!(notes: 'Rich text content')
+      }.to change(Audited::Audit, :count).by(2)
 
       expect(rich_text_audit.auditable.record).to eq(attorney)
       expect(rich_text_audit.audited_changes['body']).to include('Rich text content')
