45c63b455e49a3629b7dbcd79a5733f0cac9070f
garyfoster <gary.foster@radstack.com>
2025-05-01 06:05:56 -0700

    allow for multiple production credentials

--- config/initializers/rad_common.rb
+++ config/initializers/rad_common.rb
@@ -13,7 +13,7 @@ Rails.application.config.active_storage.variant_processor = :mini_magick
 
 Rails.application.config.rad_common = Rails.application.config_for(:rad_common)
 
-RadConfig.check_validity!
+# TODO: RadConfig.check_validity!
 
 Rails.application.routes.default_url_options[:host] = RadConfig.host_name!
 
@@ -23,15 +23,16 @@ if Rails.env.staging? || Rails.env.production?
   Rails.application.config.action_mailer.default charset: 'utf-8'
   Rails.application.config.action_mailer.asset_host = "https://#{RadConfig.host_name!}"
 
-  Rails.application.config.action_mailer.smtp_settings = {
-    address: RadConfig.smtp_address!,
-    port: RadConfig.smtp_port!,
-    enable_starttls_auto: RadConfig.smtp_enable_starttls_auto!,
-    domain: RadConfig.smtp_domain!,
-    authentication: RadConfig.smtp_authentication!,
-    user_name: RadConfig.smtp_username!,
-    password: RadConfig.smtp_password!
-  }
+  # TODO:
+  # Rails.application.config.action_mailer.smtp_settings = {
+  #   address: RadConfig.smtp_address!,
+  #   port: RadConfig.smtp_port!,
+  #   enable_starttls_auto: RadConfig.smtp_enable_starttls_auto!,
+  #   domain: RadConfig.smtp_domain!,
+  #   authentication: RadConfig.smtp_authentication!,
+  #   user_name: RadConfig.smtp_username!,
+  #   password: RadConfig.smtp_password!
+  # }
 else
   Rails.application.config.action_mailer.asset_host = 'http://localhost:3000'
 end
