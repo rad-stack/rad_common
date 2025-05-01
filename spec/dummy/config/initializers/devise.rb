d8d3cb5efd2627ab7923e359b4fae3b8b5c33813
garyfoster <gary.foster@radstack.com>
2025-05-01 06:30:52 -0700

    allow for multiple production credentials

--- spec/dummy/config/initializers/devise.rb
+++ spec/dummy/config/initializers/devise.rb
@@ -23,7 +23,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'foo@example.com' # TODO: RadConfig.from_email!
+  config.mailer_sender = RadConfig.from_email!
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
