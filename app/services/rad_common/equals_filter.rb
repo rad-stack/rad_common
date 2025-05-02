7155990b0ab6e27ac14c1b4a7105073143796952
garyfoster <gary.foster@radstack.com>
2025-05-02 16:04:23 -0700

    add col_class option to search framework

--- app/services/rad_common/equals_filter.rb
+++ app/services/rad_common/equals_filter.rb
@@ -2,20 +2,21 @@ module RadCommon
   ##
   # This is used to generate an input used for a SQL like filter
   class EqualsFilter
-    attr_reader :column, :data_type
+    attr_reader :column, :data_type, :col_class
 
     ##
     # @param [String] column the database column that is being filtered
     # @param [Symbol] data_type controls what the input type is based on data type of column
     # @param [Symbol] scope the name of an active record scope to be used for the filter on the corresponding model
     # @param [String optional] input_label by default the input label for the field is determined by the column name
-    def initialize(column:, data_type:, scope: nil, input_label: nil)
+    def initialize(column:, data_type:, scope: nil, input_label: nil, col_class: nil)
       raise 'data_type must be either :integer or :string' if supported_data_types.exclude?(data_type)
 
       @column = column
       @data_type = data_type
       @scope = scope
       @input_label = input_label
+      @col_class = col_class
     end
 
     def filter_view
