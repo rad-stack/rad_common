1a52ca1af853a7c86d360bc911f8796ee86cc99e
garyfoster <gary.foster@radstack.com>
2025-05-02 16:08:19 -0700

    add col_class option to search framework

--- app/services/rad_audit_search.rb
+++ app/services/rad_audit_search.rb
@@ -56,7 +56,8 @@ class RadAuditSearch < RadCommon::Search
       items + [{ input_label: 'User', column: :user_id, options: user_array },
                { input_label: 'Action',
                  column: :action,
-                 options: %w[create update destroy] },
+                 options: %w[create update destroy],
+                 col_class: 'col-lg-2'},
                { column: :remote_address, type: RadCommon::LikeFilter },
                { column: 'audited_changes::TEXT', type: RadCommon::LikeFilter, name: :audited_changes }]
     end
