bf532344669ce95441860e95ab6176092f39af04
Eric Bundy <elbzero@gmail.com>
2025-05-07 12:57:27 -0400

    update

--- spec/system/rad_common/search_spec.rb
+++ spec/system/rad_common/search_spec.rb
@@ -147,7 +147,7 @@ RSpec.describe 'Search' do
       expect(page).to have_no_content 'Invalid date entered for created_at'
     end
 
-    it 'does save valid date to users.filter_defaults' do
+    it 'does save valid date to users.filter_defaults', :js do
       visit divisions_path(search: { created_at_start: '2019-12-01', created_at_end: '2019-12-02',
                                      division_status: [1] })
       visit '/'
