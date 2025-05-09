8595e967258d2b5aca9d538b121318d27ffffa0a
garyfoster <gary.foster@radstack.com>
2025-05-09 06:57:35 -0700

    updates

--- spec/services/phone_number_formatter_spec.rb
+++ spec/services/phone_number_formatter_spec.rb
@@ -1,28 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumberFormatter, type: :service do
-  let(:valid_phone_formats) do
-    ['1233211234', '123-321-1234', '123 321 1234', ' 123 321 1234 ', '+11233211234',
-     '(123) 321-1234', '123-321-1234', '1-123-321-1234',
-     '21233211234', '+1 (123) 321-1234', '(123)321-1234',
-     '123.321.1234', '1.123.321.1234']
-  end
-
-  let(:invalid_phone_formats) do
-    ['foo bar', 'foo bar 1', 'foo bar 1234567890', '123-321-1234 ext 5', '123-321-1234 foo bar']
-  end
-
   describe 'format' do
     it 'formats phone numbers correctly' do
-      valid_phone_formats.each do |phone_number|
-        expect(described_class.format(phone_number)).to eq '(123) 321-1234'
-      end
-    end
-
-    it 'ignores non-phone numbers' do
-      invalid_phone_formats.each do |phone_number|
-        expect(described_class.format(phone_number)).to be_nil
-      end
+      expect(described_class.format('(999)999-9999')).to eq '(999) 999-9999'
+      expect(described_class.format('999999-9999')).to eq '(999) 999-9999'
+      expect(described_class.format('9999999999')).to eq '(999) 999-9999'
+      expect(described_class.format(9_999_999_999)).to eq '(999) 999-9999'
+      expect(described_class.format('999-999-9999')).to eq '(999) 999-9999'
     end
   end
 end
