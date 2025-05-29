50e9f005aa91d66c5b7b2b97a5f34e149317a81d
Eric Bundy <elbzero@gmail.com>
2025-05-29 12:04:56 -0400

    adding rad api client

---
+++ app/services/rad_api_client.rb
@@ -0,0 +1,50 @@
+class RadApiClient
+  class UnsuccessfulRequest < StandardError; end
+
+  def initialize(host:, token: nil)
+    @token = token
+    @host = host
+  end
+
+  def post(url:, payload: nil)
+    process_response(connection.post(url, payload&.to_json))
+  end
+
+  def patch(url:, payload: nil)
+    process_response(connection.patch(url, payload&.to_json))
+  end
+
+  def delete(url:)
+    process_response(connection.delete(url))
+  end
+
+  def get(url:, params: {})
+    RadRetry.perform_request(additional_errors: [UnsuccessfulRequest]) do
+      process_response(connection(params: params).get(url))
+    end
+  end
+
+  private
+
+  def connection(params: {})
+    Faraday.new(
+      url: HOST,
+      params: params,
+      headers: default_headers
+    )
+  end
+
+  def default_headers
+    headers = {}
+    headers[:Authorization] = "Bearer #{@token}"
+    headers[:Accept] = 'application/json'
+    headers['Content-Type'] = 'application/json'
+    headers
+  end
+
+  def process_response(response)
+    raise UnsuccessfulRequest, response.body unless response.success?
+
+    JSON.parse(response.body) if response.body.present?
+  end
+end
