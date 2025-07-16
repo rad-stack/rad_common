4ffcaa435a9f4a736fa205daa302d26dee04ce07
Eric Bundy <elbzero@gmail.com>
2025-07-16 14:22:40 -0400

    try adding caching in

--- app/controllers/concerns/active_storage_downloader.rb
+++ app/controllers/concerns/active_storage_downloader.rb
@@ -30,6 +30,8 @@ module ActiveStorageDownloader
     def serve_file(url, ext, content_type, filename)
       # crashes locally, only use for production
       data = RadRetry.perform_request(retry_count: 2) { URI.parse(url).open }
+      response.headers['Cache-Control'] = 'public, max-age=2592000'
+      response.headers['ETag'] = Digest::MD5.hexdigest(file.read)
       send_data data.read, filename: "#{filename}.#{ext}", type: content_type, disposition: 'inline'
     end
 end
