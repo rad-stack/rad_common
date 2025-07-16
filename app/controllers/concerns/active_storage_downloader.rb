edc590d6a782d118a120690fa3035de6cddd8179
Eric Bundy <elbzero@gmail.com>
2025-07-16 14:27:01 -0400

    fix

--- app/controllers/concerns/active_storage_downloader.rb
+++ app/controllers/concerns/active_storage_downloader.rb
@@ -31,7 +31,7 @@ module ActiveStorageDownloader
       # crashes locally, only use for production
       data = RadRetry.perform_request(retry_count: 2) { URI.parse(url).open }
       response.headers['Cache-Control'] = 'public, max-age=2592000'
-      response.headers['ETag'] = Digest::MD5.hexdigest(file.read)
+      response.headers['ETag'] = Digest::MD5.hexdigest(data)
       send_data data.read, filename: "#{filename}.#{ext}", type: content_type, disposition: 'inline'
     end
 end
