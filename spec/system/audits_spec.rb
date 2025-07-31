8dd75a30eed7804a6f6357b2be261827975df5b2
vincentdinic <vincentdinic@gmail.com>
2025-07-31 11:34:49 -0400

    Move caching spec to audits_spec.rb

--- spec/system/audits_spec.rb
+++ spec/system/audits_spec.rb
@@ -28,5 +28,20 @@ describe 'Audits' do
       expect(page).to have_content '(2)'
       expect(page).to have_content 'Queued'
     end
+
+    context 'with caching' do
+      before do
+        RadCommon::ApplicationHelper.instance_variable_set(:@show_routes, nil)
+      end
+
+      it 'only runs show routes method once' do
+        expect_any_instance_of(RadCommon::AppInfo).to receive(:show_routes).exactly(1).time.and_call_original
+        admin.update!(first_name: 'John')
+
+        visit audits_path
+        expect(page).to have_content('Changed First Name')
+        expect(page).to have_content('Changed Last Sign In')
+      end
+    end
   end
 end
