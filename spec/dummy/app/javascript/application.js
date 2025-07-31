2d2db791fa119d5554bc193640cc1c1e82a4736c
vincentdinic <vincentdinic@gmail.com>
2025-07-31 11:11:55 -0400

    Add trend graphs to the system usages table

--- spec/dummy/app/javascript/application.js
+++ spec/dummy/app/javascript/application.js
@@ -7,5 +7,6 @@ import 'rad_common_js/src/jqueryAreYouSure';
 import { RadCommon } from 'rad_common_js/src/radCommon';
 RadCommon.setup();
 
+import 'chartkick/chart.js';
 import './app_specific.js';
 import './controllers';
