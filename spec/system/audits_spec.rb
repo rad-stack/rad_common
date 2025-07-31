410308a2244476cc695c3145128639c3c3639cd5
garyfoster <gary.foster@radstack.com>
2025-07-31 12:08:09 -0400

    updates

--- spec/system/audits_spec.rb
+++ spec/system/audits_spec.rb
@@ -29,12 +29,10 @@ describe 'Audits' do
       expect(page).to have_content 'Queued'
     end
 
-    context 'with caching' do
-      before do
-        RadCommon::ApplicationHelper.instance_variable_set(:@show_routes, nil)
-      end
+    describe 'show_routes' do
+      before { RadCommon::ApplicationHelper.instance_variable_set(:@show_routes, nil) }
 
-      it 'only runs show routes method once' do
+      it 'is cached at startup and runs only once' do
         expect_any_instance_of(RadCommon::AppInfo).to receive(:show_routes).exactly(1).time.and_call_original
         admin.update!(first_name: 'John')
 
