72d65ec34e260d63c37f13bc4fd84d6661fd4cf6
garyfoster <gary.foster@radstack.com>
2025-08-01 11:00:51 -0400

    automate app creation/updating with application templates

---
+++ lib/generators/app_template.rb
@@ -0,0 +1,6 @@
+gem 'bootsnap', require: false
+gem 'propshaft'
+gem 'rad_common', git: 'https://github.com/rad-stack/rad_common.git', branch: 'rad-964-bootstrap5'
+
+gem 'devise-twilio-verify', git: 'https://github.com/rad-stack/twilio-verify-devise.git',
+                            branch: 'authy-to-twilio-verify'
