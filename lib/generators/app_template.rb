fe808ded8a8e00191be1fbf3a5ee2589a57ed260
Eric Bundy <elbzero@gmail.com>
2025-08-01 11:32:57 -0400

    updating with claude generated

--- lib/generators/app_template.rb
+++ lib/generators/app_template.rb
@@ -1,6 +1,150 @@
-gem 'bootsnap', require: false
-gem 'propshaft'
-gem 'rad_common', git: 'https://github.com/rad-stack/rad_common.git', branch: 'rad-964-bootstrap5'
+# Rails Application Template for rad_common setup
 
-gem 'devise-twilio-verify', git: 'https://github.com/rad-stack/twilio-verify-devise.git',
-                            branch: 'authy-to-twilio-verify'
+# Add gems to Gemfile
+gem_group :default do
+  gem 'bootsnap', require: false
+  gem 'propshaft'
+  gem 'rad_common', git: 'https://github.com/rad-stack/rad_common.git', branch: 'main'
+
+  # TODO: remove this when possible
+  gem 'devise-twilio-verify', git: 'https://github.com/rad-stack/twilio-verify-devise.git',
+      branch: 'authy-to-twilio-verify'
+
+  # project specific gems
+  gem 'acts-as-taggable-on'
+  gem 'bootstrap-select-rails'
+  gem 'faraday-retry' # https://app.radstack.com/tasks/20
+  gem 'haml_lint', '0.55.0'
+  gem 'holidays'
+  gem 'jbuilder'
+  gem 'octokit'
+  gem 'omniauth-github'
+  gem 'omniauth-rails_csrf_protection'
+  gem 'pathspec'
+  gem 'quickbooks-ruby'
+  gem 'recaptcha'
+  gem 'rexml'
+  gem 'rubocop'
+  gem 'rubocop-capybara'
+  gem 'rubocop-factory_bot'
+  gem 'rubocop-rails'
+  gem 'rubocop-rspec'
+  gem 'sidekiq-limit_fetch'
+  gem 'stripe', '~> 5.17.0'
+end
+
+gem_group :development do
+  gem 'active_record_doctor'
+  gem 'better_errors'
+  gem 'tty-prompt'
+  gem 'binding_of_caller'
+end
+
+gem_group :test do
+  gem 'capybara'
+  gem 'capybara-selenium'
+  gem 'parallel_tests'
+  gem 'selenium-webdriver'
+  gem 'simplecov', require: false
+  gem 'timecop'
+  gem 'vcr'
+  gem 'webmock'
+end
+
+gem_group :development, :test do
+  gem 'pry-rails'
+  gem 'rspec-rails'
+end
+
+# Run bundle install
+after_bundle do
+  # Copy rad_common config
+  get 'https://raw.githubusercontent.com/rad-stack/rad_common/refs/heads/rad-10334-app-templates/spec/dummy/config/rad_common.yml', 'config/rad_common.yml'
+
+  # Generate devise
+  generate 'devise:install'
+  generate 'devise', 'User'
+
+  # Uncomment all lines in the devise User migration
+  devise_migration = Dir['db/migrate/*devise_create_users.rb'].first
+  if devise_migration
+    uncomment_lines devise_migration, /^\s*#\s*t\./
+    uncomment_lines devise_migration, /^\s*#\s*add_index/
+  end
+
+  # Generate audited
+  generate 'audited:install'
+
+  # Install Active Storage
+  rails_command 'active_storage:install'
+
+  # Run database setup
+  rails_command 'db:create'
+  rails_command 'db:migrate'
+
+  # Set up binstubs
+  run 'bundle binstubs rad_common --force'
+
+  # Generate rad_common install
+  generate 'rad_common:install'
+
+  # Copy development credentials key
+  copy_file 'examples/development.key', 'config/credentials/development.key'
+
+  # Create seeder service
+  create_file 'app/services/seeder.rb', <<~RUBY
+    class Seeder < RadSeeder
+      def seed
+        # Add initial data here
+      end
+    end
+  RUBY
+
+  # Create blank app_specific.js
+  create_file 'app/javascript/app_specific.js', ''
+
+  # Create nav service
+  create_file 'app/services/nav.rb', <<~RUBY
+    class Nav < RadNav::Nav
+      def top_nav_items
+        [top_nav_item('Home', '/')]
+      end
+    end
+  RUBY
+
+  # Copy assets
+  copy_file 'examples/app_logo.png', 'app/assets/images/app_logo.png'
+  copy_file 'examples/favicon.ico', 'app/assets/images/favicon.ico'
+
+  # Remove default layout (we'll use rad_common's)
+  remove_file 'app/views/layouts/application.html.erb'
+
+  # Update User model
+  gsub_file 'app/models/user.rb', /devise :.*/, ''
+  gsub_file 'app/models/user.rb', /class User < ApplicationRecord/, <<~RUBY.strip
+    class User < ApplicationRecord
+      include RadDeviseHigh
+      include RadUser
+  RUBY
+
+  # Update ApplicationController
+  gsub_file 'app/controllers/application_controller.rb', /class ApplicationController < ActionController::Base/, <<~RUBY.strip
+    class ApplicationController < ActionController::Base
+      include RadController
+  RUBY
+
+  # Create ApplicationPolicy
+  create_file 'app/policies/application_policy.rb', <<~RUBY
+    class ApplicationPolicy < RadPolicy
+    end
+  RUBY
+
+  # Install yarn dependencies and build
+  run 'yarn install'
+  run 'yarn build'
+
+  # Seed the database
+  rails_command 'db:seed'
+
+  say 'Rails application with rad_common setup complete!', :green
+end
