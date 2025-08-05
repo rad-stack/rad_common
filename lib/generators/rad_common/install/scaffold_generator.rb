55eb80b9999eea794e93be0f86af2b4fe417d040
vincentdinic <vincentdinic@gmail.com>
2025-08-05 12:16:06 -0400

    Add to generator

--- lib/generators/rad_common/install/scaffold_generator.rb
+++ lib/generators/rad_common/install/scaffold_generator.rb
@@ -12,6 +12,27 @@ module Rspec
         template Rails.root.join('lib/templates/rspec/system/system_spec.rb.tt'),
                  File.join('spec/system', class_path, "#{file_name.pluralize}_spec.rb")
       end
+
+      def create_factory
+        if defined?(FactoryBot)
+          template Rails.root.join('lib/templates/factory_bot/factory.rb.tt'),
+                   File.join('spec/factories', class_path, "#{file_name.pluralize}.rb")
+        end
+      end
+
+      def create_policy
+        if defined?(Pundit)
+          template Rails.root.join('lib/templates/pundit/policy.rb.tt'),
+                   File.join('app/policies', class_path, "#{file_name}.rb")
+        end
+      end
+    
+      def create_policy_spec
+        if defined?(Pundit)
+          template Rails.root.join('lib/templates/pundit/policy_spec.rb.tt'),
+                   File.join('spec/policies', class_path, "#{file_name}.rb")
+        end
+      end
     end
   end
 end
