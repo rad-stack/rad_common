96db811d2418970267296d6df930f1d80b9d2153
vincentdinic <vincentdinic@gmail.com>
2025-08-05 16:20:56 -0400

    Remove policy spec

--- lib/generators/rad_common/install/install_generator.rb
+++ lib/generators/rad_common/install/install_generator.rb
@@ -171,11 +171,9 @@ module RadCommon
                   'lib/templates/rspec/system/system_spec.rb.tt'
         remove_file 'lib/templates/rspec/system/system_spec.rb' # Removed old non-TT file
 
-        # pundit templates
+        # pundit template
         copy_file '../../../../../spec/dummy/lib/templates/pundit/policy.rb.tt',
                   'lib/templates/pundit/policy.rb.tt'
-        copy_file '../../../../../spec/dummy/lib/templates/pundit/policy_spec.rb.tt',
-                  'lib/templates/pundit/policy_spec.rb.tt'
 
         # factory bot 
         copy_file '../../../../../spec/dummy/lib/templates/factory_bot/factory.rb.tt',
