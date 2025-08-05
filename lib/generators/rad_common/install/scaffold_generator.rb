9b7483fcd2562fd1344823ef556c8e3300423a32
vincentdinic <vincentdinic@gmail.com>
2025-08-05 12:53:33 -0400

    Tweaks

--- lib/generators/rad_common/install/scaffold_generator.rb
+++ lib/generators/rad_common/install/scaffold_generator.rb
@@ -2,7 +2,10 @@ require 'generators/rspec'
 
 module Rspec
   module Generators
-    class ScaffoldGenerator < Base
+    class ScaffoldGenerator < Rails::Generators::NamedBase
+      include Rails::Generators::ResourceHelpers
+      argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
+
       def create_request_spec
         template Rails.root.join('lib/templates/rspec/scaffold/request_spec.rb.tt'),
                  File.join('spec/requests', class_path, "#{file_name.pluralize}_spec.rb")
@@ -30,6 +33,10 @@ module Rspec
 
       private
 
+        def attributes_names
+          @attributes_names ||= attributes.reject(&:reference?).map(&:name)
+        end
+      
         def policy_methods
           %w[index? show? create? new? update? edit? destroy?]
         end
