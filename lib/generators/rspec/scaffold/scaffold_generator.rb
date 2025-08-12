905f3ba72f975c78cb2f18b3564cfb0a5e65d9d6
vincentdinic <vincentdinic@gmail.com>
2025-08-12 11:16:24 -0400

    Change source paths for templates

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -6,25 +6,31 @@ module Rspec
       include Rails::Generators::ResourceHelpers
       argument :attributes, type: :array, default: [], banner: 'field[:type][:index] field[:type][:index]'
 
-      source_root File.expand_path('../../../../templates', __dir__)
+      def self.source_paths
+        super + [
+          Rails.root.join('lib/templates/rspec/system').to_s,
+          Rails.root.join('lib/templates/factory_bot').to_s,
+          Rails.root.join('lib/templates/pundit').to_s
+        ]
+      end
 
       def create_request_spec
-        template 'rspec/scaffold/request_spec.rb.tt',
+        template 'request_spec.rb.tt',
                  File.join('spec/requests', class_path, "#{file_name.pluralize}_spec.rb")
       end
 
       def create_system_spec
-        template 'rspec/system/system_spec.rb.tt',
+        template 'system_spec.rb.tt',
                  File.join('spec/system', class_path, "#{file_name.pluralize}_spec.rb")
       end
 
       def create_factory
-        template 'factory_bot/factory.rb.tt',
+        template 'factory.rb.tt',
                  File.join('spec/factories', class_path, "#{file_name.pluralize}.rb")
       end
 
       def create_policy
-        template 'pundit/policy.rb.tt',
+        template 'policy.rb.tt',
                  File.join('app/policies', class_path, "#{file_name}_policy.rb")
       end
 
