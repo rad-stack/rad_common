cae3a66a76449865b8e9db8eb578e90d839568b1
vincentdinic <vincentdinic@gmail.com>
2025-08-12 10:52:55 -0400

    Test fix for failing specs

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -6,23 +6,25 @@ module Rspec
       include Rails::Generators::ResourceHelpers
       argument :attributes, type: :array, default: [], banner: 'field[:type][:index] field[:type][:index]'
 
+      source_root File.expand_path('../../../../templates', __dir__)
+
       def create_request_spec
-        template Rails.root.join('lib/templates/rspec/scaffold/request_spec.rb.tt'),
+        template 'rspec/scaffold/request_spec.rb.tt',
                  File.join('spec/requests', class_path, "#{file_name.pluralize}_spec.rb")
       end
 
       def create_system_spec
-        template Rails.root.join('lib/templates/rspec/system/system_spec.rb.tt'),
+        template 'rspec/system/system_spec.rb.tt',
                  File.join('spec/system', class_path, "#{file_name.pluralize}_spec.rb")
       end
 
       def create_factory
-        template Rails.root.join('lib/templates/factory_bot/factory.rb.tt'),
+        template 'factory_bot/factory.rb.tt',
                  File.join('spec/factories', class_path, "#{file_name.pluralize}.rb")
       end
 
       def create_policy
-        template Rails.root.join('lib/templates/pundit/policy.rb.tt'),
+        template 'pundit/policy.rb.tt',
                  File.join('app/policies', class_path, "#{file_name}_policy.rb")
       end
 
