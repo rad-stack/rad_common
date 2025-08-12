b0a158cbd6a75e3a3fde48cb2bab6964edcca4d6
vincentdinic <vincentdinic@gmail.com>
2025-08-12 16:17:04 -0400

    remove extra chartkick

--- spec/dummy/app/javascript/application.js
+++ spec/dummy/app/javascript/application.js
@@ -7,6 +7,5 @@ import 'rad_common_js/src/jqueryAreYouSure';
 import { RadCommon } from 'rad_common_js/src/radCommon';
 RadCommon.setup();
 
-import 'chartkick/chart.js';
 import './app_specific.js';
 import './controllers';
