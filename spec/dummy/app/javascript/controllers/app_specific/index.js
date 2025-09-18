61e9dda8fa802d6c2196ddbfd124977d90330662
garyfoster <gary.foster@radstack.com>
2025-09-18 14:13:59 -0700

    fix sniffs

--- spec/dummy/app/javascript/controllers/app_specific/index.js
+++ spec/dummy/app/javascript/controllers/app_specific/index.js
@@ -1,4 +1,7 @@
 import ChatPollingController from './chat_polling_controller';
 import LlmChatFormController from './llm_chat_form_controller';
-export const controllers = [{ id: 'chat-polling', controller: ChatPollingController },
-                                                   { id: 'llm-chat-form', controller: LlmChatFormController }];
+
+export const controllers = [
+  { id: 'chat-polling', controller: ChatPollingController },
+  { id: 'llm-chat-form', controller: LlmChatFormController }
+];
