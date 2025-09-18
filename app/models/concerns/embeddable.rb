0e21040777b9505ba5aeb00fe3c4a481db90c078
garyfoster <gary.foster@radstack.com>
2025-09-18 10:16:47 -0700

    common AI framework

--- app/models/concerns/embeddable.rb
+++ app/models/concerns/embeddable.rb
@@ -5,4 +5,14 @@ module Embeddable
     has_one :embedding, as: :embeddable, dependent: :destroy
     after_create_commit :update_embedding!, if: -> { EmbeddingService.enabled? && !seeding }
   end
+
+  def update_embedding!
+    content = generate_embedding_content
+
+    embedding_vector = EmbeddingService.generate(content)
+    return unless embedding_vector
+
+    embedding_record = embedding || build_embedding
+    embedding_record.update! embedding: embedding_vector, metadata: embedding_metadata
+  end
 end
