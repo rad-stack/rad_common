f162d98dde2bbd885c4e6923881b8210f1db6b54
Eric Bundy <elbzero@gmail.com>
2025-09-19 07:35:24 -0400

    move assets to rad_common_js

--- spec/dummy/app/javascript/controllers/app_specific/index.js
+++ spec/dummy/app/javascript/controllers/app_specific/index.js
@@ -1,7 +1,1 @@
-import ChatPollingController from './chat_polling_controller';
-import LlmChatFormController from './llm_chat_form_controller';
-
-export const controllers = [
-  { id: 'chat-polling', controller: ChatPollingController },
-  { id: 'llm-chat-form', controller: LlmChatFormController }
-];
+export const controllers = [];
