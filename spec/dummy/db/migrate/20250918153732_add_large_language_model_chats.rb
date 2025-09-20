d644c00cdec737f96d2d85260585045625e7f87c
garyfoster <gary.foster@radstack.com>
2025-09-20 04:32:26 -0700

    trying to fix migration

--- spec/dummy/db/migrate/20250918153732_add_large_language_model_chats.rb
+++ spec/dummy/db/migrate/20250918153732_add_large_language_model_chats.rb
@@ -1,4 +1,4 @@
-class AddLLMChats < ActiveRecord::Migration[7.2]
+class AddLargeLanguageModelChats < ActiveRecord::Migration[7.2]
   def change
     create_table :llm_chats do |t|
       t.references :user, null: false, foreign_key: true
