067db4bfede496465e4662574df28860e8b243c1
Eric Bundy <elbzero@gmail.com>
2025-09-25 11:03:27 -0400

    sniffs

--- lib/devise-twilio-verify/controllers/helpers.rb
+++ lib/devise-twilio-verify/controllers/helpers.rb
@@ -4,14 +4,14 @@ module DeviseTwilioVerify
       extend ActiveSupport::Concern
 
       included do
-        before_action :check_request_and_redirect_to_verify_token, if: :is_signing_in?
+        before_action :check_request_and_redirect_to_verify_token, if: :signing_in?
       end
 
       private
 
         def remember_device(id)
           cookies.signed[:remember_device] = {
-            value: {expires: Time.now.to_i, id: id}.to_json,
+            value: { expires: Time.now.to_i, id: id }.to_json,
             secure: !(Rails.env.test? || Rails.env.development?),
             httponly: !(Rails.env.test? || Rails.env.development?),
             expires: resource_class.twilio_verify_remember_device.from_now
@@ -28,25 +28,29 @@ module DeviseTwilioVerify
           return true if cookie.blank?
 
           # require token for old cookies which just have expiration time and no id
-          return true if cookie.to_s =~ %r{\A\d+\z}
+          return true if cookie.to_s =~ /\A\d+\z/
 
-          cookie = JSON.parse(cookie) rescue ""
-          return cookie.blank? || (Time.now.to_i - cookie['expires'].to_i) > \
-                 resource_class.twilio_verify_remember_device.to_i || cookie['id'] != id
+          cookie = begin
+            JSON.parse(cookie)
+          rescue StandardError
+            ''
+          end
+          cookie.blank? || (Time.now.to_i - cookie['expires'].to_i) >
+            resource_class.twilio_verify_remember_device.to_i || cookie['id'] != id
         end
 
-        def is_devise_sessions_controller?
-          self.class == Devise::SessionsController || self.class.ancestors.include?(Devise::SessionsController)
+        def devise_sessions_controller?
+          instance_of?(Devise::SessionsController) || self.class.ancestors.include?(Devise::SessionsController)
         end
 
-        def is_signing_in?
+        def signing_in?
           if devise_controller? &&
-            is_devise_sessions_controller? &&
-            self.action_name == "create"
+             devise_sessions_controller? &&
+             action_name == 'create'
             return true
           end
 
-          return false
+          false
         end
 
         def check_request_and_redirect_to_verify_token
@@ -56,7 +60,7 @@ module DeviseTwilioVerify
             # login with 2fa
             id = warden.session(resource_name)[:id]
 
-            remember_me = (params.fetch(resource_name, {})[:remember_me].to_s == "1")
+            remember_me = (params.fetch(resource_name, {})[:remember_me].to_s == '1')
             return_to = session["#{resource_name}_return_to"]
             sign_out
 
@@ -67,7 +71,7 @@ module DeviseTwilioVerify
             session["#{resource_name}_return_to"] = return_to if return_to
 
             redirect_to verify_twilio_verify_path_for(resource_name)
-            return
+            nil
           end
         end
 
@@ -82,6 +86,6 @@ module DeviseTwilioVerify
           sign_in(resource_name, @resource)
           set_flash_message(:notice, :signed_in) if is_navigational_format?
         end
-      end
     end
+  end
 end
