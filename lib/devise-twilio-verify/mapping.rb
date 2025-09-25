aed19dbe2c29409d5e0211806454a73859023300
Eric Bundy <elbzero@gmail.com>
2025-09-24 22:13:30 -0400

    moving devise twilio verify

---
+++ lib/devise-twilio-verify/mapping.rb
@@ -0,0 +1,16 @@
+module DeviseTwilioVerify
+  module Mapping
+    private
+    def default_controllers(options)
+      options[:controllers] ||= {}
+      options[:controllers][:passwords] ||= "devise_twilio_verify/passwords"
+      super
+    end
+
+    def default_path_names(options)
+      options[:path_names] ||= {}
+      options[:path_names][:request_sms] ||= 'request-sms'
+      super
+    end
+  end
+end
