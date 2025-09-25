7d1d771ee7faf15657540f69a524a73958ebf102
Eric Bundy <elbzero@gmail.com>
2025-09-25 11:42:15 -0400

    cleanup

--- app/controllers/devise/devise_twilio_verify_controller.rb
+++ app/controllers/devise/devise_twilio_verify_controller.rb
@@ -7,15 +7,6 @@ module Devise
       GET_verify_twilio_verify POST_verify_twilio_verify
     ]
 
-    prepend_before_action :check_resource_not_twilio_verify_enabled, only: %i[
-      GET_verify_twilio_verify_installation POST_verify_twilio_verify_installation
-    ]
-
-    prepend_before_action :authenticate_scope!, only: %i[
-      GET_enable_twilio_verify POST_enable_twilio_verify GET_verify_twilio_verify_installation
-      POST_verify_twilio_verify_installation POST_disable_twilio_verify
-    ]
-
     include Devise::Controllers::Helpers
 
     def GET_verify_twilio_verify
@@ -41,58 +32,6 @@ module Devise
         respond_with resource, location: after_sign_in_path_for(@resource)
       else
         handle_invalid_token :verify_twilio_verify, :invalid_token
-      end
-    end
-
-    def GET_enable_twilio_verify
-      render :enable_twilio_verify
-    end
-
-    # enable 2fa
-    def POST_enable_twilio_verify
-      if resource.update(twilio_verify_enabled: true)
-        redirect_to [resource_name, :verify_twilio_verify_installation] and return
-      else
-        set_flash_message(:error, :not_enabled)
-        redirect_to after_twilio_verify_enabled_path_for(resource) and return
-      end
-    end
-
-    # Disable 2FA
-    def POST_disable_twilio_verify
-      resource.assign_attributes(twilio_verify_enabled: false)
-      resource.save(validate: false)
-      redirect_to after_twilio_verify_disabled_path_for(resource)
-    end
-
-    def GET_verify_twilio_verify_installation
-      if resource_class.twilio_verify_enable_qr_code
-        # response = Authy::API.request_qr_code(id: resource.authy_id)
-        # @twilio_verify_qr_code = response.qr_code
-      end
-      render :verify_twilio_verify_installation
-    end
-
-    def POST_verify_twilio_verify_installation
-      if @resource.mobile_phone.blank? || params[:token].blank?
-        return handle_invalid_token :verify_twilio_verify_installation, :not_enabled
-      end
-
-      verification_check = TwilioVerifyService.verify_sms_token(@resource.mobile_phone, params[:token])
-
-      resource.twilio_verify_enabled = verification_check.status == 'approved'
-
-      if verification_check.status == 'approved' && resource.save
-        remember_device(@resource.id) if params[:remember_device].to_i == 1
-        record_twilio_verify_authentication
-        set_flash_message(:notice, :enabled)
-        redirect_to after_twilio_verify_verified_path_for(resource)
-      else
-        if resource_class.twilio_verify_enable_qr_code
-          # response = Authy::API.request_qr_code(id: resource.authy_id)
-          # @twilio_verify_qr_code = response.qr_code
-        end
-        handle_invalid_token :verify_twilio_verify_installation, :not_enabled
       end
     end
 
--- lib/devise-twilio-verify/routes.rb
+++ lib/devise-twilio-verify/routes.rb
@@ -1,35 +1,25 @@
 module ActionDispatch
   module Routing
     class Mapper
+
       protected
 
         def devise_twilio_verify(mapping, controllers)
+          get_verify_twilio_verify(mapping, controllers)
+          post_verify_twilio_verify(mapping, controllers)
+          match "/#{mapping.path_names[:request_sms]}", controller: controllers[:devise_twilio_verify],
+                                                        action: :request_sms, as: :request_sms, via: :post
+        end
+
+        def get_verify_twilio_verify(mapping, controllers)
           match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
                                                                  action: :GET_verify_twilio_verify,
                                                                  as: :verify_twilio_verify, via: :get
+        end
+
+        def post_verify_twilio_verify(mapping, controllers)
           match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
                                                                  action: :POST_verify_twilio_verify, as: nil, via: :post
-
-          match "/#{mapping.path_names[:enable_twilio_verify]}", controller: controllers[:devise_twilio_verify],
-                                                                 action: :GET_enable_twilio_verify,
-                                                                 as: :enable_twilio_verify, via: :get
-          match "/#{mapping.path_names[:enable_twilio_verify]}", controller: controllers[:devise_twilio_verify],
-                                                                 action: :POST_enable_twilio_verify,
-                                                                 as: nil, via: :post
-
-          match "/#{mapping.path_names[:disable_twilio_verify]}", controller: controllers[:devise_twilio_verify],
-                                                                  action: :POST_disable_twilio_verify,
-                                                                  as: :disable_twilio_verify, via: :post
-
-          match "/#{mapping.path_names[:verify_twilio_verify_installation]}",
-                controller: controllers[:devise_twilio_verify], action: :GET_verify_twilio_verify_installation,
-                as: :verify_twilio_verify_installation, via: :get
-          match "/#{mapping.path_names[:verify_twilio_verify_installation]}",
-                controller: controllers[:devise_twilio_verify], action: :POST_verify_twilio_verify_installation,
-                as: nil, via: :post
-
-          match "/#{mapping.path_names[:request_sms]}", controller: controllers[:devise_twilio_verify],
-                                                        action: :request_sms, as: :request_sms, via: :post
         end
     end
   end
