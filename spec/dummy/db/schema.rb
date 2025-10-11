cae83c7677199c7831261465fd52d8562ed10b8d
garyfoster <gary.foster@radstack.com>
2025-10-11 06:30:52 -0400

    revert changes

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -413,8 +413,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_14_154915) do
     t.string "detected_timezone"
     t.string "ignored_timezone"
     t.string "detected_timezone_js"
-    t.string "provider"
-    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["expired_at"], name: "index_users_on_expired_at"
@@ -425,7 +423,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_14_154915) do
     t.index ["last_activity_at"], name: "index_users_on_last_activity_at"
     t.index ["last_name"], name: "index_users_on_last_name"
     t.index ["password_changed_at"], name: "index_users_on_password_changed_at"
-    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_status_id"], name: "index_users_on_user_status_id"
   end
