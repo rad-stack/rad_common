798a90a71c69784008e877f5a1c16e4d0d25e2ee
Eric Bundy <elbzero@gmail.com>
2025-10-24 18:54:40 -0400

    migration for chat type changes

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_17_110121) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_24_225222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
