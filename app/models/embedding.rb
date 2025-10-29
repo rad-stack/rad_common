0f0f54deb79f252b068bee0bbdc179485a2d96fd
Eric Bundy <elbzero@gmail.com>
2025-10-29 13:42:31 -0400

    using neighbor for searching

--- app/models/embedding.rb
+++ app/models/embedding.rb
@@ -6,13 +6,15 @@ class Embedding < ApplicationRecord
   scope :for_type, ->(type) { where(embeddable_type: type) }
   scope :with_metadata, ->(metadata_query) { where('metadata @> ?', metadata_query.to_json) }
 
-  def self.search(query_embedding, limit: 10, type: nil, metadata_filters: {})
+  def self.search(embedding, limit: 10, type: nil, metadata_filters: {}, distance: nil)
     scope = all
     scope = scope.for_type(type) if type
     scope = scope.with_metadata(metadata_filters) unless metadata_filters.empty?
 
-    scope.where.not(embedding: nil)
-         .order(Arel.sql("embedding <-> '[#{query_embedding.join(',')}]'"))
-         .limit(limit)
+    results = scope.where.not(embedding: nil)
+                   .nearest_neighbors(:embedding, embedding, distance: 'cosine').limit(limit)
+    return results if distance.blank?
+
+    results.select { |ct| ct.neighbor_distance < distance }
   end
 end
