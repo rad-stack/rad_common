d521eea68bffe73f9cef37c0e2c83a84cdfa7eb7
garyfoster <gary.foster@radstack.com>
2025-11-03 14:36:50 -0500

    Add Faraday::BadRequestError to RadRetry

--- app/services/rad_retry.rb
+++ app/services/rad_retry.rb
@@ -8,7 +8,8 @@ class RadRetry
                       Errno::ENOENT, Errno::ECONNRESET, Net::ReadTimeout, Errno::ECONNREFUSED, JSON::ParserError,
                       ActiveStorage::FileNotFoundError, RadSendGridError, EOFError, Twilio::REST::TwilioError,
                       Faraday::ConnectionFailed, Faraday::TimeoutError, Faraday::SSLError, Faraday::ServerError,
-                      Faraday::RackBuilder::StackLocked, Faraday::TooManyRequestsError, RadRetryError].freeze
+                      Faraday::RackBuilder::StackLocked, Faraday::TooManyRequestsError, Faraday::BadRequestError,
+                      RadRetryError].freeze
 
   class << self
     def perform_request(no_delay: false, retry_count: 5, additional_errors: [], raise_original: false, &block)
