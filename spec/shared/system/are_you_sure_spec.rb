5598b4d74dbd18fecc97517db9573ad3c89a99bb
garyfoster <gary.foster@radstack.com>
2025-11-16 06:18:33 -0500

    fix rich text dirty form leave page warning

--- spec/shared/system/are_you_sure_spec.rb
+++ spec/shared/system/are_you_sure_spec.rb
@@ -10,9 +10,18 @@ RSpec.describe 'AreYouSure', type: :system do
       visit '/company/edit'
       fill_in 'company_name', with: 'test'
       find('body').click
-      visit current_path
 
-      expect(confirm_present?).to be true
+      expect(page).to have_css('.simple_form.dirty')
+    end
+  end
+
+  describe 'rich text fields' do
+    it 'warns', :js do
+      visit '/system_messages/new'
+      find('trix-editor').set('test')
+      find('body').click
+
+      expect(page).to have_css('.simple_form.dirty')
     end
   end
 
@@ -21,9 +30,8 @@ RSpec.describe 'AreYouSure', type: :system do
       visit '/'
       tom_search 'test', from: 'search'
       find('body').click
-      visit current_path
 
-      expect(confirm_present?).to be false
+      expect(page).not_to have_css('.simple_form.dirty')
     end
   end
 
@@ -31,9 +39,8 @@ RSpec.describe 'AreYouSure', type: :system do
     it 'does not warn', :js do
       visit '/login_activities?search%5Bcreated_at_start%5D=2020-11-11&search%5Bcreated_at_end%5D=2020-11-11'
       find('body').click
-      visit '/'
 
-      expect(confirm_present?).to be false
+      expect(page).not_to have_css('.simple_form.dirty')
     end
   end
 end
