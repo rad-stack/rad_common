7a75db26d7121756890cc3097889b919b7b7ff75
garyfoster <gary.foster@radstack.com>
2025-12-14 13:37:24 -0500

    fix inflection issue with show route detection

--- spec/services/app_info_spec.rb
+++ spec/services/app_info_spec.rb
@@ -40,4 +40,58 @@ RSpec.describe AppInfo, type: :service do
 
     it { is_expected.to eq result }
   end
+
+  describe 'show_routes' do
+    let(:result) { service.show_routes }
+
+    let(:mock_route) do
+      double('route',
+             defaults: { action: 'show', controller: controller_name },
+             path: double(spec: double(to_s: '/:id')),
+             verb: 'GET')
+    end
+
+    let(:mock_model) do
+      double('model', model_name: double(route_key: model_route_key, singular_route_key: model_singular_route_key))
+    end
+
+    before do
+      allow(Rails.application.routes).to receive(:routes).and_return([mock_route])
+      allow(service).to receive(:application_models).and_return([model_class_name])
+      allow(model_class_name).to receive(:safe_constantize).and_return(mock_model)
+    end
+
+    context 'when controller matches route_key' do
+      let(:controller_name) { 'users' }
+      let(:model_class_name) { 'User' }
+      let(:model_route_key) { 'users' }
+      let(:model_singular_route_key) { 'user' }
+
+      it 'returns the model name' do
+        expect(result).to eq(['User'])
+      end
+    end
+
+    context 'when controller matches singular_route_key (e.g. Equipment)' do
+      let(:controller_name) { 'equipment' }
+      let(:model_class_name) { 'Equipment' }
+      let(:model_route_key) { 'equipment_index' }
+      let(:model_singular_route_key) { 'equipment' }
+
+      it 'returns the model name' do
+        expect(result).to eq(['Equipment'])
+      end
+    end
+
+    context 'when controller does not match either route key' do
+      let(:controller_name) { 'other' }
+      let(:model_class_name) { 'User' }
+      let(:model_route_key) { 'users' }
+      let(:model_singular_route_key) { 'user' }
+
+      it 'does not return the model name' do
+        expect(result).to eq([])
+      end
+    end
+  end
 end
