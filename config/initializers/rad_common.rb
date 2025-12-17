8b510feb55b73bbed25556178f9d515ffbfbe07e
garyfoster <gary.foster@radstack.com>
2025-12-17 08:14:44 -0500

    allow deploying without credentials

--- config/initializers/rad_common.rb
+++ config/initializers/rad_common.rb
@@ -15,28 +15,28 @@ Rails.application.config.active_storage.variant_processor = :mini_magick
 
 Rails.application.config.rad_common = Rails.application.config_for(:rad_common)
 
-RadConfig.check_validity!
+# RadConfig.check_validity!
 
 Rails.application.routes.default_url_options[:host] = RadConfig.host_name!
 
-if Rails.env.staging? || Rails.env.production?
-  Rails.application.config.action_mailer.delivery_method = :smtp
-  Rails.application.config.action_mailer.perform_deliveries = true
-  Rails.application.config.action_mailer.default charset: 'utf-8'
-  Rails.application.config.action_mailer.asset_host = "https://#{RadConfig.host_name!}"
-
-  Rails.application.config.action_mailer.smtp_settings = {
-    address: RadConfig.smtp_address!,
-    port: RadConfig.smtp_port!,
-    enable_starttls_auto: RadConfig.smtp_enable_starttls_auto!,
-    domain: RadConfig.smtp_domain!,
-    authentication: RadConfig.smtp_authentication!,
-    user_name: RadConfig.smtp_username!,
-    password: RadConfig.smtp_password!
-  }
-else
-  Rails.application.config.action_mailer.asset_host = 'http://localhost:3000'
-end
+# if Rails.env.staging? || Rails.env.production?
+#   Rails.application.config.action_mailer.delivery_method = :smtp
+#   Rails.application.config.action_mailer.perform_deliveries = true
+#   Rails.application.config.action_mailer.default charset: 'utf-8'
+#   Rails.application.config.action_mailer.asset_host = "https://#{RadConfig.host_name!}"
+#
+#   Rails.application.config.action_mailer.smtp_settings = {
+#     address: RadConfig.smtp_address!,
+#     port: RadConfig.smtp_port!,
+#     enable_starttls_auto: RadConfig.smtp_enable_starttls_auto!,
+#     domain: RadConfig.smtp_domain!,
+#     authentication: RadConfig.smtp_authentication!,
+#     user_name: RadConfig.smtp_username!,
+#     password: RadConfig.smtp_password!
+#   }
+# else
+#   Rails.application.config.action_mailer.asset_host = 'http://localhost:3000'
+# end
 
 if Rails.env.staging?
   class ChangeStagingEmailSubject
