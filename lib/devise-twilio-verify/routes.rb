36d9292dfb16ebd8223493a42f245a650a600826
garyfoster <gary.foster@radstack.com>
2026-01-07 08:13:52 -0500

    updates

--- lib/devise-twilio-verify/routes.rb
+++ lib/devise-twilio-verify/routes.rb
@@ -4,12 +4,21 @@ module ActionDispatch
       protected
 
         def devise_twilio_verify(mapping, controllers)
-          match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
-                                                                 action: :GET_verify_twilio_verify, as: :verify_twilio_verify, via: :get
-          match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
-                                                                 action: :POST_verify_twilio_verify, as: nil, via: :post
+          get_verify_twilio_verify(mapping, controllers)
+          post_verify_twilio_verify(mapping, controllers)
           match "/#{mapping.path_names[:request_sms]}", controller: controllers[:devise_twilio_verify],
                                                         action: :request_sms, as: :request_sms, via: :post
+        end
+
+        def get_verify_twilio_verify(mapping, controllers)
+          match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
+                                                                 action: :GET_verify_twilio_verify,
+                                                                 as: :verify_twilio_verify, via: :get
+        end
+
+        def post_verify_twilio_verify(mapping, controllers)
+          match "/#{mapping.path_names[:verify_twilio_verify]}", controller: controllers[:devise_twilio_verify],
+                                                                 action: :POST_verify_twilio_verify, as: nil, via: :post
         end
     end
   end
