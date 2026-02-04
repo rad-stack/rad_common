3b7bf810fb359c26fcfc3699c89d905445396716
Eric Bundy <elbzero@gmail.com>
2026-02-04 11:52:13 -0500

    parse array values and store as array in config

--- app/services/rad_reports/configuration_builder.rb
+++ app/services/rad_reports/configuration_builder.rb
@@ -42,7 +42,34 @@ module RadReports
       def process_config_item(param, parse_formula, normalize_sortable)
         param_hash = param.to_h
         param_hash = parse_formula_if_needed(param_hash, parse_formula)
+        param_hash = parse_default_value_array(param_hash)
         normalize_sortable_if_needed(param_hash, normalize_sortable)
+      end
+
+      def parse_default_value_array(param_hash)
+        default_value = param_hash['default_value']
+        return param_hash unless default_value.is_a?(String) && default_value.match?(/\A\s*\[.*\]\s*\z/m)
+
+        param_hash['default_value'] = parse_array_string(default_value)
+        param_hash
+      end
+
+      def parse_array_string(str)
+        # Try JSON first (handles [1, 2, 3] and ["one", "two"])
+        JSON.parse(str)
+      rescue JSON::ParserError
+        # Handle Ruby-style single quotes: ['one', 'two', 'three']
+        inner = str.strip[1..-2] # Remove brackets
+        inner.split(',').map do |item|
+          item = item.strip
+          # Remove surrounding quotes (single or double)
+          if (item.start_with?("'") && item.end_with?("'")) ||
+             (item.start_with?('"') && item.end_with?('"'))
+            item[1..-2]
+          else
+            item
+          end
+        end
       end
 
       def parse_formula_if_needed(param_hash, parse_formula)
