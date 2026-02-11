96cf837f7244e5573520656c5e9f6c2e9477209a
vincentdinic <vincentdinic@gmail.com>
2026-02-11 16:34:29 -0500

    add chartjs to yarn and tweak radCommon.js import

--- app/javascript/rad_common/radCommon.js
+++ app/javascript/rad_common/radCommon.js
@@ -15,9 +15,7 @@ import { Duplicates } from './duplicates';
 import { RadTomSelect } from './radTomSelect';
 import { SentryTest } from './sentry';
 import { ActionTextFileValidations } from './actionTextFileValidations';
-import Chartkick from 'chartkick';
-import * as Chart from 'chart.js';
-Chartkick.use(Chart);
+import 'chartkick/chart.js';
 
 import './radTurbo';
 
