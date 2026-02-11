4b495aaf6873dfe475f29e0871f6a84244fb43e9
garyfoster <gary.foster@radstack.com>
2026-02-11 11:00:40 -0500

    temporary solution to vector limits/chunking situation

--- app/services/embedding_service.rb
+++ app/services/embedding_service.rb
@@ -5,7 +5,7 @@ class EmbeddingService
     client = OpenAI::Client.new(access_token: RadConfig.open_ai_api_key!)
 
     response = RadRetry.perform_request do
-      client.embeddings(parameters: { model: RadCommon::OPEN_AI_EMBEDDING_MODEL, input: text })
+      client.embeddings(parameters: { model: RadCommon::OPEN_AI_EMBEDDING_MODEL, input: text.slice(0, 8192) })
     end
 
     response.dig('data', 0, 'embedding')
