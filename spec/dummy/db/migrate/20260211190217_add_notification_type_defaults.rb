0660fd1ce0891e1f2dda7548e846ead6619485d6
Eric Bundy <elbzero@gmail.com>
2026-02-17 10:59:20 -0500

    add defaults in migration

--- spec/dummy/db/migrate/20260211190217_add_notification_type_defaults.rb
+++ spec/dummy/db/migrate/20260211190217_add_notification_type_defaults.rb
@@ -3,5 +3,10 @@ class AddNotificationTypeDefaults < ActiveRecord::Migration[7.2]
     add_column :notification_types, :default_email, :boolean, null: false, default: false
     add_column :notification_types, :default_feed, :boolean, null: false, default: false
     add_column :notification_types, :default_sms, :boolean, null: false, default: false
+
+    NotificationType.all.each do |notification_type|
+      notification_type.add_defaults
+      notification_type.save!
+    end
   end
 end
