ac7cafae6e6c66a44cb098400e60664fe27dd68d
garyfoster <gary.foster@radstack.com>
2026-03-10 11:39:10 -0400

    ability to select all/none for notification settings

--- app/javascript/rad_common/radCommonDynamicUpdater.js
+++ app/javascript/rad_common/radCommonDynamicUpdater.js
@@ -11,6 +11,20 @@ export class RadCommonDynamicUpdater {
             form[0].reset();
           });
       });
+
+      $('.select-all-settings, .select-none-settings').click(function(e) {
+        e.preventDefault();
+        var field = $(this).data('field');
+        var checked = $(this).hasClass('select-all-settings');
+
+        $('form.dynamic-updater').each(function() {
+          var checkbox = $(this).find('input[type="checkbox"][id*="_' + field + '_"]');
+          if (checkbox.length && !checkbox.prop('disabled') && checkbox.prop('checked') !== checked) {
+            checkbox.prop('checked', checked);
+            checkbox.trigger('change');
+          }
+        });
+      });
     });
   }
 }
