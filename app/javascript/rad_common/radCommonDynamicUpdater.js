c323579e807289de2a2f27a38faa5126858c3307
garyfoster <gary.foster@radstack.com>
2026-03-21 05:29:07 -0400

    updates

--- app/javascript/rad_common/radCommonDynamicUpdater.js
+++ app/javascript/rad_common/radCommonDynamicUpdater.js
@@ -5,7 +5,13 @@ export class RadCommonDynamicUpdater {
         var form = $(this).closest('form');
         var url = form.attr('action') + '.json';
         var data = $(this).closest('form').serializeArray();
+        var input = $(this);
         $.post(url, data)
+          .done(function() {
+            if (input.attr('id') && input.attr('id').match(/^notification_setting_enabled_/)) {
+              input.closest('tr').toggleClass('table-danger', !input.prop('checked'));
+            }
+          })
           .fail(function(data) {
             alert(data.responseJSON.error);
             form[0].reset();
