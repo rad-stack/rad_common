0984c59cc463702c3edc7f5a36dec9d1abc8d712
calebtocco <32717912+calebtocco@users.noreply.github.com>
2026-03-28 07:50:21 -0400

    Update devise_twilio_verify_controller.rb

--- app/controllers/users/devise_twilio_verify_controller.rb
+++ app/controllers/users/devise_twilio_verify_controller.rb
@@ -1,13 +1,21 @@
 module Users
   class DeviseTwilioVerifyController < Devise::DeviseTwilioVerifyController
     def GET_verify_twilio_verify
-      if RadTwilio.send_verify_sms(@resource.mobile_phone)
-        flash.now[:notice] = 'A verification token has been texted to you.'
-      else
-        flash.now[:error] = 'The verification code failed to send. Please click "Resend Text".'
+      if should_send_sms?
+        if RadTwilio.send_verify_sms(@resource.mobile_phone)
+          flash.now[:notice] = 'A verification token has been texted to you.'
+        else
+          flash.now[:error] = 'The verification code failed to send. Please click "Resend Text".'
+        end
       end
 
-      super
+      render :verify_twilio_verify
     end
+
+    private
+
+      def should_send_sms?
+        @resource.twilio_totp_factor_sid.blank? && @resource.mobile_phone.present?
+      end
   end
 end
