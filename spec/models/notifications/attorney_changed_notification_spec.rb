fbcc975e8614548b576de8d5abde6857114f8648
garyfoster <gary.foster@radstack.com>
2026-04-03 06:25:28 -0400

    fix hole in notification defaults when no mobile phone

--- spec/models/notifications/attorney_changed_notification_spec.rb
+++ spec/models/notifications/attorney_changed_notification_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe Notifications::AttorneyChangedNotification do
       expect(notification_type.default_feed).to be false
     end
   end
+
+  describe 'enabled_for_method?' do
+    let(:notification_type) { Notifications::AttorneyChangedNotification.main }
+
+    context 'when user has no notification setting record' do
+      let(:user) { create :admin }
+
+      before { NotificationSetting.where(notification_type: notification_type, user: user).destroy_all }
+
+      it 'falls back to default_sms when user has a mobile phone' do
+        expect(notification_type.send(:enabled_for_method?, user.id, :sms)).to be true
+      end
+
+      it 'returns false for sms when user has no mobile phone' do
+        user.update_columns(mobile_phone: nil)
+        expect(notification_type.send(:enabled_for_method?, user.id, :sms)).to be false
+      end
+
+      it 'still falls back to default_email regardless of mobile phone' do
+        user.update_columns(mobile_phone: nil)
+        expect(notification_type.send(:enabled_for_method?, user.id, :email)).to be true
+      end
+    end
+  end
 end
