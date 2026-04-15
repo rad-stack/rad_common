22d98191b2f9c043264694686c68b0ba084ed5e7
garyfoster <gary.foster@radstack.com>
2026-04-15 07:55:36 -0400

    automatically delete stale pending users

--- app/models/notifications/stale_pending_user_deleted_notification.rb
+++ app/models/notifications/stale_pending_user_deleted_notification.rb
@@ -1,10 +1,13 @@
 module Notifications
   class StalePendingUserDeletedNotification < ::NotificationType
     def mailer_message
-      message = "#{payload[:name]} was deleted because they were pending for more than #{RadUser::STALE_PENDING_DAYS} days."
+      days = RadUser::STALE_PENDING_DAYS
+
+      message = "#{payload[:name]} was deleted because they were pending for more than #{days} days."
       message += "\n\n\u2022 Email: #{payload[:email]}"
       message += "\n\n\u2022 Mobile Phone: #{payload[:mobile_phone] || 'N/A'}"
       message += "\n\n\u2022 Created: #{ApplicationController.helpers.format_date(payload[:created_at])}"
+
       message
     end
 
