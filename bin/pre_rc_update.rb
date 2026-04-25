06da1dce9ecd44d670d73433b6dfc93afe1941be
garyfoster <gary.foster@radstack.com>
2026-04-25 07:02:21 -0400

    unrelated installer fixes

--- bin/pre_rc_update.rb
+++ bin/pre_rc_update.rb
@@ -2,8 +2,23 @@
 
 require_relative '../lib/rad_common/config_updater'
 
+RadCommon::ConfigUpdater.add_rad_config_setting 'last_first_user', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'timezone_detection', 'true'
+RadCommon::ConfigUpdater.add_rad_config_setting 'portal', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'validate_user_domains', 'true'
+RadCommon::ConfigUpdater.add_rad_config_setting 'show_sign_in_marketing', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'filter_toggle_default_behavior', 'always_open'
 RadCommon::ConfigUpdater.add_rad_config_setting 'action_cable_enabled', 'false'
 RadCommon::ConfigUpdater.add_rad_config_setting 'expire_password_after_days', '90'
+RadCommon::ConfigUpdater.add_rad_config_setting 'two_factor_auth_email_fallback', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'mailer_phone_number', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'additional_user_registration_params', '[]'
+RadCommon::ConfigUpdater.add_rad_config_setting 'clone_local_exclude', '[]'
+RadCommon::ConfigUpdater.add_rad_config_setting 'crawlable_subdomains', '[]'
+RadCommon::ConfigUpdater.add_rad_config_setting 'always_crawl', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'allow_crawling', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'procfile_override', 'false'
+RadCommon::ConfigUpdater.add_rad_config_setting 'database_config_override', 'false'
 RadCommon::ConfigUpdater.rename_rad_config_setting 'twilio_verify_enabled', 'two_factor_auth_enabled'
 
 RadCommon::ConfigUpdater.rename_rad_config_setting 'twilio_verify_remember_device_days',
