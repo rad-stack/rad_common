f69d1f18cc37912137749a4ad20ff5b93d4e788b
garyfoster <gary.foster@radstack.com>
2026-05-11 05:16:18 -0700

    updates

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -92,10 +92,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [".#{RadConfig.host_name!.split('.', 2).last}"]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
